7295ea7aa261bb3dfdac56a7718c027faa9dd942
zihehuang <zihehuang@gmail.com>
2017-04-14 01:11:56 +0000

    added tests for agreement.

--- examples/src/test/scala/bifrost/BifrostGenerators.scala
+++ examples/src/test/scala/bifrost/BifrostGenerators.scala
@@ -1,7 +1,9 @@
 package bifrost
 
+import examples.bifrost.contract._
 import examples.bifrost.transaction.box.ContractBox
 import org.scalacheck.Gen
+import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 import scorex.testkit.CoreGenerators
 
 /**
@@ -10,9 +12,51 @@ import scorex.testkit.CoreGenerators
 trait BifrostGenerators extends CoreGenerators {
   lazy val stringGen = nonEmptyBytesGen.map(new String(_))
 
+  lazy val doubleGen: Gen[Double] = Gen.choose(0, Double.MaxValue)
+
+  lazy val bigDecimalGen: Gen[BigDecimal] = for {
+    a <- doubleGen
+  } yield BigDecimal.double2bigDecimal(a)
+
+  //generate a num from smallInt for len of seq, map that many tuples, concatenate together into seq
+  lazy val seqDoubleGen: Gen[Seq[(Double, (Double, Double, Double))]] = for {
+      a <- doubleGen
+      b <- doubleGen
+      c <- doubleGen
+      d <- doubleGen
+    } yield Seq((a, (b,c,d)))
+
+  lazy val shareFuncGen: Gen[ShareFunction] = seqDoubleGen.map(new PiecewiseLinearMultiple(_))
+
+  lazy val seqLongDoubleGen: Gen[Seq[(Long, Double)]] = for {
+    a <- positiveLongGen
+    b <- doubleGen
+  } yield Seq((a, b))
+
+  lazy val fulfilFuncGen: Gen[FulfilmentFunction] = seqLongDoubleGen.map(new PiecewiseLinearSingle(_))
+
   lazy val bifrostBoxGen: Gen[ContractBox] = for {
     proposition <- propositionGen
     nonce <- positiveLongGen
     value <- stringGen
   } yield ContractBox(proposition, nonce, value, "This is an Agreement")
+
+  lazy val agreementTermsGen: Gen[AgreementTerms] = for {
+    pledge <- bigDecimalGen
+    xrate <- bigDecimalGen
+    share <- shareFuncGen
+    fulfilment <- fulfilFuncGen
+  } yield new AgreementTerms(pledge, xrate, share, fulfilment)
+
+  lazy val partiesGen: Gen[IndexedSeq[PublicKey25519Proposition]] = for {
+    a <- propositionGen
+  } yield IndexedSeq(a)
+
+  lazy val agreementGen: Gen[Agreement] = for {
+    parties <- partiesGen
+    terms <- agreementTermsGen
+    nonce <- positiveLongGen
+    timestamp <- positiveLongGen
+    expirationTimestamp <- positiveLongGen
+  } yield Agreement(parties, terms, nonce, timestamp, expirationTimestamp)
 }
--- examples/src/test/scala/bifrost/serialization/BifrostBoxTest.scala
+++ examples/src/test/scala/bifrost/serialization/BifrostBoxTest.scala
@@ -1,6 +1,8 @@
 package bifrost.serialization
 
 import bifrost.BifrostGenerators
+import examples.bifrost.contract.Agreement
+import examples.bifrost.transaction.AgreementCompanion
 import examples.bifrost.transaction.box.{BifrostBoxSerializer, ContractBox}
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
@@ -14,12 +16,21 @@ class SerializationTests extends PropSpec
   with Matchers
   with BifrostGenerators {
 
-  property("BifrostBox Serialization") {
+  property("Agreement Serialization") {
+    forAll(agreementGen) {
+      b: Agreement =>
+        val parsed = AgreementCompanion.parseBytes(AgreementCompanion.toBytes(b)).get
+
+        AgreementCompanion.toBytes(parsed) shouldEqual AgreementCompanion.toBytes(b)
+    }
+  }
+
+  property("ContractBox Serialization") {
     forAll(bifrostBoxGen) {
       b: ContractBox =>
         val parsed = BifrostBoxSerializer.parseBytes(BifrostBoxSerializer.toBytes(b)).get
         val serialized = BifrostBoxSerializer.toBytes(parsed)
-        print(new String(serialized))
+        // print(new String(serialized))
         serialized shouldEqual BifrostBoxSerializer.toBytes(b)
     }
   }
