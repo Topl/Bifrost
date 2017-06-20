bd81883580823a2cbb90854bf6abf4badc8bc170
Matthew Kindy II <matt.kindy.ii@gmail.com>
2017-06-20 11:08:34 -0500

    Cleaned up some of the double-calls. Printing our js properly now and loading Bifrost

--- src/main/scala/bifrost/console/BifrostConsole.java
+++ src/main/scala/bifrost/console/BifrostConsole.java
@@ -2,9 +2,12 @@ package bifrost.console;
 
 import jdk.nashorn.api.scripting.NashornScriptEngine;
 import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import scala.collection.script.Script;
 
 import javax.script.*;
 import java.io.*;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -21,71 +24,52 @@ public class BifrostConsole {
 
     private static ScheduledExecutorService globalScheduledThreadPool = Executors.newScheduledThreadPool(20);
 
-    BifrostConsole() {
-        //initNashornEngine();
-    }
-
-   /* synchronized private void initNashornEngine() {
-        ScriptContext context = new SimpleScriptContext();
-
-        context.setBindings(nashornEngine.createBindings(), ScriptContext.ENGINE_SCOPE);
-        context.setAttribute("__NASHORN_POLYFILL_TIMER__", globalScheduledThreadPool, ScriptContext.ENGINE_SCOPE);
-
-        Bindings initialBindings = context.getBindings(ScriptContext.ENGINE_SCOPE);
-
-    }*/
-
-    public ScriptEngine getEngine() {
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("nashorn");
-        return engine;
-    }
-
-    public ScriptContext getContext(ScriptEngine engine) {
-        ScriptContext context = new SimpleScriptContext();
-        Bindings bindings = engine.createBindings();
-        context.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
-        context.setBindings(nashornEngine.createBindings(), ScriptContext.ENGINE_SCOPE);
-        context.setAttribute("__NASHORN_POLYFILL_TIMER__", globalScheduledThreadPool, ScriptContext.ENGINE_SCOPE);
-
-        Bindings initialBindings = context.getBindings(ScriptContext.ENGINE_SCOPE);
-
-        return context;
-    }
-
     public static void main(String[] args) throws ScriptException, FileNotFoundException {
-        BifrostConsole bifrostConsole = new BifrostConsole();
-        ScriptEngine testEngine = bifrostConsole.getEngine();
-        ScriptContext testContext = bifrostConsole.getContext(testEngine);
-        testEngine.setContext(testContext);
+        ScriptContext context = nashornEngine.getContext();
+        context.setBindings(nashornEngine.createBindings(), ScriptContext.ENGINE_SCOPE);
+        context.setAttribute("__NASHORN_POLYFILL_TIMER__", globalScheduledThreadPool, ScriptContext.ENGINE_SCOPE);
+
+        /* Sets standard context so we can have all the globals in the same place */
 
        // Compilable compilableEngine = (Compilable) testEngine;
        // CompiledScript compiled = compilableEngine.compile("src/main/scala/bifrost/console/BifrostConsole.js");
 
-        Bindings bindings = testEngine.getBindings(ScriptContext.ENGINE_SCOPE);
+
+        Bindings bindings = nashornEngine.getBindings(ScriptContext.ENGINE_SCOPE);
 
         for(Map.Entry me : bindings.entrySet()) {
             System.out.printf("%s: %s\n",me.getKey(),String.valueOf(me.getValue()));
         }
 
         try {
-            testEngine.eval(new FileReader("src/main/scala/bifrost/console/bundle.js"));
+            context.setAttribute("Bifrost", nashornEngine.eval(new FileReader("src/main/scala/bifrost/console/bundle.js")), ScriptContext.ENGINE_SCOPE);
+
+            bindings = nashornEngine.getBindings(ScriptContext.ENGINE_SCOPE);
+
+            for(Map.Entry me : bindings.entrySet()) {
+                System.out.printf("%s: %s\n",me.getKey(),String.valueOf(me.getValue()));
+            }
         } catch(ScriptException|FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        Invocable invocable = (Invocable) testEngine;
+        //System.out.println("Engine " + testEngine.getBindings(ScriptContext.ENGINE_SCOPE).);
+        //System.out.println("Global " + testEngine.getBindings(ScriptContext.GLOBAL_SCOPE).toString());
+
+        Invocable invocable = (Invocable) nashornEngine;
 
         Scanner scan = new Scanner(System.in);
         String input = "";
         do {
-            System.out.print("Topl: ");
+            System.out.print("Topl > ");
             input = scan.nextLine();
             try {
-                Map<String, Object> result = (Map<String, Object>) testEngine.eval(input);
-                System.out.println(result);
+                ScriptObjectMirror result = ((ScriptObjectMirror)nashornEngine.eval(input));
+                for (Map.Entry<String, Object> entry : result.entrySet()) {
+                    System.out.println(entry.getKey() + ", " + entry.getValue());
+                }
             } catch(ScriptException e) {
-                throw new RuntimeException(e);
+               e.printStackTrace();
             } /*catch (NoSuchMethodException e) {
                 e.printStackTrace();
             }*/
