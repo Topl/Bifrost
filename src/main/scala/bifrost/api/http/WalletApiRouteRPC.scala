0d218d77c1188caa950e9aeb25cec30e835211db
yamir1995 <yamirtainwala@gmail.com>
2018-11-12 06:41:19 -0500

    Bifrost #128 added new RPC format wallet api route

---
+++ src/main/scala/bifrost/api/http/WalletApiRouteRPC.scala
@@ -0,0 +1,128 @@
+package bifrost.api.http
+
+import akka.actor.{ActorRef, ActorRefFactory}
+import akka.http.scaladsl.server.Route
+import bifrost.history.BifrostHistory
+import bifrost.mempool.BifrostMemPool
+import bifrost.state.BifrostState
+import bifrost.transaction.PolyTransfer
+import bifrost.transaction.box.{ArbitBox, PolyBox}
+import bifrost.wallet.BWallet
+import io.circe.Json
+import io.circe.parser.parse
+import io.circe.parser._
+import io.circe.syntax._
+import scorex.core.LocalInterface.LocallyGeneratedTransaction
+import scorex.core.api.http.{ApiException, SuccessApiResponse}
+import scorex.core.settings.Settings
+import scorex.core.transaction.box.proposition.{ProofOfKnowledgeProposition, PublicKey25519Proposition}
+import scorex.core.transaction.state.PrivateKey25519
+import scorex.crypto.encode.Base58
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success, Try}
+
+case class WalletApiRouteRPC(override val settings: Settings, nodeViewHolderRef: ActorRef)
+                       (implicit val context: ActorRefFactory) extends ApiRouteWithView {
+  type HIS = BifrostHistory
+  type MS = BifrostState
+  type VL = BWallet
+  type MP = BifrostMemPool
+  override val route: Route = pathPrefix("wallet") { walletRoute }
+
+  //noinspection ScalaStyle
+  def walletRoute: Route = path("") { entity(as[String]) { body =>
+    withAuth {
+      postJsonRoute {
+        viewAsync().map { view =>
+          var reqId = ""
+          parse(body) match {
+            case Left(failure) => ApiException(failure.getCause)
+            case Right(json) =>
+              val futureResponse: Try[Future[Json]] = Try {
+                val id = (json \\ "id").head.asString.get
+                reqId = id
+                require((json \\ "jsonrpc").head.asString.get == "2.0")
+                val params = (json \\ "params").head.asArray.get
+                require(params.size <= 5, s"size of params is ${params.size}")
+
+                (json \\ "method").head.asString.get match {
+                  case "transfer" => transfer(params.head, id)
+                  case "balances" => balances(params.head, id)
+                  case "unlockKeyfile" => unlockKeyfile(params.head, id)
+                  case "generateKeyfile" => generateKeyfile(params.head, id)
+                }
+              }
+              futureResponse map {
+                response => Await.result(response, timeout.duration)
+              }
+              match {
+                case Success(resp) => BifrostSuccessResponse(resp, reqId)
+                case Failure(e) => BifrostErrorResponse(e, 500, reqId, verbose = settings.settingsJSON.getOrElse("verboseAPI", false.asJson).asBoolean.get)
+              }
+          }
+        }
+      }
+    }
+  }}
+
+  private def transfer(params: Json, id: String): Future[Json] = {
+    viewAsync().map { view =>
+      val wallet = view.vault
+      val amount: Long = (params \\ "amount").head.asNumber.get.toLong.get
+      val recipient: PublicKey25519Proposition = PublicKey25519Proposition(Base58.decode((params \\ "recipient").head.asString.get).get)
+      val fee: Long = (params \\ "fee").head.asNumber.flatMap(_.toLong).getOrElse(0L)
+      val data: String = (params \\ "data").head.asString.get
+      val tx = PolyTransfer.create(wallet, IndexedSeq((recipient, amount)), fee, data).get
+      nodeViewHolderRef ! LocallyGeneratedTransaction[ProofOfKnowledgeProposition[PrivateKey25519], PolyTransfer](tx)
+      tx.json
+    }
+  }
+
+  private def balances(params: Json, id: String): Future[Json] = {
+      viewAsync().map { view =>
+        val wallet = view.vault
+        val boxes = wallet.boxes()
+
+        Map("polyBalance" -> boxes.flatMap(_.box match {
+            case pb: PolyBox => Some(pb.value)
+            case _ => None
+          }).sum.toString.asJson,
+          "arbitBalance" -> boxes.flatMap(_.box match {
+            case ab: ArbitBox => Some(ab.value)
+            case _ => None
+          }).sum.toString.asJson,
+          "publicKeys" -> wallet.publicKeys.flatMap(_ match {
+            case pkp: PublicKey25519Proposition => Some(Base58.encode(pkp.pubKeyBytes))
+            case _ => None
+          }).asJson,
+          "boxes" -> boxes.map(_.box.json).asJson
+        ).asJson
+      }
+    }
+
+  private def generateKeyfile(params: Json, id: String): Future[Json] = {
+    viewAsync().map { view =>
+      val wallet = view.vault
+      val password: String = (params \\ "password").head.asString.get
+      val pubKey = wallet.generateNewSecret(password)
+      Map(
+        "publicKey" -> Base58.encode(pubKey.pubKeyBytes).asJson
+      ).asJson
+    }
+  }
+
+  private def unlockKeyfile(params: Json, id: String): Future[Json] = {
+    viewAsync().map { view =>
+      val wallet = view.vault
+      val publicKey: String = (params \\ "publicKey").head.asString.get
+      val password: String = (params \\ "password").head.asString.get
+      wallet.unlockKeyFile(publicKey, password)
+      Map(
+        publicKey -> "unlocked".asJson
+      ).asJson
+    }
+  }
+
+}
---
+++ src/test/scala/bifrost/api/WalletRPCSpec.scala
@@ -0,0 +1,159 @@
+package bifrost.api
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest, MediaTypes}
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.pattern.ask
+import akka.util.{ByteString, Timeout}
+import bifrost.api.http.{WalletApiRouteRPC}
+import bifrost.history.BifrostHistory
+import bifrost.mempool.BifrostMemPool
+import bifrost.scorexMod.GenericNodeViewHolder.{CurrentView, GetCurrentView}
+import bifrost.state.{BifrostState}
+import bifrost.transaction.{BifrostTransaction}
+import bifrost.wallet.BWallet
+import bifrost.{BifrostGenerators, BifrostNodeViewHolder}
+import io.circe.parser.parse
+import org.scalatest.{Matchers, WordSpec}
+import scorex.crypto.encode.Base58
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.reflect.io.Path
+import scala.util.Try
+
+/**
+  * Created by cykoz on 7/3/2017.
+  */
+class WalletRPCSpec extends WordSpec
+  with Matchers
+  with ScalatestRouteTest
+  with BifrostGenerators {
+
+  val actorSystem = ActorSystem(settings.agentName)
+  val nodeViewHolderRef: ActorRef = actorSystem.actorOf(Props(new BifrostNodeViewHolder(settings)))
+  nodeViewHolderRef
+  val route = WalletApiRouteRPC(settings, nodeViewHolderRef).route
+
+  def httpPOST(jsonRequest: ByteString): HttpRequest = {
+    HttpRequest(
+      HttpMethods.POST,
+      uri = "/wallet/",
+      entity = HttpEntity(MediaTypes.`application/json`, jsonRequest)
+    )
+  }
+
+  implicit val timeout = Timeout(10.seconds)
+
+  private def view() = Await.result((nodeViewHolderRef ? GetCurrentView)
+    .mapTo[CurrentView[BifrostHistory, BifrostState, BWallet, BifrostMemPool]], 10.seconds)
+
+  val publicKeys = Map(
+    "investor" -> "6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ",
+    "producer" -> "A9vRt6hw7w4c7b4qEkQHYptpqBGpKM5MGoXyrkGCbrfb",
+    "hub" -> "F6ABtYMsJABDLH2aj7XVPwQr5mH7ycsCE4QGQrLeB3xU"
+  )
+
+  var newPubKey: String = ""
+
+  // Unlock Secrets
+  val gw: BWallet = view().vault
+  gw.unlockKeyFile(publicKeys("investor"), "genesis")
+  gw.unlockKeyFile(publicKeys("producer"), "genesis")
+  gw.unlockKeyFile(publicKeys("hub"), "genesis")
+
+  "Wallet RPC" should {
+    "Get balances" in {
+      val requestBody = ByteString(
+        s"""
+           |{
+           |   "jsonrpc": "2.0",
+           |   "id": "30",
+           |   "method": "balances",
+           |   "params": [{}]
+           |}
+        """.stripMargin)
+
+      httpPOST(requestBody) ~> route ~> check {
+        val res = parse(responseAs[String]).right.get
+        (res \\ "error").isEmpty shouldBe true
+        (res \\ "result").head.asObject.isDefined shouldBe true
+      }
+    }
+
+    "Generate a keyfile" in {
+      val requestBody = ByteString(
+        s"""
+           |{
+           |   "jsonrpc": "2.0",
+           |   "id": "30",
+           |   "method": "generateKeyfile",
+           |   "params": [{
+           |     "password": "testpassword"
+           |   }]
+           |}
+        """.stripMargin)
+      //println(requestBody)
+      httpPOST(requestBody) ~> route ~> check {
+        val res = parse(responseAs[String]).right.get
+        (res \\ "error").isEmpty shouldBe true
+        (res \\ "result").head.asObject.isDefined shouldBe true
+        newPubKey = ((res \\ "result").head \\ "publicKey").head.asString.get
+      }
+    }
+
+    "Unlock a keyfile" in {
+      val requestBody = ByteString(
+        s"""
+           |{
+           |   "jsonrpc": "2.0",
+           |   "id": "30",
+           |   "method": "unlockKeyfile",
+           |   "params": [{
+           |     "publicKey": "${newPubKey}",
+           |     "password": "testpassword"
+           |   }]
+           |}
+        """.stripMargin)
+      //println(requestBody)
+      httpPOST(requestBody) ~> route ~> check {
+        val res = parse(responseAs[String]).right.get
+        (res \\ "error").isEmpty shouldBe true
+        (res \\ "result").head.asObject.isDefined shouldBe true
+      }
+    }
+
+    "Transfer some polys" in {
+      val requestBody = ByteString(
+        s"""
+           |{
+           |   "jsonrpc": "2.0",
+           |   "id": "30",
+           |   "method": "transfer",
+           |   "params": [{
+           |     "recipient": "${publicKeys("investor")}",
+           |     "amount": 5,
+           |     "fee": 0,
+           |     "data": ""
+           |   }]
+           |}
+        """.stripMargin)
+      //println(requestBody)
+      httpPOST(requestBody) ~> route ~> check {
+        val res = parse(responseAs[String]).right.get
+        (res \\ "error").isEmpty shouldBe true
+        (res \\ "result").head.asObject.isDefined shouldBe true
+        val txHash = ((res \\ "result").head \\ "id").head.asString.get
+        val txInstance: BifrostTransaction = view().pool.getById(Base58.decode(txHash).get).get
+        view().pool.remove(txInstance)
+      }
+    }
+
+  }
+
+
+  object WalletRPCSpec {
+    val path: Path = Path("/tmp/scorex/test-data")
+    Try(path.deleteRecursively())
+  }
+}
