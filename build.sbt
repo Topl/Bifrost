a9a54770835a76aeda07fca5dfde0c5b402c3a57
tuxman <n.edmonds@topl.me>
2019-05-22 12:48:27 -0500

    Successful JS parsing using GraalJS

--- build.sbt
+++ build.sbt
@@ -84,6 +84,9 @@ val consoleDependencies = Seq(
 // https://mvnrepository.com/artifact/org.graalvm.sdk/graal-sdk
 libraryDependencies += "org.graalvm.sdk" % "graal-sdk" % "19.0.0"
 
+// https://mvnrepository.com/artifact/org.graalvm.js/js
+libraryDependencies += "org.graalvm.js" % "js" % "19.0.0"
+
 // https://mvnrepository.com/artifact/org.graalvm.truffle/truffle-api
 libraryDependencies += "org.graalvm.truffle" % "truffle-api" % "19.0.0"
 
--- src/test/scala/bifrost/contract/TruffleSpec.scala
+++ src/test/scala/bifrost/contract/TruffleSpec.scala
@@ -1,9 +1,11 @@
 package bifrost.contract
 
+
 import bifrost.{BifrostGenerators, ValidGenerators}
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
 import org.graalvm.polyglot._
+import com.oracle.js.parser.{ErrorManager, Parser, ScriptEnvironment, Source}
 
 class TruffleSpec extends PropSpec
   with Matchers
@@ -14,9 +16,26 @@ class TruffleSpec extends PropSpec
     """
       |var x = 1;
       |var y = "test";
+      |function z() { return x; };
     """.stripMargin
 
-  val context: Context = Context.create("js", script)
+  val scriptEnv: ScriptEnvironment = ScriptEnvironment.builder
+    .ecmaScriptVersion(8)
+    .constAsVar(false)
+    .earlyLvalueError(true)
+    .emptyStatements(false)
+    .syntaxExtensions(true)
+    .scripting(false)
+    .shebang(false)
+    .strict(true)
+    .functionStatementBehavior(ScriptEnvironment.FunctionStatementBehavior.ERROR)
+    .build()
 
+  val errManager = new ErrorManager.ThrowErrorManager
+  val src = Source.sourceFor("script", script)
+  val parser: Parser = new Parser(scriptEnv, src, errManager)
+  val parsed = parser.parse()
+
+  println(parsed.getBody.getStatements)
 
 }
