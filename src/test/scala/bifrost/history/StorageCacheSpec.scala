f288faf9da92016256aba1c78b9f0812409b9d38
Jingfei Zhou <jingfei1024@gmail.com>
2020-03-31 11:18:02 -0500

    adding multiple block and checking if cache and storage match

--- src/test/scala/bifrost/history/StorageCacheSpec.scala
+++ src/test/scala/bifrost/history/StorageCacheSpec.scala
@@ -2,14 +2,17 @@ package bifrost.history
 
 
 import bifrost.BifrostGenerators
+import bifrost.blocks.BifrostBlock
 import io.iohk.iodb.ByteArrayWrapper
 import org.scalatest.{Matchers, PropSpec}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 class StorageCacheSpec extends PropSpec
+  with ScalaCheckPropertyChecks
   with Matchers
   with BifrostGenerators {
 
-  val history: BifrostHistory = generateHistory
+  var history: BifrostHistory = generateHistory
 
   property("The genesis block is stored in cache") {
 
@@ -17,4 +20,18 @@ class StorageCacheSpec extends PropSpec
 
     history.storage.blockCache.getIfPresent(genesisBlockId) shouldEqual history.storage.storage.get(genesisBlockId)
   }
+
+  property("The new block updated is stored in cache") {
+
+    forAll(bifrostBlockGen) { blockTemp =>
+
+      val block:BifrostBlock = blockTemp.copy(parentId = history.bestBlockId)
+
+      history = history.append(block).get._1
+
+      history.storage.blockCache.getIfPresent(ByteArrayWrapper(block.id)) shouldEqual
+        history.storage.storage.get(ByteArrayWrapper(block.id))
+    }
+  }
+
 }
