c5b44333ba64086238911b6e638eda71c55eb681
James Aman <jim.a.aman@gmail.com>
2020-04-09 15:47:10 -0500

    update documentation, #341

--- src/main/scala/bifrost/api/http/UtilsApiRoute.scala
+++ src/main/scala/bifrost/api/http/UtilsApiRoute.scala
@@ -17,8 +17,7 @@ import scorex.crypto.encode.Base58
 
 import scala.util.{Failure, Success, Try}
 
-case class UtilsApiRoute(override val settings: Settings)
-                        (implicit val context: ActorRefFactory) extends ApiRoute {
+case class UtilsApiRoute(override val settings: Settings)(implicit val context: ActorRefFactory) extends ApiRoute {
   type HIS = BifrostHistory
   type MS = BifrostState
   type VL = BWallet
@@ -46,14 +45,23 @@ case class UtilsApiRoute(override val settings: Settings)
                 require(params.size <= 5, s"size of params is ${params.size}")
 
                 (request \\ "method").head.asString.get match {
-                  case "seed" => seedRoute(params.head, id)
+                  case "seed"         => seedRoute(params.head, id)
                   case "seedOfLength" => seedOfLength(params.head, id)
-                  case "hashBlake2b" => hashBlake2b(params.head, id)
+                  case "hashBlake2b"  => hashBlake2b(params.head, id)
                 }
               }
               response match {
                 case Success(resp) => BifrostSuccessResponse(resp, reqId)
-                case Failure(e) => BifrostErrorResponse(e, 500, reqId, verbose = settings.settingsJSON.getOrElse("verboseAPI", false.asJson).asBoolean.get)
+                case Failure(e) =>
+                  BifrostErrorResponse(
+                    e,
+                    500,
+                    reqId,
+                    verbose = settings.settingsJSON
+                      .getOrElse("verboseAPI", false.asJson)
+                      .asBoolean
+                      .get
+                  )
               }
           }
         }
@@ -67,21 +75,61 @@ case class UtilsApiRoute(override val settings: Settings)
     Map("seed" -> Base58.encode(seed)).asJson
   }
 
-  //Generates random seed
+  /**  #### Summary
+    *    Generates random seed of 32 bytes
+    * 
+    * ---
+    *  #### Params
+    * 
+    *  | Fields                  	| Data type 	| Required / Optional 	| Description                                                            	|
+    *  |-------------------------	|-----------	|---------------------	|------------------------------------------------------------------------	|
+    *  | --None specified--       |           	|                     	|                                                                         |
+    *
+    * @param params input parameters as specified above
+    * @param id request identifier
+    * @return
+    */
   private def seedRoute(params: Json, id: String): Json = {
     seed(SeedSize)
   }
 
-  //Generates random seed of specified length
+  /**  #### Summary
+    *    Generates random seed of specified length
+    * 
+    * ---
+    *  #### Params
+    *  | Fields                  	| Data type 	| Required / Optional 	| Description                                                            	|
+    *  |-------------------------	|-----------	|---------------------	|------------------------------------------------------------------------	|
+    *  | length                   | Int        	| Required             	| The number of characters to return                                      |
+    *
+    * @param params input parameters as specified above
+    * @param id request identifier
+    * @return
+    */
   private def seedOfLength(params: Json, id: String): Json = {
     val length: Int = (params \\ "length").head.asNumber.get.toInt.get
     seed(length)
   }
 
-  //Returns Blake2b hash of specified message
+  /** 
+    *  #### Summary
+    *    Returns Blake2b hash of specified message
+    * 
+    * ---
+    *  #### Params
+    *  | Fields                  	| Data type 	| Required / Optional 	| Description                                                            	|
+    *  |-------------------------	|-----------	|---------------------	|------------------------------------------------------------------------	|
+    *  | message                  | String     	| Required             	| The message that will be hashed                                         |
+    *
+    * @param params input parameters as specified above
+    * @param id request identifier
+    * @return
+    */
   private def hashBlake2b(params: Json, id: String): Json = {
     val message: String = (params \\ "message").head.asString.get
-    Map("message" -> message,
-        "hash" -> Base58.encode(FastCryptographicHash(message))).asJson
+    Map(
+      "message" -> message,
+      "hash" -> Base58.encode(FastCryptographicHash(message))
+    ).asJson
   }
 }
