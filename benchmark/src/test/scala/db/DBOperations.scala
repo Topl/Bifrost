a3b0350983d2742eb0895320298202a14aa05508
jingsting <jingfei1024@gmail.com>
2020-04-24 16:22:50 -0500

    commented out println

--- benchmark/src/test/scala/db/DBOperations.scala
+++ benchmark/src/test/scala/db/DBOperations.scala
@@ -31,7 +31,7 @@ class DBOperations extends BifrostGenerators {
   val listBlockId: List[ByteArrayWrapper] = (for (i <- 1 to numOfBlocks) yield {
     val oneBlock: BifrostBlock = bifrostBlockGen.sample.get.copy(parentId = history.bestBlockId)
     history = history.append(oneBlock).get._1
-    println(s"forging====$i====${ByteArrayWrapper(oneBlock.id)}")
+    /* println(s"forging====$i====${ByteArrayWrapper(oneBlock.id)}") */
     ByteArrayWrapper(oneBlock.id)
   }).take(numLastBlocks).toList
 
@@ -85,56 +85,57 @@ class DBOperations extends BifrostGenerators {
 }
 
 
+/*
+package db
 
-//package db
-//
-//import java.io.File
-//import java.util.concurrent.TimeUnit
-//import java.util.concurrent.atomic.AtomicLong
-//
-//import org.openjdk.jmh.annotations._
-//import org.openjdk.jmh.infra.Blackhole
-//import bifrost.history._
-//import bifrost.forging.ForgingSettings
-//import io.circe.Json
-//import io.iohk.iodb.LSMStore
-//
-//
-//@OutputTimeUnit(TimeUnit.NANOSECONDS)
-//@BenchmarkMode(Array(Mode.AverageTime))
-//@Threads(1)
-//@Fork(1)
-//@Warmup(iterations = 10)
-//@Measurement(iterations = 10)
-//@State(Scope.Thread)
-//class DBOperations {
-//
-//  private val value = new AtomicLong()
-//
-//  @Benchmark
-//  def test(bh: Blackhole): Unit = {
-//    bh.consume(value.addAndGet(42))
-//  }
-//
-//  @Benchmark
-//  def readBlock(bh: Blackhole): Unit = {
-//
-//  }
-//}
-//
-//object DBOperations {
-//
-//
-//
-//  @State(Scope.Benchmark)
-//  class baseState {
-//
-//    //TODO Replace hardcoded dir with one from settings
-//    val settings: ForgingSettings = new ForgingSettings {
-//      override def settingsJSON: Map[String, Json] = settingsFromFile("bench.json")
-//    }
-//    val dataDir: File = new File(settings.dataDirOpt.get)
-//    println(settings.dataDirOpt.getOrElse("Could not find bench.json"))
-//    val blockStorage: LSMStore = new LSMStore(dataDir)
-//  }
-//}
+import java.io.File
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
+
+import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
+import bifrost.history._
+import bifrost.forging.ForgingSettings
+import io.circe.Json
+import io.iohk.iodb.LSMStore
+
+
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Array(Mode.AverageTime))
+@Threads(1)
+@Fork(1)
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@State(Scope.Thread)
+class DBOperations {
+
+  private val value = new AtomicLong()
+
+  @Benchmark
+  def test(bh: Blackhole): Unit = {
+    bh.consume(value.addAndGet(42))
+  }
+
+  @Benchmark
+  def readBlock(bh: Blackhole): Unit = {
+
+  }
+}
+
+object DBOperations {
+
+
+
+  @State(Scope.Benchmark)
+  class baseState {
+
+    //TODO Replace hardcoded dir with one from settings
+    val settings: ForgingSettings = new ForgingSettings {
+      override def settingsJSON: Map[String, Json] = settingsFromFile("bench.json")
+    }
+    val dataDir: File = new File(settings.dataDirOpt.get)
+    println(settings.dataDirOpt.getOrElse("Could not find bench.json"))
+    val blockStorage: LSMStore = new LSMStore(dataDir)
+  }
+}
+ */
