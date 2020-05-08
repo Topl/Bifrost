2db867787f95d3d9df8d84930499c2ca5bb5abd7
tuxman <n.edmonds@topl.me>
2020-05-08 15:38:03 -0500

    Delete AccountTransactionHistory #331

--- src/main/scala/bifrost/transaction/account/AccountTransactionsHistory.scala
+++
@@ -1,8 +0,0 @@
-package bifrost.transaction.account
-
-import bifrost.transaction.Transaction
-import bifrost.transaction.box.proposition.Proposition
-
-trait AccountTransactionsHistory[P <: Proposition, TX <: Transaction[P]] {
-  def accountTransactions(id: P): Array[TX]
-}
