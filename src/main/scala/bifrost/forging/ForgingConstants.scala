863999347ea51743dd3df282a8083e0f3053dbdf
tuxman <n.edmonds@topl.me>
2020-05-08 09:03:55 -0500

    Remove ForgingConstants #331

--- src/main/scala/bifrost/forging/ForgingConstants.scala
+++
@@ -1,9 +0,0 @@
-package bifrost.forging
-
-trait ForgingConstants {
-
-  lazy val MaxTarget = BigInt(1, Array.fill(32)(Byte.MinValue))
-  lazy val Difficulty = BigInt("50")
-
-  lazy val GenesisParentId: Array[Byte] = Array.fill(32)(1: Byte)
-}
