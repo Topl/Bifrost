11c2f6ce3181b185c65bf74c3a6e8c37044939bb
tuxman <n.edmonds@topl.me>
2020-05-09 15:07:06 -0500

    Delete StateChanges #331

--- src/main/scala/bifrost/transaction/state/StateChanges.scala
+++
@@ -1,6 +0,0 @@
-package bifrost.transaction.state
-
-import bifrost.transaction.box.Box
-import bifrost.transaction.box.proposition.Proposition
-
-case class StateChanges[P <: Proposition, BX <: Box[P]] (boxIdsToRemove: Set[Array[Byte]], toAppend: Set[BX])
