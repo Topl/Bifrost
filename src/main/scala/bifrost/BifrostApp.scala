d0aa784cc79502a5dc852061c70b1cb45711f07e
jingsting <jingfei1024@gmail.com>
2020-05-14 11:28:40 -0500

    Removed BifrostApp; Moved application name limit to application.conf

--- src/main/scala/bifrost/BifrostApp.scala
+++ src/main/scala/bifrost/BifrostApp.scala
@@ -2,7 +2,9 @@ package bifrost
 
 import java.lang.management.ManagementFactory
 
-import akka.actor.{ActorRef, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
 import bifrost.api.http.{ApiRoute, UtilsApiRoute, _}
 import bifrost.crypto.PrivateKey25519
 import bifrost.forging.{Forger, ForgingSettings}
@@ -10,41 +12,72 @@ import bifrost.modifier.block.Block
 import bifrost.modifier.box.BifrostBox
 import bifrost.modifier.box.proposition.ProofOfKnowledgeProposition
 import bifrost.modifier.transaction.bifrostTransaction.BifrostTransaction
-import bifrost.network.{BifrostLocalInterface, BifrostNodeViewSynchronizer, BifrostSyncInfoMessageSpec}
-import bifrost.network.message.MessageSpec
-import bifrost.scorexMod.GenericApplication
+import bifrost.network.{BifrostLocalInterface, BifrostNodeViewSynchronizer, BifrostSyncInfoMessageSpec, NetworkController, UPnP}
+import bifrost.network.message.{GetPeersSpec, InvSpec, MessageHandler, MessageSpec, ModifiersSpec, PeersSpec, RequestModifierSpec}
+import bifrost.network.peer.PeerManager
+import bifrost.utils.Logging
 import com.sun.management.HotSpotDiagnosticMXBean
 import com.typesafe.config.{Config, ConfigFactory}
 import io.circe
 import kamon.Kamon
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.reflect.runtime.universe._
 
-class BifrostApp(val settingsFilename: String) extends GenericApplication with Runnable {
+class BifrostApp(val settingsFilename: String) extends Logging with Runnable {
 
-  override type P = ProofOfKnowledgeProposition[PrivateKey25519]
-  override type BX = BifrostBox
-  override type TX = BifrostTransaction
-  override type PMOD = Block
-  override type NVHT = BifrostNodeViewHolder
+  type P = ProofOfKnowledgeProposition[PrivateKey25519]
+  type BX = BifrostBox
+  type TX = BifrostTransaction
+  type PMOD = Block
+  type NVHT = BifrostNodeViewHolder
+
+  private val conf: Config = ConfigFactory.load("application")
+  private val ApplicationNameLimit: Int = conf.getInt("app.applicationNameLimit")
 
   implicit lazy val settings = new ForgingSettings {
     override val settingsJSON: Map[String, circe.Json] = settingsFromFile(settingsFilename)
   }
   log.debug(s"Starting application with settings \n$settings")
 
-  override protected lazy val additionalMessageSpecs: Seq[MessageSpec[_]] =
+  /* networkController */
+  protected lazy val additionalMessageSpecs: Seq[MessageSpec[_]] =
     Seq(BifrostSyncInfoMessageSpec)
 
-  override val nodeViewHolderRef: ActorRef = actorSystem.actorOf(Props(new NVHT(settings)), "nodeViewHolder")
+  private lazy val basicSpecs =
+    Seq(
+      GetPeersSpec,
+      PeersSpec,
+      InvSpec,
+      RequestModifierSpec,
+      ModifiersSpec
+    )
 
+  lazy val messagesHandler: MessageHandler = MessageHandler(basicSpecs ++ additionalMessageSpecs)
+
+  lazy val upnp = new UPnP(settings)
+
+  protected implicit lazy val actorSystem = ActorSystem(settings.agentName)
+
+  val peerManagerRef = actorSystem.actorOf(Props(classOf[PeerManager], settings), "peerManager")
+
+  val nProps = Props(classOf[NetworkController], settings, messagesHandler, upnp, peerManagerRef)
+  val networkController = actorSystem.actorOf(nProps, "networkController")
+  /* ----------------- */
+
+  /* nodeViewHoderRef */
+  val nodeViewHolderRef: ActorRef = actorSystem.actorOf(Props(new NVHT(settings)), "nodeViewHolder")
+  /* ---------------- */
+
+  /* localInterface */
   val forger: ActorRef = actorSystem.actorOf(Props(classOf[Forger], settings, nodeViewHolderRef), "forger")
 
-  override val localInterface: ActorRef = actorSystem.actorOf(
+  val localInterface: ActorRef = actorSystem.actorOf(
     Props(classOf[BifrostLocalInterface], nodeViewHolderRef, forger, settings), "localInterface"
   )
+  /* -------------- */
 
-  override val nodeViewSynchronizer: ActorRef = actorSystem.actorOf(
+  val nodeViewSynchronizer: ActorRef = actorSystem.actorOf(
     Props(classOf[BifrostNodeViewSynchronizer],
       networkController,
       nodeViewHolderRef,
@@ -52,7 +85,7 @@ class BifrostApp(val settingsFilename: String) extends GenericApplication with R
       BifrostSyncInfoMessageSpec), "nodeViewSynchronizer"
   )
 
-  override val apiRoutes: Seq[ApiRoute] = Seq(
+  val apiRoutes: Seq[ApiRoute] = Seq(
     DebugApiRoute(settings, nodeViewHolderRef),
     WalletApiRoute(settings, nodeViewHolderRef),
     ProgramApiRoute(settings, nodeViewHolderRef, networkController),
@@ -63,24 +96,23 @@ class BifrostApp(val settingsFilename: String) extends GenericApplication with R
     NodeViewApiRoute(settings, nodeViewHolderRef)
   )
 
-  override val apiTypes: Seq[Type] = Seq(typeOf[UtilsApiRoute],
-                                         typeOf[DebugApiRoute],
-                                         typeOf[WalletApiRoute],
-                                         typeOf[ProgramApiRoute],
-                                         typeOf[AssetApiRoute],
-//                                         typeOf[GenericNodeViewApiRoute[P, TX]],
-//                                         typeOf[PeersApiRoute],
-                                         typeOf[NodeViewApiRoute])
+  val apiTypes: Seq[Type] = Seq(typeOf[UtilsApiRoute],
+    typeOf[DebugApiRoute],
+    typeOf[WalletApiRoute],
+    typeOf[ProgramApiRoute],
+    typeOf[AssetApiRoute],
+//    typeOf[GenericNodeViewApiRoute[P, TX]],
+//    typeOf[PeersApiRoute],
+    typeOf[NodeViewApiRoute])
 
-
+  lazy val combinedRoute = CompositeHttpService(actorSystem, apiTypes, apiRoutes, settings).compositeRoute
 
   // Am I running on a JDK that supports JVMCI?
   val vm_version = System.getProperty("java.vm.version")
-
   System.out.printf("java.vm.version = %s%n", vm_version)
 
-  val bean = ManagementFactory.getPlatformMXBean(classOf[HotSpotDiagnosticMXBean])
   // Is JVMCI enabled?
+  val bean = ManagementFactory.getPlatformMXBean(classOf[HotSpotDiagnosticMXBean])
   val enableJVMCI = bean.getVMOption("EnableJVMCI")
   System.out.println(enableJVMCI)
 
@@ -91,12 +123,43 @@ class BifrostApp(val settingsFilename: String) extends GenericApplication with R
   // What compiler is selected?
   val compiler = System.getProperty("jvmci.Compiler")
   System.out.printf("jvmci.Compiler = %s%n", compiler)
+
+  def run(): Unit = {
+    require(settings.agentName.length <= ApplicationNameLimit)
+
+    log.debug(s"Available processors: ${Runtime.getRuntime.availableProcessors}")
+    log.debug(s"Max memory available: ${Runtime.getRuntime.maxMemory}")
+    log.debug(s"RPC is allowed at 0.0.0.0:${settings.rpcPort}")
+
+    implicit val materializer = ActorMaterializer()
+    Http().bindAndHandle(combinedRoute, "0.0.0.0", settings.rpcPort)
+
+    // on unexpected shutdown
+    Runtime.getRuntime.addShutdownHook(new Thread() {
+      override def run() {
+        log.error("Unexpected shutdown")
+        stopAll()
+      }
+    })
+  }
+
+  def stopAll(): Unit = synchronized {
+    log.info("Stopping network services")
+    if (settings.upnpEnabled) upnp.deletePort(settings.port)
+    networkController ! NetworkController.ShutdownNetwork
+
+    log.info("Stopping actors (incl. block generator)")
+
+    actorSystem.terminate().onComplete { _ =>
+      log.info("Exiting from the app...")
+      System.exit(0)
+    }
+  }
 }
 
 object BifrostApp extends App {
   private val conf: Config = ConfigFactory.load("application")
-  if (conf.getBoolean("kamon.enable"))
-    Kamon.init()
+  if (conf.getBoolean("kamon.enable")) Kamon.init()
   val settingsFilename = args.headOption.getOrElse("testnet-private.json")
   new BifrostApp(settingsFilename).run()
 }
