7c43a36609de2aa214b3cdd970d2ce51bbdf9271
jingsting <jingfei1024@gmail.com>
2020-05-20 09:54:09 -0500

    Updated imports: org.scalatest.prop is depricated, updating to org.scalatestplus.scalacheck

--- src/test/scala/bifrost/transaction/TransactionSpec.scala
+++ src/test/scala/bifrost/transaction/TransactionSpec.scala
@@ -1,12 +1,13 @@
 package bifrost.transaction
 
 import bifrost.{BifrostGenerators, ValidGenerators}
-import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
+import org.scalatestplus.scalacheck.{ScalaCheckPropertyChecks, ScalaCheckDrivenPropertyChecks}
+
 
 class TransactionSpec extends PropSpec
-  with PropertyChecks
-  with GeneratorDrivenPropertyChecks
+  with ScalaCheckPropertyChecks
+  with ScalaCheckDrivenPropertyChecks
   with Matchers
   with BifrostGenerators
   with ValidGenerators {
