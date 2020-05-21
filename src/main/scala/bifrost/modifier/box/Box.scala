41d9a19d2b25f2e47af87edd0823d1a2a5d574fe
tuxman <n.edmonds@topl.me>
2020-05-21 17:49:59 -0500

    Split AssetBox from Box #350

--- src/main/scala/bifrost/modifier/box/Box.scala
+++ src/main/scala/bifrost/modifier/box/Box.scala
@@ -69,57 +69,7 @@ object BoxSerializer extends Serializer[Box] {
   }
 }
 
-case class AssetBox(override val proposition: PublicKey25519Proposition,
-                    override val nonce: Long,
-                    amount: Long,
-                    assetCode: String,
-                    issuer: PublicKey25519Proposition,
-                    data: String) extends NoncedBox(proposition, nonce, amount) {
-  override lazy val typeOfBox: String = "Asset"
 
-  override lazy val json: Json = Map(
-    "id" -> Base58.encode(id).asJson,
-    "type" -> typeOfBox.asJson,
-    "proposition" -> Base58.encode(proposition.pubKeyBytes).asJson,
-    "assetCode" -> assetCode.asJson,
-    "value" -> value.toString.asJson,
-    "issuer" -> Base58.encode(issuer.pubKeyBytes).asJson,
-    "data" -> data.asJson,
-    "nonce" -> nonce.toString.asJson
-  ).asJson
-}
-
-object AssetBoxSerializer extends Serializer[AssetBox] with NoncedBoxSerializer {
-
-  def toBytes(obj: AssetBox): Array[Byte] = {
-    noncedBoxToBytes(obj, "AssetBox") ++
-      obj.issuer.pubKeyBytes ++
-      obj.assetCode.getBytes ++
-      Ints.toByteArray(obj.assetCode.getBytes.length) ++
-      obj.data.getBytes ++
-      Ints.toByteArray(obj.data.getBytes.length)
-  }
-
-  override def parseBytes(bytes: Array[Byte]): Try[AssetBox] = Try {
-
-    val params = noncedBoxParseBytes(bytes)
-
-    val dataLen = Ints.fromByteArray(bytes.slice(bytes.length - Ints.BYTES, bytes.length))
-    val data: String = new String(bytes.slice(bytes.length - Ints.BYTES - dataLen, bytes.length - Ints.BYTES))
-
-    val assetLen = Ints.fromByteArray(bytes.slice(bytes.length - Ints.BYTES - Ints.BYTES - dataLen,
-      bytes.length - Ints.BYTES - dataLen))
-    val asset: String = new String(bytes.slice(bytes.length - (2 * Ints.BYTES) - dataLen - assetLen,
-      bytes.length - (2 * Ints.BYTES) - dataLen))
-
-    val issuer: PublicKey25519Proposition = PublicKey25519Proposition(
-      bytes.slice(bytes.length - (2 * Ints.BYTES) - dataLen - assetLen - Constants25519.PubKeyLength,
-                  bytes.length - (2 * Ints.BYTES) - dataLen - assetLen)
-    )
-
-    AssetBox(params._1, params._2, params._3, asset, issuer, data)
-  }
-}
 
 case class StateBox(override val proposition: PublicKey25519Proposition,
                             override val nonce: Long,
