76682c55c57a7148b619e1363220a3c3a1a52a7b
tuxman <n.edmonds@topl.me>
2020-05-21 17:48:24 -0500

    Split ArbitBox from Box #350

--- src/main/scala/bifrost/modifier/box/Box.scala
+++ src/main/scala/bifrost/modifier/box/Box.scala
@@ -69,25 +69,6 @@ object BoxSerializer extends Serializer[Box] {
   }
 }
 
-case class ArbitBox(override val proposition: PublicKey25519Proposition,
-                    override val nonce: Long,
-                    override val value: Long) extends NoncedBox(proposition, nonce, value) {
-  override lazy val typeOfBox: String = "Arbit"
-}
-
-object ArbitBoxSerializer extends Serializer[ArbitBox] with NoncedBoxSerializer {
-
-  def toBytes(obj: ArbitBox): Array[Byte] = {
-    noncedBoxToBytes(obj, "ArbitBox")
-  }
-
-  override def parseBytes(bytes: Array[Byte]): Try[ArbitBox] = Try {
-    val params = noncedBoxParseBytes(bytes)
-    ArbitBox(params._1, params._2, params._3)
-  }
-
-}
-
 case class AssetBox(override val proposition: PublicKey25519Proposition,
                     override val nonce: Long,
                     amount: Long,
