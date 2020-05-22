a431f553a2021ba00963923182fd207ad9f120fb
jingsting <jingfei1024@gmail.com>
2020-05-22 09:32:16 -0500

    Switched to using JavaConvertes in UPnP

--- src/main/scala/bifrost/network/UPnP.scala
+++ src/main/scala/bifrost/network/UPnP.scala
@@ -6,7 +6,7 @@ import bifrost.settings.Settings
 import bifrost.utils.Logging
 import org.bitlet.weupnp.{GatewayDevice, GatewayDiscover}
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import scala.util.Try
 
 class UPnP(settings: Settings) extends Logging {
@@ -24,7 +24,7 @@ class UPnP(settings: Settings) extends Logging {
     val defaultDiscoverTimeout = settings.upnpDiscoverTimeout.getOrElse(discover.getTimeout)
     discover.setTimeout(defaultDiscoverTimeout)
 
-    val gatewayMap = Option(discover.discover).map(_.toMap).getOrElse(Map())
+    val gatewayMap = Option(discover.discover).map(_.asScala).map(_.toMap).getOrElse(Map())
     if (gatewayMap.isEmpty) {
       log.debug("There are no UPnP gateway devices")
     } else {
