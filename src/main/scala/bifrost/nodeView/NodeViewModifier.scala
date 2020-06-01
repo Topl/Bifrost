c68e240a36f6983ded8c677c8f7004e534cf7887
tuxman <n.edmonds@topl.me>
2020-06-01 13:53:41 -0500

    Add ModifierId and helper functions to NodeViewModifier #392

--- src/main/scala/bifrost/nodeView/NodeViewModifier.scala
+++ src/main/scala/bifrost/nodeView/NodeViewModifier.scala
@@ -2,8 +2,13 @@ package bifrost.nodeView
 
 import bifrost.modifier.box.proposition.Proposition
 import bifrost.modifier.transaction.bifrostTransaction.GenericTransaction
+import bifrost.network.message.InvData
 import bifrost.nodeView.NodeViewModifier.ModifierId
 import bifrost.serialization.{BytesSerializable, JsonSerializable}
+import bifrost.state.MinimalState
+import bifrost.state.MinimalState.VersionTag
+import bifrost.utils.BifrostEncoder
+import bifrost.utils.encode.Base16
 import com.typesafe.config.ConfigFactory
 import scorex.crypto.encode.Base58
 import supertagged.TaggedType
@@ -33,11 +38,29 @@ object NodeViewModifier {
 
   //TODO implement ModifierTypeId as a trait
   object ModifierTypeId extends TaggedType[Byte]
-  object ModifierId extends TaggedType[String]
   type ModifierTypeId = ModifierTypeId.Type
-  type ModifierId = Array[Byte]
+
+  object ModifierId extends TaggedType[String]
+  type ModifierId = ModifierId.Type
 
   val ModifierIdSize: Int = Try(ConfigFactory.load().getConfig("app").getInt("modifierIdSize")).getOrElse(DefaultIdSize)
+
+  def idsToString(ids: Seq[(ModifierTypeId, ModifierId)])(implicit enc: BifrostEncoder): String = {
+    List(ids.headOption, ids.lastOption)
+      .flatten
+      .map { case (typeId, id) => s"($typeId,${enc.encodeId(id)})" }
+      .mkString("[", "..", "]")
+  }
+
+  def idsToString(modifierType: ModifierTypeId, ids: Seq[ModifierId])(implicit encoder: BifrostEncoder): String = {
+    idsToString(ids.map(id => (modifierType, id)))
+  }
+
+  def idsToString(invData: InvData)(implicit encoder: BifrostEncoder): String = idsToString(invData.typeId, invData.ids)
+
+  def bytesToId: Array[Byte] => ModifierId = bifrost.utils.bytesToId
+
+  def idToBytes: ModifierId => Array[Byte] = bifrost.utils.idToBytes
 }
 
 
