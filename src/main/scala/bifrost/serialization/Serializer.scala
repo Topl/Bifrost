537866ad58e76e223b5c949593a41d0d88c0e92e
tuxman <n.edmonds@topl.me>
2020-06-01 19:07:51 -0500

    Reduce Serializer to smaller trait #392

--- src/main/scala/bifrost/serialization/Serializer.scala
+++ src/main/scala/bifrost/serialization/Serializer.scala
@@ -1,10 +1,6 @@
 package bifrost.serialization
 
-import java.nio.ByteBuffer
-
-import akka.util.ByteString
-import bifrost.utils.ByteArrayBuilder
-import bifrost.utils.serialization._
+import bifrost.utils.serialization.{Reader, Writer}
 
 import scala.util.Try
 
@@ -16,35 +12,5 @@ trait Serializer[TFamily, T <: TFamily, R <: Reader, W <: Writer] {
 
   def parseTry(r: R): Try[TFamily] = {
     Try(parse(r))
-  }
-
-  def toByteString(obj: T): ByteString = {
-    val writer = new VLQByteStringWriter()
-    serialize(obj, writer)
-    writer.result()
-  }
-
-  def parseByteString(byteString: ByteString): T = {
-    val reader = new VLQByteStringReader(byteString)
-    parse(reader)
-  }
-
-  def parseByteStringTry(byteString: ByteString): Try[T] = {
-    Try(parseByteString(byteString))
-  }
-
-  def toBytes(obj: T): Array[Byte] = {
-    val writer = new VLQByteBufferWriter(new ByteArrayBuilder())
-    serialize(obj, writer)
-    writer.result().toBytes
-  }
-
-  def parseBytes(bytes: Array[Byte]): T = {
-    val reader = new VLQByteBufferReader(ByteBuffer.wrap(bytes))
-    parse(reader)
-  }
-
-  def parseBytesTry(bytes: Array[Byte]): Try[T] = {
-    Try(parseBytes(bytes))
   }
 }
