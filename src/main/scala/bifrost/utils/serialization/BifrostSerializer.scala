4079d217fa7f6d01e60c8724aaf396ad4e2e933a
jingsting <jingfei1024@gmail.com>
2020-06-09 17:39:42 -0500

    Reverted serialize/parse the to how we got them from scorex

--- src/main/scala/bifrost/utils/serialization/BifrostSerializer.scala
+++ src/main/scala/bifrost/utils/serialization/BifrostSerializer.scala
@@ -11,13 +11,13 @@ trait BifrostSerializer[T] extends Serializer[T, T, Reader, Writer] {
 
   def toByteString(obj: T): ByteString = {
     val writer = new VLQByteStringWriter()
-    toBytes(obj, writer)
+    serialize(obj, writer)
     writer.result()
   }
 
   def parseByteString(byteString: ByteString): T = {
     val reader = new VLQByteStringReader(byteString)
-    parseBytes(reader)
+    parse(reader)
   }
 
   def parseByteStringTry(byteString: ByteString): Try[T] = {
@@ -26,12 +26,12 @@ trait BifrostSerializer[T] extends Serializer[T, T, Reader, Writer] {
 
   def toBytes(obj: T): Array[Byte] = {
     val writer = new VLQByteBufferWriter(new ByteArrayBuilder())
-    toBytes(obj, writer)
+    serialize(obj, writer)
     writer.result().toBytes
   }
 
   def parseBytes(bytes: Array[Byte]): Try[T] = {
     val reader = new VLQByteBufferReader(ByteBuffer.wrap(bytes))
-    Try(parseBytes(reader))
+    Try(parse(reader))
   }
 }
