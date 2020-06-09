229cecb9fe2700d520c166d4c75d810df429984c
jingsting <jingfei1024@gmail.com>
2020-06-09 14:16:03 -0500

    Removed parseBytesTry

--- src/main/scala/bifrost/utils/serialization/Serializer.scala
+++ src/main/scala/bifrost/utils/serialization/Serializer.scala
@@ -1,14 +1,8 @@
 package bifrost.utils.serialization
 
-import scala.util.Try
-
 trait Serializer[TFamily, T <: TFamily, R <: Reader, W <: Writer] {
 
-  def serialize(obj: T, w: W): Unit
+  def toBytes(obj: T, w: W): Unit
 
-  def parse(r: R): TFamily
-
-  def parseTry(r: R): Try[TFamily] = {
-    Try(parse(r))
-  }
+  def parseBytes(r: R): TFamily
 }
