d17dbf24c6841a92e1a487158e91ca9cd0b9baec
tuxman <n.edmonds@topl.me>
2020-06-11 17:03:20 -0500

    Remove unused utils methods #418

--- src/main/scala/bifrost/utils/utils.scala
+++ src/main/scala/bifrost/utils/utils.scala
@@ -8,12 +8,4 @@ package object utils {
   def bytesToId(bytes: Array[Byte]): ModifierId = ModifierId @@ Base16.encode(bytes)
 
   def idToBytes(id: ModifierId): Array[Byte] = Base16.decode(id).get
-
-  implicit class ModifierIdOps(val m: ModifierId) extends AnyVal {
-    @inline def toBytes: Array[Byte] = idToBytes(m)
-  }
-
-  implicit class ByteArrayOps(val b: Array[Byte]) extends AnyVal  {
-    @inline def toModifierId: ModifierId = bytesToId(b)
-  }
 }
