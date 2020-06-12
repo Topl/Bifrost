b396e9a8cf6fd84441c1614d89c00430e5e03821
tuxman <n.edmonds@topl.me>
2020-06-12 10:38:56 -0500

    Delete Bas16 #419

--- src/main/scala/bifrost/utils/utils.scala
+++ src/main/scala/bifrost/utils/utils.scala
@@ -1,14 +1,13 @@
 package bifrost
 
 import bifrost.modifier.ModifierId
-import bifrost.utils.encode.Base16
 import scorex.crypto.encode.Base58
 
 package object utils {
 
   def bytesToId(bytes: Array[Byte]): ModifierId = ModifierId(bytes)
 
-  def idToBytes(id: ModifierId): Array[Byte] = Base16.decode(id).get
+  def idToBytes(id: ModifierId): Array[Byte] = Base58.decode(id).get
   
   def idToString(id: ModifierId): String = Base58.encode(id.hashBytes)
 }
