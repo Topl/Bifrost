cd9a3021e7feaa0498f5479ed7de0e5db8eb352d
jingsting <jingfei1024@gmail.com>
2020-06-17 15:57:02 -0500

    Added serialized and parse without implementation

--- src/main/scala/bifrost/wallet/WalletBoxSerializer.scala
+++ src/main/scala/bifrost/wallet/WalletBoxSerializer.scala
@@ -4,13 +4,17 @@ import bifrost.modifier.ModifierId
 import bifrost.modifier.box.GenericBox
 import bifrost.modifier.box.proposition.Proposition
 import bifrost.nodeView.NodeViewModifier
-import bifrost.utils.serialization.BifrostSerializer
+import bifrost.utils.serialization.{BifrostSerializer, Reader, Writer}
 import com.google.common.primitives.{Bytes, Longs}
 
 import scala.util.Try
 
 class WalletBoxSerializer[T, P <: Proposition, B <: GenericBox[P, T]](subclassDeser: BifrostSerializer[B])
   extends BifrostSerializer[WalletBox[T, P, B]] {
+
+  override def serialize(obj: WalletBox[T, P, B], w: Writer): Unit = ???
+
+  override def parse(r: Reader): WalletBox[T, P, B] = ???
 
   override def toBytes(box: WalletBox[T, P, B]): Array[Byte] = {
     Bytes.concat(box.transactionId.hashBytes, Longs.toByteArray(box.createdAt), box.box.bytes)
