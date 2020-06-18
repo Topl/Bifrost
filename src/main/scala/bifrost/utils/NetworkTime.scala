167e0fca2f2f08bb25c1e6bb2f6f74d4d010593c
tuxman <n.edmonds@topl.me>
2020-06-18 14:08:31 -0500

    Scope private var to fix warning about var never updated #432

--- src/main/scala/bifrost/utils/NetworkTime.scala
+++ src/main/scala/bifrost/utils/NetworkTime.scala
@@ -20,7 +20,7 @@ class NetworkTimeProvider(ntpSettings: NetworkTimeProviderSettings)(implicit ec:
   extends TimeProvider with Logging {
 
   private val lastUpdate = new AtomicLong(0)
-  private var offset = new AtomicLong(0)
+  private[NetworkTime] var offset = new AtomicLong(0)
   private val client = new NTPUDPClient()
   client.setDefaultTimeout(ntpSettings.timeout.toMillis.toInt)
   client.open()
