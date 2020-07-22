8cc8dfc65e45c59d0e619e8cf7db05d7a0fdbf4e
ngjulia <jen67@cornell.edu>
2020-07-21 21:49:23 -0400

    fixed import issues, added necessary dependencies

---
+++ gjallarhorn/src/main/scala/example/serialization/BytesSerializable.scala
@@ -0,0 +1,10 @@
+package example.serialization
+
+trait BytesSerializable extends Serializable {
+
+  type M >: this.type <: BytesSerializable
+
+  lazy val bytes: Array[Byte] = serializer.toBytes(this)
+
+  def serializer: Serializer[M]
+}
