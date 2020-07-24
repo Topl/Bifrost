ebe1464b9b9a7ec5f5488763fad8567908a0e543
ngjulia <jen67@cornell.edu>
2020-07-23 22:49:02 -0400

    key generation tests

---
+++ gjallarhorn/src/test/scala/example/KeyManagerSpec.scala
@@ -0,0 +1,31 @@
+package example
+
+import org.scalatest.{Matchers, WordSpec}
+import scorex.crypto.hash.Blake2b256
+
+class KeyManagerSpec extends WordSpec with Matchers{
+  "A message" should {
+    "match its signature to the expected private key" in {
+
+    }
+  }
+
+  "Generating keys" should {
+    "come as a private key and public key pair" in {
+      val randomBytes = Blake2b256(java.util.UUID.randomUUID.toString)
+      val (sk, pk) = PrivateKey25519Companion.generateKeys(randomBytes)
+      assert(sk != null)
+      assert(pk != null)
+      assert(sk.isInstanceOf[PrivateKey25519])
+      assert(pk.isInstanceOf[PublicKey25519Proposition])
+    }
+    "be deterministic" in {
+      val randomBytes = Blake2b256(java.util.UUID.randomUUID.toString)
+      val (sk1, pk1) = PrivateKey25519Companion.generateKeys(randomBytes)
+      val (sk2, pk2) = PrivateKey25519Companion.generateKeys(randomBytes)
+      assert(sk1.privKeyBytes === sk2.privKeyBytes)
+      assert(pk1.equals(pk2))
+    }
+  }
+
+}
