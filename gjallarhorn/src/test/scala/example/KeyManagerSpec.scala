a92579fb5a4b6489f4148a60ed2b367b2452aca8
ngjulia <jen67@cornell.edu>
2020-08-05 20:53:20 -0400

    reusing keys fix

--- gjallarhorn/src/test/scala/example/KeyManagerSpec.scala
+++ gjallarhorn/src/test/scala/example/KeyManagerSpec.scala
@@ -9,70 +9,53 @@ import scala.reflect.io.Path
 import scala.util.Try
 
 class KeyManagerSpec extends WordSpec with Matchers{
+  val randomBytes1 = Blake2b256(java.util.UUID.randomUUID.toString)
+  val randomBytes2 = Blake2b256(java.util.UUID.randomUUID.toString)
+  val (sk1, pk1) = PrivateKey25519Companion.generateKeys(randomBytes1)
+  val (sk2, pk2) = PrivateKey25519Companion.generateKeys(randomBytes2)
+  val (sk3, pk3) =  PrivateKey25519Companion.generateKeys(Blake2b256("keyBytes"))
+  val (sk4, pk4) =  PrivateKey25519Companion.generateKeys(randomBytes1)
+
   "A signed message" should {
+    val messageBytes = Blake2b256("messageBytes")
+    val messageToSign = Blake2b256(java.util.UUID.randomUUID.toString)
+
     "match its signature to the expected private key" in {
       //Entropic input to input for pub/priv keypair
-      val randomBytes = Blake2b256(java.util.UUID.randomUUID.toString)
-      val (sk, pk) = PrivateKey25519Companion.generateKeys(randomBytes)
-
-      val messageToSign = Blake2b256(java.util.UUID.randomUUID.toString)
-      val proof = PrivateKey25519Companion.sign(sk,messageToSign)
-      assert(PrivateKey25519Companion.verify(messageToSign, pk, proof))
+      val proof = PrivateKey25519Companion.sign(sk1,messageToSign)
+      assert(PrivateKey25519Companion.verify(messageToSign, pk1, proof))
     }
     "yield an invalid signature if signed with incorrect foreign key" in {
-      val randomBytes1 = Blake2b256(java.util.UUID.randomUUID.toString)
-      val randomBytes2 = Blake2b256(java.util.UUID.randomUUID.toString)
-      val (sk1, _) = PrivateKey25519Companion.generateKeys(randomBytes1)
-      val (_, pk2) = PrivateKey25519Companion.generateKeys(randomBytes2)
-
-      val messageToSign = Blake2b256(java.util.UUID.randomUUID.toString)
       val proof = PrivateKey25519Companion.sign(sk1,messageToSign)
       assert(!PrivateKey25519Companion.verify(messageToSign, pk2, proof))
     }
     "sign and verify as expected when msg is deterministic" in {
-      val (sk, pk) =  PrivateKey25519Companion.generateKeys(Blake2b256("keyBytes"))
-      val messageBytes = Blake2b256("messageBytes")
-
-      val proof = PrivateKey25519Companion.sign(sk,messageBytes)
-      assert(PrivateKey25519Companion.verify(messageBytes, pk, proof))
-
+      val proof = PrivateKey25519Companion.sign(sk3,messageBytes)
+      assert(PrivateKey25519Companion.verify(messageBytes, pk3, proof))
     }
   }
 
   "Generating keys" should {
     "come as a private key and public key pair" in {
       //Entropic input used to make pub/priv keypair
-      val randomBytes = Blake2b256(java.util.UUID.randomUUID.toString)
-      val (sk, pk) = PrivateKey25519Companion.generateKeys(randomBytes)
-      assert(sk != null)
-      assert(pk != null)
-      assert(sk.isInstanceOf[PrivateKey25519])
-      assert(pk.isInstanceOf[PublicKey25519Proposition])
+      assert(sk1 != null)
+      assert(pk1 != null)
+      assert(sk1.isInstanceOf[PrivateKey25519])
+      assert(pk1.isInstanceOf[PublicKey25519Proposition])
     }
 
     "be deterministic" in {
-      val randomBytes = Blake2b256(java.util.UUID.randomUUID.toString)
-      val (sk1, pk1) = PrivateKey25519Companion.generateKeys(randomBytes)
-      val (sk2, pk2) = PrivateKey25519Companion.generateKeys(randomBytes)
-
       //Used the same entropic input
-      assert(sk1.privKeyBytes === sk2.privKeyBytes)
-      assert(pk1.equals(pk2))
+      assert(sk1.privKeyBytes === sk4.privKeyBytes)
+      assert(pk1.equals(pk4))
     }
 
     "have sufficient randomness in entropic keypair gen" in {
-      //Assumption: This method call is sufficiently random so no collision
-      val randomBytesALICE = Blake2b256(java.util.UUID.randomUUID.toString)
-      val randomBytesBOB = Blake2b256(java.util.UUID.randomUUID.toString)
-
       //Valid pretest to ensure sufficiently random input so no shared keys
-      assert(randomBytesALICE != randomBytesBOB)
+      assert(randomBytes1 != randomBytes2)
 
-      val (skALICE, pkALICE) = PrivateKey25519Companion.generateKeys(randomBytesALICE)
-      val (skBOB, pkBOB) = PrivateKey25519Companion.generateKeys(randomBytesBOB)
-
-      assert(skALICE != null && pkALICE != null && skALICE.isInstanceOf[PrivateKey25519] && pkALICE.isInstanceOf[PublicKey25519Proposition])
-      assert(skBOB != null && pkBOB != null && skBOB.isInstanceOf[PrivateKey25519] && pkBOB.isInstanceOf[PublicKey25519Proposition])
+      assert(sk1 != null && pk1 != null && sk1.isInstanceOf[PrivateKey25519] && pk1.isInstanceOf[PublicKey25519Proposition])
+      assert(sk2 != null && pk2 != null && sk2.isInstanceOf[PrivateKey25519] && pk2.isInstanceOf[PublicKey25519Proposition])
     }
   }
 
