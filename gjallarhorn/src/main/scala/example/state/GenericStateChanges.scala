1a785d79bbe7e53d5e699a1dc4a68cd270ae975c
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-08-12 01:39:52 -0700

    Issue #485: Copied wallet files and relevant dependencies and helper files locally within Gjallahorn folder. Combed through codebase to understand how wallet transactions worked.

---
+++ gjallarhorn/src/main/scala/example/state/GenericStateChanges.scala
@@ -0,0 +1,7 @@
+package bifrost.state
+
+import bifrost.modifier.box.GenericBox
+import bifrost.modifier.box.proposition.Proposition
+
+class GenericStateChanges[T, P <: Proposition, BX <: GenericBox[P, T]](val boxIdsToRemove: Set[Array[Byte]], val toAppend: Set[BX])
+
