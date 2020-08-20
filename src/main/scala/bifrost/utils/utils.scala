d40674cdaec6215565837e2a6b6dbe0e31efdda3
jingsting <jingfei1024@gmail.com>
2020-08-20 16:58:42 -0500

    We should get rid of the 3 util methods here

--- src/main/scala/bifrost/utils/utils.scala
+++ src/main/scala/bifrost/utils/utils.scala
@@ -2,6 +2,7 @@ package bifrost
 
 import bifrost.modifier.ModifierId
 
+// TODO: Jing - This is no longer needed, we could use ModifierId.hashBytes instead
 package object utils {
 
   def bytesToId(bytes: Array[Byte]): ModifierId = ModifierId(bytes)
