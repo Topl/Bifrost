95a8e78387f73ef3b4e765560f7a1ff9a6a4c6bc
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-08-20 11:55:30 -0700

    Removed HelloSpec.scala

--- gjallarhorn/src/test/scala/example/HelloSpec.scala
+++
@@ -1,9 +0,0 @@
-package example
-
-import org.scalatest.{FlatSpec, Matchers}
-
-class HelloSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
-  }
-}
