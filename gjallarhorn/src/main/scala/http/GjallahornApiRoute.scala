2f32e9a387d03c00024dc61593570e8c4ce3c392
ngjulia <jen67@cornell.edu>
2020-08-26 01:04:01 -0400

    general template for HTTP server

---
+++ gjallarhorn/src/main/scala/http/GjallahornApiRoute.scala
@@ -0,0 +1,65 @@
+package http
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+import requests.Requests
+import io.circe.Json
+
+import scala.concurrent.Future
+
+class GjallahornApiRoute {
+
+  implicit val actorsystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val serverSource = Http().newServerAt("localhost", 9086).connectionSource()
+
+  val r = new Requests
+
+  def handlers(method: String, params: Vector[Json], id: String): Future[Json] =
+    method match {
+      case "transaction" => r.transaction(params.head, id)
+      case "signTx" => r.signTx(params.head, id)
+      case "broadcastTx" => r.broadcastTx(params.head, id)
+    }
+
+  val requestHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(GET, Uri.Path("/transaction"), _, _, _) =>
+      HttpResponse(entity = HttpEntity(
+        ContentTypes.`text/html(UTF-8)`,
+        "<html><body>Hello world!</body></html>"))
+
+    case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+      HttpResponse(entity = "PONG!")
+
+    case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
+      sys.error("BOOM!")
+
+    case r: HttpRequest =>
+      r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+      HttpResponse(404, entity = "Unknown resource!")
+  }
+
+}
+
+/*
+  transaction
+
+  broadcast
+
+  sign
+
+  postJSON route
+
+  API response.scala
+
+  basicRoute --> check dev branch - in ApiRoute
+}
+routes
+
+need a handler (look at AssetAPI Route)
+ */
