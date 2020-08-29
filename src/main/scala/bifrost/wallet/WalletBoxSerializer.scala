9416958fa1a887f4790c338a715e27251284c760
jingsting <jingfei1024@gmail.com>
2020-08-28 19:50:01 -0500

    Serialization for WalletBox

--- src/main/scala/bifrost/wallet/WalletBoxSerializer.scala
+++ src/main/scala/bifrost/wallet/WalletBoxSerializer.scala
@@ -12,20 +12,38 @@ import scala.util.Try
 class WalletBoxSerializer[T, P <: Proposition, B <: GenericBox[P, T]](subclassDeser: BifrostSerializer[B])
   extends BifrostSerializer[WalletBox[T, P, B]] {
 
-  override def serialize(obj: WalletBox[T, P, B], w: Writer): Unit = ???
+  override def serialize(obj: WalletBox[T, P, B], w: Writer): Unit = {
+    /* box: B */
+    subclassDeser.serialize(obj.box, w)
 
-  override def parse(r: Reader): WalletBox[T, P, B] = ???
+    /* transactionId: ModifierId */
+    w.putBytes(obj.transactionId.hashBytes)
 
-  override def toBytes(box: WalletBox[T, P, B]): Array[Byte] = {
-    Bytes.concat(box.transactionId.hashBytes, Longs.toByteArray(box.createdAt), box.box.bytes)
+    /* createdAt: Long */
+    w.putULong(obj.createdAt)
   }
 
-  override def parseBytes(bytes: Array[Byte]): Try[WalletBox[T, P, B]] = Try {
-    val txId = ModifierId(bytes.slice(0, NodeViewModifier.ModifierIdSize))
-    val createdAt = Longs.fromByteArray(
-      bytes.slice(NodeViewModifier.ModifierIdSize, NodeViewModifier.ModifierIdSize + 8))
-    val boxB = bytes.slice(NodeViewModifier.ModifierIdSize + 8, bytes.length)
-    val box: B = subclassDeser.parseBytes(boxB).get
-    WalletBox[T, P, B](box, txId, createdAt)(subclassDeser)
+  override def parse(r: Reader): WalletBox[T, P, B] = {
+    val box: B = subclassDeser.parse(r)
+    val transactionId: ModifierId = ModifierId(r.getBytes(NodeViewModifier.ModifierIdSize))
+    val createdAt: Long = r.getULong()
+
+    WalletBox[T, P, B](box, transactionId, createdAt)(subclassDeser)
   }
+
+
+// TODO: Jing - remove
+//
+//  override def toBytes(box: WalletBox[T, P, B]): Array[Byte] = {
+//    Bytes.concat(box.transactionId.hashBytes, Longs.toByteArray(box.createdAt), box.box.bytes)
+//  }
+//
+//  override def parseBytes(bytes: Array[Byte]): Try[WalletBox[T, P, B]] = Try {
+//    val txId = ModifierId(bytes.slice(0, NodeViewModifier.ModifierIdSize))
+//    val createdAt = Longs.fromByteArray(
+//      bytes.slice(NodeViewModifier.ModifierIdSize, NodeViewModifier.ModifierIdSize + 8))
+//    val boxB = bytes.slice(NodeViewModifier.ModifierIdSize + 8, bytes.length)
+//    val box: B = subclassDeser.parseBytes(boxB).get
+//    WalletBox[T, P, B](box, txId, createdAt)(subclassDeser)
+//  }
 }
