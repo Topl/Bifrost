b049e671c55ca3d6f69b9fd4dc4edc7948a00ea5
ngjulia <jen67@cornell.edu>
2020-08-31 14:20:11 -0400

    fixed executionbuilderterms

--- src/main/scala/bifrost/program/ExecutionBuilderTerms.scala
+++ src/main/scala/bifrost/program/ExecutionBuilderTerms.scala
@@ -1,21 +1,23 @@
 package bifrost.program
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.coding.Gzip
+import akka.http.scaladsl.coding.{Coders, Gzip}
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
 import io.circe.syntax._
 import io.circe.{Decoder, HCursor, Json}
 import scorex.crypto.encode.Base64
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 case class ExecutionBuilderTerms(terms: String){
+  implicit val actorsystem = ActorSystem()
+  implicit val materializer = ActorMaterializer()
 
   require(terms.length < 16*1024)
   lazy val json: Json = if(terms.length > 1024) {
-    s"gzip:${Base64.encode(Gzip.encode(ByteString(terms.getBytes)).toArray[Byte])}".asJson
+    s"gzip:${Base64.encode(Await.result(Coders.Gzip.encodeAsync(ByteString(terms.getBytes)), 10 seconds).toArray[Byte])}".asJson
   } else {
     terms.asJson
   }
@@ -29,16 +31,15 @@ object ExecutionBuilderTerms {
   implicit val materializer = ActorMaterializer()
 
   def decodeGzip(zipped: String): Future[ByteString] = {
-    Gzip.decode(ByteString(Base64.decode(zipped)))
+    Coders.Gzip.decode(ByteString(Base64.decode(zipped)))
   }
 
   implicit val decodeTerms: Decoder[ExecutionBuilderTerms] = (c: HCursor) => for {
     terms <- c.as[String]
   } yield {
-
+    import scala.concurrent.ExecutionContext.Implicits.global
     if(terms.startsWith("gzip:")) {
       Await.result({
-        import scala.concurrent.ExecutionContext.Implicits.global
         for {
           decodedTerms <- decodeGzip(terms.substring("gzip:".length))
         } yield ExecutionBuilderTerms(new String(decodedTerms.toArray[Byte]))
