81d30b824602c8a37381031fd26b65f7162137a7
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-09-02 01:25:33 -0700

    Fleshed out actor domain for KeyManagerActor in KeyManager.scala

--- gjallarhorn/src/main/scala/keymanager/KeyManager.scala
+++ gjallarhorn/src/main/scala/keymanager/KeyManager.scala
@@ -2,6 +2,12 @@ package keymanager
 
 import akka.actor.Actor
 
+//DOMAIN: KeyManager Actor
+object KeyManagerActor {
+  case class lockKeyFile(publicKeyString: String, password: String)
+  case class unlockKeyFile(publicKeyString: String, password: String)
+}
+
 class KeyManager extends Actor {
 
   override def receive: Receive = ???
