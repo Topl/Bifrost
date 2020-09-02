4bed43978d245eef0650d29db069a07877672887
ngjulia <jen67@cornell.edu>
2020-09-02 02:09:02 -0400

    modified receive function

--- gjallarhorn/src/main/scala/keymanager/KeyManager.scala
+++ gjallarhorn/src/main/scala/keymanager/KeyManager.scala
@@ -1,12 +1,24 @@
 package keymanager
 
 import akka.actor.Actor
+import keymanager.KeyFile.uuid
+import scorex.crypto.hash.Blake2b256
 
 object KeyManager {
-  case class
+  case class GenerateKeyFile(password: String, seed: Array[Byte] = Blake2b256(uuid), defaultKeyDir: String)
+  case class UnlockKeyFile(publicKeyString: String, password: String)
+  case class LockKeyFile(publicKeyString: String, password: String)
 }
 
 class KeyManager extends Actor {
 
-  override def receive: Receive = ???
+  import KeyManager._
+
+  val keyManager = Keys(Set.empty, "")
+
+  override def receive: Receive = {
+    case GenerateKeyFile(password, seed, defaultKeyDir) => KeyFile.apply(password, seed, defaultKeyDir)
+    case UnlockKeyFile(pubKeyString, password) => // not context.become bc we want stateful actors?
+    case LockKeyFile(pubKeyString, password) =>
+  }
 }
