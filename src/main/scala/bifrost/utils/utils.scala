603cf2ae0052ae69522110ec7f7abda8775c612d
jingsting <jingfei1024@gmail.com>
2020-09-09 13:26:45 -0500

    comments

--- src/main/scala/bifrost/utils/utils.scala
+++ src/main/scala/bifrost/utils/utils.scala
@@ -5,9 +5,12 @@ import bifrost.modifier.ModifierId
 // TODO: Jing - This is no longer needed, we could use ModifierId.hashBytes instead
 package object utils {
 
+  @deprecated
   def bytesToId(bytes: Array[Byte]): ModifierId = ModifierId(bytes)
 
+  @deprecated
   def idToBytes(id: ModifierId): Array[Byte] = id.hashBytes
-  
+
+  @deprecated
   def idToString(id: ModifierId): String = id.toString
 }
