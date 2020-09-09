bacd814e7d29ca80f259ddd164b0e7745a5e52ab
jingsting <jingfei1024@gmail.com>
2020-09-09 11:30:06 -0500

    Moved version to the first data in a block

--- src/main/scala/bifrost/modifier/block/BlockSerializer.scala
+++ src/main/scala/bifrost/modifier/block/BlockSerializer.scala
@@ -13,14 +13,14 @@ import bifrost.utils.serialization.{BifrostSerializer, Reader, Writer}
 object BlockSerializer extends BifrostSerializer[Block] {
 
   override def serialize(block: Block, w: Writer): Unit = {
+    /* version: Byte */
+    w.put(block.version)
+
     /* parentId: ModifierId */
     w.putBytes(block.parentId.hashBytes)
 
     /* timestamp: Long */
     w.putULong(block.timestamp)
-
-    /* version: Byte */
-    w.put(block.version)
 
     /* generatorBox: ArbitBox */
     BoxSerializer.serialize(block.forgerBox, w)
@@ -38,15 +38,15 @@ object BlockSerializer extends BifrostSerializer[Block] {
   override def parse(r: Reader): Block = {
     /* The order of the getByte, getLong... calls should not be changed */
 
+    // TODO: Jing - Version should be used in the future to determine if we need additional procedures in parsing
+    val version: Byte = r.getByte()
+
     // TODO: Jing - maybe we could check that the size of bytes to read in reader is less or equal to the max size of a block
 
     val parentId: ModifierId = ModifierId(r.getBytes(Block.blockIdLength))
     val timestamp: Long = r.getULong()
 
     // TODO: Jing - scorex uses toIntExact to make sure the Long does not exceed the length of an Int
-
-    // TODO: Jing - Version should be used in the future to determine if we need additional procedures in parsing
-    val version: Byte = r.getByte()
 
     val generatorBox: ArbitBox = BoxSerializer.parse(r).asInstanceOf[ArbitBox]
 
