65b86c764d6a8188cc9e20f8f39b0e1a51e0f279
tuxman <n.edmonds@topl.me>
2020-09-20 16:00:26 -0500

    Add execution context and IntegrationConstants to IntegrationSuite #574

--- it/src/test/scala/bifrost/it/IntegrationSuite.scala
+++ it/src/test/scala/bifrost/it/IntegrationSuite.scala
@@ -1,12 +1,21 @@
 package bifrost.it
 
+import java.util.concurrent.Executors
+
 import bifrost.it.util.Docker
 import bifrost.utils.Logging
 import org.scalatest.{BeforeAndAfterAll, Suite}
 
+import scala.concurrent.ExecutionContext
 import scala.util.Random
 
-trait IntegrationSuite extends BeforeAndAfterAll with Logging { this: Suite =>
+trait IntegrationSuite
+  extends IntegrationConstants
+    with BeforeAndAfterAll
+    with Logging { this: Suite =>
+
+  implicit val defaultExecutionContext: ExecutionContext =
+    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
 
   protected val localDataDir: String = s"/tmp/bifrost/it-${Random.nextInt(Int.MaxValue)}"
 
