ed1325994045cd794e5aaa7fa915dadc4c91bd9f
mhalloran21 <mmh3@williams.edu>
2020-09-29 13:44:08 -0800

    Added getBalances to Request.scala and edited WalletManager #627

--- gjallarhorn/src/main/scala/wallet/WalletManager.scala
+++ gjallarhorn/src/main/scala/wallet/WalletManager.scala
@@ -1,13 +1,12 @@
 package wallet
 
 import akka.actor.Actor
-import crypto.{PrivateKey25519, ProofOfKnowledgeProposition}
 import io.circe.Json
 
 import scala.collection.mutable.{Map => MMap}
 
 
-class WalletManager(publicKeys: Set[ProofOfKnowledgeProposition[PrivateKey25519]]) extends Actor {
+class WalletManager(publicKeys: Set[String]) extends Actor {
 
   import WalletManager._
 
@@ -19,25 +18,56 @@ class WalletManager(publicKeys: Set[ProofOfKnowledgeProposition[PrivateKey25519]
     returnVal
   }
 
+  def parseUpdate(json: Json): MMap[String, MMap[String, Json]] = {
+    val pubKeys: scala.collection.Set[String] = walletBoxes.keySet
+    pubKeys.foreach(key => {
+      val info: Json = (json \\ key).head
+      val boxesMap: MMap[String, Json] = MMap.empty
+      val boxes = (info \\ "Boxes")
+      if (boxes.nonEmpty) {
+        val boxesJson: Json = boxes.head
+        val asset = boxesJson \\ "Asset"
+        val poly = boxesJson \\ "Poly"
+        val arbit = boxesJson \\ "Arbit"
+        if (asset.nonEmpty) {
+          val assetBox: Json = asset.head
+          val id = (assetBox \\ "id").head.toString()
+          boxesMap.put(id, assetBox)
+        }
+        if (poly.nonEmpty) {
+          val polyBox: Json = poly.head
+          val id = (polyBox \\ "id").head.toString()
+          boxesMap.put(id, polyBox)
+        }
+        if (arbit.nonEmpty) {
+          val arbitBox: Json = arbit.head
+          val id = (arbitBox \\ "id").head.toString()
+          boxesMap.put(id, arbitBox)
+        }
+        walletBoxes(key) = boxesMap
+      }
+    })
+    walletBoxes
+  }
 
   override def receive: Receive = {
 
-    case UpdateWallet(add, remove) => {
+    case UpdateWallet(updatedBoxes) => sender ! parseUpdate(updatedBoxes)
+
+    /*case UpdateWallet(add, remove) => {
       remove.foreach { case (publicKey, ids) =>
         walletBoxes.get(publicKey).map(boxes => ids.foreach(boxes.remove))
       }
       add.foreach { case (publicKey, newBoxes) =>
         walletBoxes.get(publicKey).map(boxes => newBoxes.foreach(box => boxes.put(box._1, box._2)))
       }
-    }
+    }*/
 
-    case GetWalletBoxes() => {
-      sender ! walletBoxes
-    }
   }
 }
 
 object WalletManager {
-  case class UpdateWallet(add: MMap[String, MMap[String, Json]], remove: List[(String, List[String])])
-  case class GetWalletBoxes()
+  case class UpdateWallet(updatedBoxes: Json)
+  //case class UpdateWallet(add: MMap[String, MMap[String, Json]], remove: List[(String, List[String])])
+  //case class GetWalletBoxes()
 }
