30eeae1e9091da38351e5b4a507b2fbc9a58bfa8
tuxman <n.edmonds@topl.me>
2020-10-02 12:47:46 -0500

    PeerConnectionHandler test #529

---
+++ src/test/scala/bifrost/network/PeerConnectionHandlerSpec.scala
@@ -0,0 +1,42 @@
+package bifrost.network
+
+import java.net.InetSocketAddress
+
+import akka.actor._
+import akka.io.{IO, Tcp}
+import akka.testkit.TestKit
+import akka.testkit.TestActorRef
+import bifrost.BifrostGenerators
+import bifrost.network.peer.PeerConnectionHandlerRef
+import bifrost.settings.BifrostContext
+import org.scalatest.PrivateMethodTester
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.propspec.AnyPropSpecLike
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class PeerConnectionHandlerSpec extends TestKit(ActorSystem("PCHSpec"))
+  with AnyPropSpecLike
+  with ScalaCheckPropertyChecks
+  with PrivateMethodTester
+  with Matchers
+  with BifrostGenerators {
+
+  property("A new PeerConnectionHandler should be created with the specified message codes") {
+
+    val bifrostContext = new BifrostContext(settings, None)
+
+    val peerManagerRef: ActorRef = peer.PeerManagerRef("peerManager", settings.network, bifrostContext)
+    val networkControllerRef: ActorRef =
+      NetworkControllerRef("networkController", settings.network, peerManagerRef, bifrostContext)
+
+    val localPort = 9085
+    val remotePort = 9086
+    val connectionId = ConnectionId(new InetSocketAddress(localPort), new InetSocketAddress(remotePort), Incoming)
+
+    val connectionDescription = ConnectionDescription(networkControllerRef, connectionId, None, Seq())
+
+    val pch = PeerConnectionHandlerRef(settings.network, networkControllerRef, bifrostContext, connectionDescription)
+  }
+}
