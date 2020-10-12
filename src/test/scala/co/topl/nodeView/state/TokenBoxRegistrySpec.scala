50068d752742af7de84b8c7dda90e5093e23e550
Tuxman <n.edmonds@topl.me>
2020-10-12 17:34:01 -0500

    Rollback test for TokenRegistrySpec #664

--- src/test/scala/co/topl/nodeView/state/TokenBoxRegistrySpec.scala
+++ src/test/scala/co/topl/nodeView/state/TokenBoxRegistrySpec.scala
@@ -1,7 +1,10 @@
 package co.topl.nodeView.state
 
+import co.topl.crypto.FastCryptographicHash
+import co.topl.modifier.ModifierId
 import co.topl.nodeView.NodeViewHolder.MS
 import co.topl.{BifrostGenerators, ValidGenerators}
+import com.google.common.primitives.Ints
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.OptionValues.convertOptionToValuable
 import org.scalatest.matchers.should.Matchers
@@ -21,10 +24,27 @@ class TokenBoxRegistrySpec extends AnyPropSpec
   property("Token boxes should be inserted into the registry") {
     forAll(tokenBoxesGen) { tokens =>
       val keys = tokens.groupBy(_.proposition)
-      directlyAddTBRStorage(scala.util.Random.nextInt(), tokens, state)
+      directlyAddTBRStorage(scala.util.Random.nextInt, tokens, state)
       keys.foreach { key =>
         val ids = key._2.map(_.id)
         state.registryLookup(key._1).value shouldEqual ids
+      }
+    }
+  }
+
+  property("Rolling back should remove tokens from registry") {
+    forAll(tokenBoxesGen) { tokens =>
+      val tbr = state.tbrOpt.get
+      val version = ModifierId(FastCryptographicHash(Ints.toByteArray(scala.util.Random.nextInt)))
+      val keys = tokens.groupBy(_.proposition)
+      val update = keys.map(k => k._1 -> k._2.map(_.nonce))
+
+      val newTbr = tbr.update(version, Map(), update).get
+      val rollback = newTbr.rollbackTo(state.version).get
+
+      keys.foreach { key =>
+        val ids = key._2.map(_.id)
+        state.registryLookup(key._1) shouldBe None
       }
     }
   }
