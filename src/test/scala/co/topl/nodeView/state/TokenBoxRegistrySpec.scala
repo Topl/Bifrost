86888bc307ea5423b5b1f132d074710246d53f33
Tuxman <n.edmonds@topl.me>
2020-10-12 15:36:35 -0500

    use registryLookup and match against id's in TokenBoxRegistrySpec #664

--- src/test/scala/co/topl/nodeView/state/TokenBoxRegistrySpec.scala
+++ src/test/scala/co/topl/nodeView/state/TokenBoxRegistrySpec.scala
@@ -3,6 +3,7 @@ package co.topl.nodeView.state
 import co.topl.nodeView.NodeViewHolder.MS
 import co.topl.{BifrostGenerators, ValidGenerators}
 import org.scalatest.BeforeAndAfterAll
+import org.scalatest.OptionValues.convertOptionToValuable
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.propspec.AnyPropSpec
 import org.scalatestplus.scalacheck.{ScalaCheckDrivenPropertyChecks, ScalaCheckPropertyChecks}
@@ -19,10 +20,11 @@ class TokenBoxRegistrySpec extends AnyPropSpec
 
   property("Token boxes should be inserted into the registry") {
     forAll(tokenBoxesGen) { tokens =>
-      val keys = tokens.groupBy(tk => tk.proposition)
+      val keys = tokens.groupBy(_.proposition)
       directlyAddTBRStorage(scala.util.Random.nextInt(), tokens, state)
       keys.foreach { key =>
-        state.getTokenBoxes(key._1).get shouldEqual key._2
+        val ids = key._2.map(_.id)
+        state.registryLookup(key._1).value shouldEqual ids
       }
     }
   }
