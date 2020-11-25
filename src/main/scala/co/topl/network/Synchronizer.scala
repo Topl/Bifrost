a67cc3b2295d571249c01d2c8b15fb57b448300e
jingsting <jingfei1024@gmail.com>
2020-11-25 02:54:18 -0600

    Some comments for synchronizer and synctracker

--- src/main/scala/co/topl/network/Synchronizer.scala
+++ src/main/scala/co/topl/network/Synchronizer.scala
@@ -9,9 +9,10 @@ import scala.util.{Failure, Success}
 
 trait Synchronizer extends Actor with Logging {
 
-  /** these are the case statements for identifying the message handlers */
+  /** These are the case statements for identifying the message handlers */
   protected val msgHandlers: PartialFunction[(MessageSpec[_], _, ConnectedPeer), Unit]
 
+  /** Receive data from peer and start parsing and handling the message depending on the message spec */
   protected def processDataFromPeer: Receive = { case Message(spec, Left(msgBytes), Some(source)) =>
     parseAndHandle(spec, msgBytes, source)
   }
@@ -25,15 +26,15 @@ trait Synchronizer extends Actor with Logging {
     */
   protected def parseAndHandle(spec: MessageSpec[Any], msgBytes: Array[Byte], source: ConnectedPeer): Unit = {
 
-    /** attempt to parse the message */
+    /** Attempt to parse the message */
     spec.parseBytes(msgBytes) match {
-      /** if a message could be parsed, match the type of content found and ensure a handler is defined */
+      /** If a message could be parsed, match the type of content found and ensure a handler is defined */
       case Success(content) =>
         val parsedMsg = (spec, content, source)
         if (msgHandlers.isDefinedAt(parsedMsg)) msgHandlers.apply(parsedMsg)
         else log.error(s"Function handler not found for the parsed message: $parsedMsg")
 
-      /** if a message could not be parsed, penalize the remote peer */
+      /** If a message could not be parsed, penalize the remote peer */
       case Failure(e) =>
         log.error(s"Failed to deserialize data from ${source}: ", e)
         penalizeMaliciousPeer(source)
