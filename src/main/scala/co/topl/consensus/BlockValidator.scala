c4965e2e36e5e3212965eec222eaf4d8694c0ca7
James Aman <jim.a.aman@gmail.com>
2020-11-30 09:09:50 -0600

    removed uneeded fields in validator

--- src/main/scala/co/topl/consensus/BlockValidator.scala
+++ src/main/scala/co/topl/consensus/BlockValidator.scala
@@ -16,56 +16,26 @@ sealed trait BlockValidator[PM <: Block] {
 
 class DifficultyBlockValidator(storage: Storage, blockProcessor: BlockProcessor) extends BlockValidator[Block] {
   def validate(block: Block): Try[Unit] = Try {
+    // lookup our local data about the parent
+    val (parent, prevBlockTimes) = getParentDetailsOf(block)
 
     // first ensure that we can calculate the same block data as is stamped on the block
-    ensureHeightAndDifficulty(storage, block) match {
+    ensureHeightAndDifficulty(block, parent, prevBlockTimes) match {
       case Failure(ex) => throw ex
       case _ => // continue on
     }
 
     // next, ensure the hit was valid
-    val (parent, parentDifficulty, parentHeight) = getParentDetails(block.parentId)
-    checkValidHit(block, parent, parentHeight, parentDifficulty) match {
+    ensureValidHit(block, parent) match {
       case Failure(ex) => throw ex
       case _ => // continue on
     }
   }
 
-  /** Helper function to find the source of the parent block (either storage or chain cache) */
-  //todo: JAA - no longer need to separately pull the difficulty and height since they are in the block,
-  //            can also remove these fields from cache blocks
-  private def getParentDetails(parentId: ModifierId): (Block, Long, Long) =
-    blockProcessor.getCacheBlock(parentId) match {
-      case Some(cacheParent) =>
-        (cacheParent.block, cacheParent.baseDifficulty, cacheParent.height)
-      case None =>
-        (
-          storage.modifierById(parentId).get, //we have already checked if the parent exists so can get
-          storage.difficultyAt(parentId),
-          storage.heightAt(parentId)
-        )
-    }
-
-  /** Gets the timestamps for 'count' number of blocks prior to the startBlock */
-  def getTimestamps(storage: Storage, count: Long, startBlock: Block): Seq[Block.Timestamp] = {
-    @tailrec
-    def loop(b: Block, acc: Seq[Block.Timestamp] = Seq()): Seq[Block.Timestamp] = {
-      if (acc.length >= count) acc
-      else storage.modifierById(b.parentId) match {
-        case Some(parent: Block) => loop(parent, b.timestamp +: acc)
-        case _ => b.timestamp +: acc
-      }
-    }
-
-    loop(startBlock)
-  }
-
-  private def ensureHeightAndDifficulty(storage: Storage, block: Block): Try[Unit] = Try {
+  private def ensureHeightAndDifficulty(block: Block, parent: Block, prevTimes: Seq[Block.Timestamp]): Try[Unit] = Try {
     // calculate the new base difficulty
-    val parentDifficulty = storage.difficultyOf(block.parentId).get
-    val prevTimes = getTimestamps(storage, consensus.nxtBlockNum + 1, block)
-    val newHeight = storage.heightOf(block.parentId).get + 1
-    val newBaseDifficulty = consensus.calcNewBaseDifficulty(newHeight, parentDifficulty, prevTimes)
+    val newHeight = parent.height + 1
+    val newBaseDifficulty = consensus.calcNewBaseDifficulty(newHeight, parent.difficulty, prevTimes)
 
     // does the difficulty stamped on the block match what we would calculate locally?
     require(block.difficulty == newBaseDifficulty,
@@ -76,17 +46,44 @@ class DifficultyBlockValidator(storage: Storage, blockProcessor: BlockProcessor)
       s"Local calculation of block height failed since ${block.height} != $newHeight")
   }
 
-  private def checkValidHit(block: Block, parent: Block, parentHeight: Long, parentDifficulty: Long): Try[Unit] = Try {
+  private def ensureValidHit(block: Block, parent: Block): Try[Unit] = Try {
     // calculate the hit value from the forger box included in the new block
     val hit = calcHit(parent)(block.generatorBox)
 
     // calculate the adjusted difficulty the forger would have used to determine eligibility
     val timestamp = block.timestamp
-    val target = calcAdjustedTarget(parent, parentHeight, parentDifficulty, timestamp)
+    val target = calcAdjustedTarget(parent, parent.height, parent.difficulty, timestamp)
     val valueTarget = (target * BigDecimal(block.generatorBox.value)).toBigInt
 
     // did the forger create a block with a valid forger box and adjusted difficulty?
     require(BigInt(hit) < valueTarget, s"Block difficulty failed since $hit > $valueTarget")
+  }
+
+  /** Helper function to find the source of the parent block (either storage or chain cache) */
+  private def getParentDetailsOf(block: Block): (Block, Seq[Block.Timestamp]) =
+    blockProcessor.getCacheBlock(block.parentId) match {
+      case Some(cacheParent) => (cacheParent.block, cacheParent.prevBlockTimes)
+      case None =>
+        //we have already checked if the parent exists so can get
+        val parent = storage.modifierById(block.parentId).get
+        (parent, getTimestamps(consensus.nxtBlockNum + 1, parent) :+ block.timestamp)
+    }
+
+  /** Gets the timestamps for 'count' number of blocks prior to (and including) the startBlock */
+  def getTimestamps(count: Long, startBlock: Block): Seq[Block.Timestamp] = {
+    @tailrec
+    def loop(id: ModifierId, acc: Seq[Block.Timestamp] = Seq()): Seq[Block.Timestamp] = {
+      if (acc.length >= count) acc
+      else storage.parentIdOf(id) match {
+        case Some(parentId: ModifierId) =>
+          val parentTimestamp = storage.timestampOf(parentId).get
+          loop(parentId, parentTimestamp +: acc)
+
+        case _ => acc
+      }
+    }
+
+    loop(startBlock.id, Seq(startBlock.timestamp))
   }
 }
 
