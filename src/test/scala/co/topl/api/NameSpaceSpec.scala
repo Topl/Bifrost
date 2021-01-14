fd9f1490d50fa7285908b3580cefc048ac020dde
jingsting <jingfei1024@gmail.com>
2021-01-14 15:33:22 -0600

    Code clean up

--- src/test/scala/co/topl/api/NameSpaceSpec.scala
+++ src/test/scala/co/topl/api/NameSpaceSpec.scala
@@ -1,18 +1,23 @@
 package co.topl.api
 
+import akka.http.scaladsl.model.StatusCodes.{BadRequest, InternalServerError}
 import akka.http.scaladsl.server.Route
 import akka.util.ByteString
 import co.topl.http.HttpService
 import co.topl.http.api.ApiEndpoint
-import co.topl.http.api.endpoints.{DebugApiEndpoint, KeyManagementApiEndpoint, NodeViewApiEndpoint, TransactionApiEndpoint, UtilsApiEndpoint}
+import co.topl.http.api.endpoints.{
+  DebugApiEndpoint,
+  KeyManagementApiEndpoint,
+  NodeViewApiEndpoint,
+  TransactionApiEndpoint,
+  UtilsApiEndpoint
+}
 import co.topl.settings.AppSettings
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import io.circe.parser.parse
 
-class NameSpaceSpec extends AnyWordSpec
-  with Matchers
-  with RPCMockState {
+class NameSpaceSpec extends AnyWordSpec with Matchers with RPCMockState {
 
   val rpcSettingsFalse: AppSettings = rpcSettings.copy(
     rpcApi = rpcSettings.rpcApi.copy(
@@ -52,8 +57,7 @@ class NameSpaceSpec extends AnyWordSpec
 
   "debug RPC with namespaces turned on" should {
     "Refuse debug requests with undefined method and return the correct errors" in {
-      val requestBody = ByteString(
-        s"""
+      val requestBody = ByteString(s"""
            |{
            |   "jsonrpc": "2.0",
            |   "id": "1",
@@ -63,17 +67,19 @@ class NameSpaceSpec extends AnyWordSpec
         """.stripMargin)
 
       httpPOST(requestBody) ~> routeTrue ~> check {
-        val res = parse(responseAs[String]) match {case Right(re) => re; case Left(ex) => throw ex}
-        (res \\ "code").head.asNumber.get.toInt.get == 500 shouldBe true
-        (res \\ "message").head.asString.get == "Service handler not found for method: debug_empty" shouldBe true
+        val res = parse(responseAs[String]) match { case Right(re) => re; case Left(ex) => throw ex }
+        val code = res.hcursor.downField("error").get[Int]("code")
+        val message = res.hcursor.downField("error").get[String]("message")
+
+        code shouldEqual Right(InternalServerError.intValue)
+        message shouldEqual Right("Service handler not found for method: debug_empty")
       }
     }
   }
 
   "debug RPC with namespaces turned off" should {
     "Refuse debug requests with correct method and return the correct errors" in {
-      val requestBody = ByteString(
-        s"""
+      val requestBody = ByteString(s"""
            |{
            |   "jsonrpc": "2.0",
            |   "id": "1",
@@ -83,9 +89,12 @@ class NameSpaceSpec extends AnyWordSpec
         """.stripMargin)
 
       httpPOST(requestBody) ~> routeFalse ~> check {
-        val res = parse(responseAs[String]) match {case Right(re) => re; case Left(ex) => throw ex}
-        (res \\ "code").head.asNumber.get.toInt.get == 500 shouldBe true
-        (res \\ "message").head.asString.get == "Service handler not found for method: debug_myBlocks" shouldBe true
+        val res = parse(responseAs[String]) match { case Right(re) => re; case Left(ex) => throw ex }
+        val code = res.hcursor.downField("error").get[Int]("code")
+        val message = res.hcursor.downField("error").get[String]("message")
+
+        code shouldEqual Right(InternalServerError.intValue)
+        message shouldEqual Right("Service handler not found for method: debug_myBlocks")
       }
     }
   }
@@ -95,15 +104,21 @@ class NameSpaceSpec extends AnyWordSpec
       val requestBody = ByteString("")
 
       httpPOST(requestBody) ~> routeTrue ~> check {
-        val res = parse(responseAs[String]) match {case Right(re) => re; case Left(ex) => throw ex}
-        (res \\ "code").head.asNumber.get.toInt.get == 400 shouldBe true
-        (res \\ "message").head.asString.get == "Unable to parse Json body" shouldBe true
+        val res = parse(responseAs[String]) match { case Right(re) => re; case Left(ex) => throw ex }
+        val code = res.hcursor.downField("error").get[Int]("code")
+        val message = res.hcursor.downField("error").get[String]("message")
+
+        code shouldEqual Right(BadRequest.intValue)
+        message shouldEqual Right("Unable to parse Json body")
       }
 
       httpPOST(requestBody) ~> routeFalse ~> check {
-        val res = parse(responseAs[String]) match {case Right(re) => re; case Left(ex) => throw ex}
-        (res \\ "code").head.asNumber.get.toInt.get == 400 shouldBe true
-        (res \\ "message").head.asString.get == "Unable to parse Json body" shouldBe true
+        val res = parse(responseAs[String]) match { case Right(re) => re; case Left(ex) => throw ex }
+        val code = res.hcursor.downField("error").get[Int]("code")
+        val message = res.hcursor.downField("error").get[String]("message")
+
+        code shouldEqual Right(BadRequest.intValue)
+        message shouldEqual Right("Unable to parse Json body")
       }
     }
   }
