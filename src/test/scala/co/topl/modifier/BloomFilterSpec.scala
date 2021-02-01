8d7761271a39007c36cdd8e8b12c19f0163b7fa9
jingsting <jingfei1024@gmail.com>
2021-02-01 11:43:40 -0600

    Added test on the probability of false positives

--- src/test/scala/co/topl/modifier/BloomFilterSpec.scala
+++ src/test/scala/co/topl/modifier/BloomFilterSpec.scala
@@ -1,11 +1,16 @@
 package co.topl.modifier
 
+import co.topl.attestation.{Address, PrivateKeyCurve25519}
+import co.topl.modifier.block.BloomFilter.BloomTopic
 import co.topl.modifier.block.{BloomFilter, TransactionsCarryingPersistentNodeViewModifier}
 import co.topl.modifier.transaction.Transaction
 import co.topl.utils.ValidGenerators
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.propspec.AnyPropSpec
 import org.scalatestplus.scalacheck.{ScalaCheckDrivenPropertyChecks, ScalaCheckPropertyChecks}
+import scorex.crypto.hash.Blake2b256
+import scorex.crypto.signatures.Curve25519
+import scorex.util.encode.Base58
 
 class BloomFilterSpec
     extends AnyPropSpec
@@ -14,7 +19,7 @@ class BloomFilterSpec
     with ValidGenerators
     with Matchers {
 
-  property("Bloomfilter should be able to tell if it definitely contains an address") {
+  property("Bloomfilter should be able to tell if it definitely contains an address(false negatives)") {
     forAll(validBifrostTransactionSeqGen) { txs =>
       val bloomfilter: BloomFilter = TransactionsCarryingPersistentNodeViewModifier.createBloom(txs)
 
@@ -26,17 +31,48 @@ class BloomFilterSpec
     }
   }
 
-  property("Bloomfilter should be able to tell if an address is likely not in the block") {
+  property("Bloomfilter should be able to tell if an address is likely not in the block(false positives)") {
     forAll(validBifrostTransactionSeqGen) { txs =>
       val bloomfilter: BloomFilter = TransactionsCarryingPersistentNodeViewModifier.createBloom(txs.dropRight(1))
-      val addressCount = txs.foldLeft(0)(_+_.bloomTopics.size)
+      val addressCount = txs.foldLeft(0)(_ + _.bloomTopics.size)
+      val addressInBloom: Int = txs.dropRight(1).foldLeft(0)(_ + _.bloomTopics.size)
+      val numAddressLastTx: Int = txs.last.bloomTopics.size
 
       val falsePositives = txs.last.bloomTopics.foldLeft(0) { (count, bt) =>
         if (bloomfilter.contains(bt)) count + 1
         else count
       }
 
-      (falsePositives <= addressCount/100) shouldBe true
+      /** Sometimes there's very few addresses in the last transaction, we only test here to make sure we don't get too
+        * many false positives. There's a very slight chance that this will break
+        */
+      (falsePositives <= numAddressLastTx / 2) shouldBe true
     }
+  }
+
+  /** See: https://hur.st/bloomfilter/?n=500&p=&m=2048&k=4
+    * Assuming that there will be 500 address in the bloom filter:
+    *   the probability of false positives will be 0.151 (1 in 7)
+    */
+  property("The probability of false positives in bloomfilter with 500 addresses should be lower than 0.15") {
+    /** The generated addresses are made deterministic, so that the variation won't break the test */
+    val rand = new scala.util.Random(1)
+
+    val randAddr: Seq[Address] =
+      (0 until 600)
+        .map(_ => Array.fill(Curve25519.KeyLength)((rand.nextInt(256) - 128).toByte))
+        .map(s => PrivateKeyCurve25519.secretGenerator.generateSecret(s)._2)
+        .map(k => k.address)
+
+    val bloomTopics: Set[BloomTopic] = randAddr.take(500).map(addr => BloomTopic @@ addr.bytes).toSet
+    val bloomfilter: BloomFilter = BloomFilter(bloomTopics)
+    val testTopics: Seq[BloomTopic] = randAddr.drop(500).map(addr => BloomTopic @@ addr.bytes)
+
+    val falsePositives = testTopics.foldLeft(0) { (count, bt) =>
+      if (bloomfilter.contains(bt)) count + 1
+      else count
+    }
+
+    (falsePositives <= testTopics.size * 0.15) shouldBe true
   }
 }
