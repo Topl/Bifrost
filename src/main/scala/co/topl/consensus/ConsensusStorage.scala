23245da54965cd5c0b4860683ca55663fdcb7f13
Branden Vennes <b.n.vennes@gmail.com>
2021-03-30 10:44:44 -0700

    add constant keys for ConsensusStorage #1112
    - add constant ByteWrapper values for each param key in ConsensusStorage
    - add comments for methods in ConsensusStorage and ByteArrayWrapper

--- src/main/scala/co/topl/consensus/ConsensusStorage.scala
+++ src/main/scala/co/topl/consensus/ConsensusStorage.scala
@@ -10,48 +10,74 @@ import scala.util.Try
 
 class ConsensusStorage(val storage: LSMStore) {
 
-  /** Update consensus storage whenever a new block is appended to the chain
-    *
+  // constant keys for each piece of consensus state
+  private val totalStakeKey = ByteArrayWrapper("totalStake".getBytes)
+  private val difficultyKey = ByteArrayWrapper("difficulty".getBytes)
+  private val inflationKey = ByteArrayWrapper("inflation".getBytes)
+  private val heightKey = ByteArrayWrapper("height".getBytes)
+
+  /** Updates the consensus storage with the given values at a given block as the version.
+    * @param blockId the block ID associated with the consensus parameters
+    * @param params the current state of the parameters
     */
   def updateConsensusStorage(blockId: ModifierId, params: ConsensusParams): Unit = {
     // TODO: Branden - determine if this should be blockId.bytes or blockId.getIdBytes
-    val versionId = ByteArrayWrapper(blockId.bytes)
+    val versionId = toVersionId(blockId)
 
-    val totalStakePair = Seq("totalStake".getBytes -> params.totalStake.toByteArray)
-    val difficultyPair = Seq("difficulty".getBytes -> params.difficulty.toByteArray)
-    val inflationPair = Seq("inflation".getBytes -> params.inflation.toByteArray)
-    val heightPair = Seq("height".getBytes -> params.height.toByteArray)
+    val totalStakePair = Seq(totalStakeKey -> params.totalStake.toByteArray)
+    val difficultyPair = Seq(difficultyKey -> params.difficulty.toByteArray)
+    val inflationPair = Seq(inflationKey -> params.inflation.toByteArray)
+    val heightPair = Seq(heightKey -> params.height.toByteArray)
 
     val toUpdate = (totalStakePair ++ difficultyPair ++ inflationPair ++ heightPair).map{
-      case (k, v) => ByteArrayWrapper(k) -> ByteArrayWrapper(v)
+      case (k, v) => k -> ByteArrayWrapper(v)
     }
 
     storage.update(versionId, Seq(), toUpdate)
   }
 
   def totalStake: Option[Int128] =
-    storage.get(ByteArrayWrapper("totalStake".getBytes)).map(b => Int128(b.data))
+    storage.get(totalStakeKey).map(v => Int128(v.data))
 
   def difficulty: Option[Int128] =
-    storage.get(ByteArrayWrapper("difficulty".getBytes)).map(b => Int128(b.data))
+    storage.get(difficultyKey).map(v => Int128(v.data))
 
   def inflation: Option[Int128] =
-    storage.get(ByteArrayWrapper("inflation".getBytes)).map(b => Int128(b.data))
+    storage.get(inflationKey).map(v => Int128(v.data))
 
   def height: Option[Int128] =
-    storage.get(ByteArrayWrapper("height".getBytes)).map(b => Int128(b.data))
+    storage.get(heightKey).map(v => Int128(v.data))
 
-  def rollbackTo(blockId: ModifierId): Try[Unit] = Try(storage.rollback(ByteArrayWrapper(blockId.bytes)))
+  /** Rolls back the current state of storage to the data within the given version.
+    * @param blockId the ID of the block to rollback to
+    * @return a Try with a Success result if no errors occurred in the rollback
+    */
+  def rollbackTo(blockId: ModifierId): Try[Unit] = Try(storage.rollback(toVersionId(blockId)))
+
+  /** Converts the given block ID to a version ID to use with LSM Store.
+    * @param blockId the ID of the block
+    * @return the version ID
+    */
+  private def toVersionId(blockId: ModifierId): ByteArrayWrapper =
+    ByteArrayWrapper(blockId.getIdBytes)
 
 }
 
 case class ConsensusParams(totalStake: Int128, difficulty: Int128, inflation: Int128, height: Int128)
 
 object ConsensusStorage {
+
+  /** Initializes a new instance of consensus storage defaulted with the given values at the given block ID.
+    * @param blockId the ID of the version
+    * @param totalStake the total amount of stake
+    * @param difficulty the starting difficulty
+    * @param settings the configured app settings
+    * @return an initialized ConsensusStorage with default values at the genesis block version
+    */
   def initialize(
     blockId: ModifierId,
     totalStake: Int128,
-    initialDifficulty: Int128,
+    difficulty: Int128,
     settings: AppSettings
   ): ConsensusStorage = {
     val dataDir = settings.application.dataDir.ensuring(_.isDefined, "A data directory must be specified").get
@@ -60,9 +86,10 @@ object ConsensusStorage {
 
     val consensusStorage = new ConsensusStorage(storage)
 
-    val params = ConsensusParams(totalStake, initialDifficulty, 0, 0)
+    val params = ConsensusParams(totalStake, difficulty, 0, 0)
     consensusStorage.updateConsensusStorage(blockId, params)
 
     consensusStorage
   }
+
 }
