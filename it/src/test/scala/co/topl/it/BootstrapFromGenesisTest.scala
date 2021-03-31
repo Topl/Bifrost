090fed5a43a33f874e35b6d16552bfae7bae6481
Sean Cheatham <seancheatham@gmail.com>
2021-03-31 15:06:16 -0400

    Update test RPC retry mechanism to use akka-streams.  Change Genesis test to forge old node for 1024+ blocks.

--- it/src/test/scala/co/topl/it/BootstrapFromGenesisTest.scala
+++ it/src/test/scala/co/topl/it/BootstrapFromGenesisTest.scala
@@ -1,9 +1,10 @@
 package co.topl.it
 
 import co.topl.it.util._
-import co.topl.utils.Int128, Int128._
+import co.topl.utils.Int128
+import Int128._
+import akka.actor.Scheduler
 import com.typesafe.config.ConfigFactory
-import io.circe.syntax._
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.freespec.AnyFreeSpec
@@ -20,22 +21,35 @@ class BootstrapFromGenesisTest
     with EitherValues
     with Inspectors {
 
-  val forgeDuration: FiniteDuration = 30.seconds
+  implicit val scheduler: Scheduler = system.scheduler
+
+  val initialForgeTarget: Int128 = 1024 + 50
   val newNodeForgeDuration: FiniteDuration = 10.seconds
+  val targetBlockTime: FiniteDuration = 50.milli
+  val syncWindow: FiniteDuration = 30.seconds
   val seed: String = "BootstrapFromGenesisTest" + System.currentTimeMillis()
 
   override implicit val patienceConfig: PatienceConfig = PatienceConfig(2.seconds)
 
   "A new node can sync its genesis block with an old node" in {
 
-    val nodeNames = List("oldNode", "newNode")
-
     val config =
       ConfigFactory.parseString(
-        raw"""bifrost.network.knownPeers = ${nodeNames.map(n => s"$n:${BifrostDockerNode.NetworkPort}").asJson}
+        raw"""bifrost.network.knownPeers = []
              |bifrost.rpcApi.namespaceSelector.debug = true
+             |bifrost.forging.forgeOnStartup = false
+             |bifrost.forging.blockGenerationDelay = $targetBlockTime
              |bifrost.forging.privateTestnet.numTestnetAccts = 2
              |bifrost.forging.privateTestnet.genesisSeed = "$seed"
+             |bifrost.forging.protocolVersions = [
+             |      {
+             |        version { value = 1.0.0 }
+             |        startBlock = 0
+             |        blockVersion = 1
+             |        targetBlockTime = $targetBlockTime
+             |        numTxPerBlock = 100
+             |      }
+             |    ]
              |""".stripMargin
       )
 
@@ -58,14 +72,22 @@ class BootstrapFromGenesisTest
 
     oldNode.Admin.startForging().futureValue.value
 
-    logger.info(s"Allowing oldNode to forge for $forgeDuration")
+    logger.info(s"Allowing oldNode to forge $initialForgeTarget blocks.  This may take a while.")
 
-    Thread.sleep(forgeDuration.toMillis)
+    oldNode.pollUntilHeight(initialForgeTarget).futureValue(Timeout(20.minutes)).value
 
     logger.info("Starting newNode")
 
     val newNode = dockerSupport.createNode("newNode", seed)
-    newNode.reconfigure(config)
+
+    val newNodeConfig =
+      ConfigFactory.parseString(
+        raw"""bifrost.network.knownPeers = ["oldNode:${BifrostDockerNode.NetworkPort}"]
+          |""".stripMargin
+      )
+        .withFallback(config)
+
+    newNode.reconfigure(newNodeConfig)
     newNode.start()
 
     newNode.waitForStartup().futureValue(Timeout(60.seconds)).value
@@ -78,6 +100,15 @@ class BootstrapFromGenesisTest
 
     Thread.sleep(newNodeForgeDuration.toMillis)
 
+    logger.info("Stopping forging on both nodes")
+
+    oldNode.Admin.stopForging().futureValue.value
+    newNode.Admin.stopForging().futureValue.value
+
+    logger.info(s"Waiting $syncWindow for the nodes to sync")
+
+    Thread.sleep(syncWindow.toMillis)
+
     val oldNodeHeight: Int128 =
       oldNode.Topl.head().futureValue.value.height
     logger.info(s"Old node height=$oldNodeHeight")
@@ -87,7 +118,7 @@ class BootstrapFromGenesisTest
     logger.info(s"New node height=$newNodeHeight")
 
     oldNodeHeight shouldBe newNodeHeight +- 1
-    oldNodeHeight should be > Int128(1L)
+    oldNodeHeight should be >= initialForgeTarget
   }
 
 }
--- it/src/test/scala/co/topl/it/util/NodeRpcApi.scala
+++ it/src/test/scala/co/topl/it/util/NodeRpcApi.scala
@@ -5,12 +5,13 @@ import akka.actor.{ActorSystem, Scheduler}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.RawHeader
+import akka.stream.scaladsl.{Sink, Source}
 import cats.data.{EitherT, NonEmptyList}
 import cats.implicits._
 import co.topl.utils.Int128
 import com.spotify.docker.client.DockerClient
 import io.circe._
-import io.circe.generic.auto._
+import io.circe.generic.semiauto._
 import io.circe.parser._
 import io.circe.syntax._
 import org.slf4j.{Logger, LoggerFactory}
@@ -19,37 +20,50 @@ import scorex.util.encode.Base58
 
 import java.util.UUID
 import scala.concurrent.Future
-import scala.concurrent.duration.DurationInt
-import scala.util.{Success, Try}
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 case class NodeRpcApi(host: String, rpcPort: Int)(implicit system: ActorSystem) {
 
-  import system.dispatcher
-
   import NodeRpcApi._
+  import system.dispatcher
 
   protected val log: Logger = LoggerFactory.getLogger(s"${getClass.getName} host=$host rpcPort=$rpcPort")
 
   implicit def scheduler: Scheduler = system.scheduler
 
+  def retry[T](
+    f:               () => Future[T],
+    attemptInterval: FiniteDuration = 1.seconds,
+    timeout:         FiniteDuration,
+    attempts:        Int = 20
+  ): Future[T] =
+    Source
+      .tick(Duration.Zero, attemptInterval, {})
+      .take(attempts)
+      .takeWithin(timeout)
+      .mapAsync(1)(_ => f().map(Success(_)).recover { case e => Failure(e) })
+      .collect { case Success(value) => value }
+      .runWith(Sink.head)
+
   def call(request: HttpRequest): Future[StrictHttpResponse] =
-    akka.pattern
-      .retry(
-        () => Http().singleRequest(request),
-        attempts = 20,
-        1.seconds
-      )
-      .flatMap(response =>
-        response.entity
-          .toStrict(1.second)
-          .map(entity =>
-            StrictHttpResponse(
-              response.status,
-              response.headers.map(header => header.name() -> header.value()).toMap,
-              entity.data.utf8String
-            )
-          )
-      )
+    retry(
+      () =>
+        Http()
+          .singleRequest(request)
+          .flatMap(response =>
+            response.entity
+              .toStrict(1.second)
+              .map(entity =>
+                StrictHttpResponse(
+                  response.status,
+                  response.headers.map(header => header.name() -> header.value()).toMap,
+                  entity.data.utf8String
+                )
+              )
+          ),
+      timeout = 30.seconds
+    )
       .andThen { case Success(strict) =>
         log.debug(s"Request: ${request.uri} \n Response: ${strict.body}")
       }
@@ -89,6 +103,18 @@ case class NodeRpcApi(host: String, rpcPort: Int)(implicit system: ActorSystem) 
   def waitForStartup(): Future[Either[NodeApiError, Done]] =
     EitherT(Debug.myBlocks()).map(_ => Done).value
 
+  def pollUntilHeight(targetHeight: Int128, period: FiniteDuration = 200.milli)(implicit
+    scheduler:                      Scheduler
+  ): Future[Either[NodeApiError, Done]] =
+    Source
+      .tick(Duration.Zero, period, {})
+      .mapAsync(1)(_ => Topl.head())
+      .collect {
+        case Right(response) if response.height >= targetHeight =>
+          Right(Done)
+      }
+      .runWith(Sink.head)
+
   object Debug {
 
     def myBlocks(): Future[Either[NodeApiError, Long]] =
@@ -118,6 +144,11 @@ case class NodeRpcApi(host: String, rpcPort: Int)(implicit system: ActorSystem) 
       EitherT(rpc("admin_startForging"))
         .map(_ => Done)
         .value
+
+    def stopForging(): Future[Either[NodeApiError, Done]] =
+      EitherT(rpc("admin_stopForging"))
+        .map(_ => Done)
+        .value
   }
 
   object Topl {
@@ -129,6 +160,9 @@ case class NodeRpcApi(host: String, rpcPort: Int)(implicit system: ActorSystem) 
 
     object Responses {
       case class HeadResponse(height: Int128, score: Long, bestBlockId: String)
+
+      implicit val headResponseDecoder: Decoder[HeadResponse] =
+        Decoder.forProduct3("height", "score", "bestBlockId")(HeadResponse.apply)
 
     }
   }
@@ -145,8 +179,12 @@ object NodeRpcApi {
     new NodeRpcApi(host, BifrostDockerNode.RpcPort)
   }
 
+  implicit val rpcInEncoder: Encoder[RpcIn] =
+    deriveEncoder[RpcIn]
+
   implicit val int128Decoder: Decoder[Int128] =
-    Decoder.decodeString.emapTry(v => Try(Int128(v)))
+    Decoder.decodeString
+      .emapTry(v => Try(Int128(v)))
       .or(Decoder.decodeBigInt.emapTry(v => Try(Int128(v))))
 }
 
