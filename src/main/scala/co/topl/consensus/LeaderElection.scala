9f59feb68d33bd982abd4ff32d0d7388604b8fa7
Branden Vennes <b.n.vennes@gmail.com>
2021-04-12 14:10:36 -0700

    latest changes to prosomo leader election implementation #1199

--- src/main/scala/co/topl/consensus/LeaderElection.scala
+++ src/main/scala/co/topl/consensus/LeaderElection.scala
@@ -1,10 +1,13 @@
 package co.topl.consensus
 
+import co.topl.attestation.Address
 import co.topl.attestation.keyManagement.PrivateKeyCurve25519
+import co.topl.consensus.LeaderElectionProsomo.{Proof, isSlotLeaderForThreshold}
 import co.topl.cryptoprimitives.{Ratio, Vrf}
 import co.topl.modifier.block.Block
-import co.topl.modifier.box.ArbitBox
+import co.topl.modifier.box.{ArbitBox, ProgramId}
 import co.topl.modifier.transaction.Transaction
+import co.topl.nodeView.state.StateReader
 import co.topl.utils.{Logging, TimeProvider}
 import com.google.common.primitives.Longs
 import scorex.crypto.hash.Blake2b256
@@ -20,7 +23,12 @@ object LeaderElectionBifrost extends Logging {
 
   type Hit = ArbitBox
 
-  def getHit(parent: Block, arbitBoxes: Seq[ArbitBox], timestamp: TimeProvider.Time): Option[Hit] = {
+  type SR = StateReader[ProgramId, Address]
+
+  def getHit(parent: Block, addresses: Set[Address], timestamp: TimeProvider.Time, stateReader: SR
+            ): Either[NoArbitBoxesError, Option[Hit]] = {
+    val arbitBoxes = getArbitBoxes(stateReader, addresses)
+
     val adjustedTarget: BigDecimal = {
       val target: Double = parent.difficulty.toDouble / consensusStorage.totalStake.toDouble
       val timeDelta = timestamp - parent.timestamp
@@ -36,13 +44,30 @@ object LeaderElectionBifrost extends Logging {
 
     // test procedure to determine eligibility
     arbitBoxes
-      .map(box => (box, calculateHit(parent.bytes, box.bytes)))
-      .filter { test =>
-        BigInt(test._2) < (test._1.value.quantity.doubleValue() * adjustedTarget).toBigInt
-      }
-      .map(h => h._1)
-      .headOption
+      .map(
+        _.map(box => (box, calculateHit(parent.bytes, box.bytes)))
+          .filter { test =>
+            BigInt(test._2) < (test._1.value.quantity.doubleValue() * adjustedTarget).toBigInt
+          }
+          .map(h => h._1)
+          .headOption)
   }
+
+  def getArbitBoxes(stateReader: SR, addresses: Set[Address]): Either[NoArbitBoxesError, Seq[ArbitBox]] =
+    if (addresses.nonEmpty) {
+      val boxes = addresses.flatMap {
+        stateReader
+          .getTokenBoxes(_)
+          .getOrElse(Seq())
+          .collect { case box: ArbitBox => box }
+      }.toSeq
+
+      Right(boxes)
+    } else {
+      Left(NoArbitBoxesError())
+    }
+
+  case class NoArbitBoxesError()
 }
 
 object LeaderElectionProsomo extends Logging {
@@ -60,49 +85,52 @@ object LeaderElectionProsomo extends Logging {
 
   case class Hit(cert: Certificate, proof: Proof)
 
+  val defaultLddCutoff = 40
+  val precision = 16
+
   def getHit(
     key: Key,
     relativeStake: Ratio,
     slot: Slot,
     slotDiff: Slot, // diff between current slot and parent slot
     parentProof: Proof,
-    vrfBuilder: (SecretKey, Slot, Nonce) => VrfProof,
-    hash: Array[Byte] => Array[Byte],
-    epochNonce: Nonce, // unknown value
-    lddCutoff: Slot = 40,
-    convergentAccuracy: Int = 16
+    epochNonce: Nonce,
+    lddCutoff: Slot = defaultLddCutoff
   ): Option[Hit] = {
-    val vrf = vrfBuilder(key.privateKey, slot, epochNonce)
+    val privateKeyBytes = key.privateKey.tail
+    val vrf = VrfProof(privateKeyBytes, epochNonce, slot)
 
     val testProof = vrf.testProof
 
-    val threshold = getThreshold(relativeStake, mF(slotDiff), convergentAccuracy)
+    val threshold = getThreshold(relativeStake, slotDiff)
     val isSlotLeader = isSlotLeaderForThreshold(threshold) _
 
-    // TODO: add meta info
-    if (isSlotLeader(testProof, parentProof, slotDiff, lddCutoff, hash))
+    if (isSlotLeader(testProof, parentProof, slotDiff, lddCutoff, vrf.hash))
+      // TODO: add meta info
       Some(Hit(Certificate(key.publicKey, vrf.testProofHashed, testProof, threshold, ""), vrf.nonceProof))
     else None
   }
 
   // coefficient log(1-f(slot-parentSlot))
-  def mF(slotDiff: Int): Ratio = {
-    Ratio(1, 2)
-  }
+  def mF(slotDiff: Int): Ratio = ProsomoMath.logOneMinus(slotDiff)
 
-  def getThreshold(relativeStake: Ratio, mF: Ratio, convergentAccuracy: Int = 16): Ratio =
-    (1 to convergentAccuracy)
+  def getThreshold(relativeStake: Ratio, slotDiff: Int): Ratio = {
+    val mFValue = mF(slotDiff)
+    val base = mFValue * relativeStake
+
+    (1 to precision)
       .foldLeft(Ratio(0)) { (total, i) =>
         // TODO: digest and verify what this is doing
-        total - (mF * relativeStake).pow(i) * Ratio(BigInt(1), ProsomoMath.factorial(i))
+        total - (base.pow(i) * Ratio(BigInt(1), ProsomoMath.factorial(i)))
       }
+  }
 
   def isSlotLeaderForThreshold(threshold: Ratio)(
     testProof: Proof,
     parentProof: Proof,
     slotDiff: Int,
     lddCutoff: Int,
-    hash: Array[Byte] => Array[Byte]
+    hash: Array[Byte] => Hash
   ): Boolean = {
 
     // I don't understand what this is for
@@ -120,55 +148,67 @@ object LeaderElectionProsomo extends Logging {
 
   }
 
-  trait VrfProof {
-    def testProof: Proof
-    def testProofHashed: Proof
-    def nonceProof: Proof
+  case class VrfProof(vrf: Vrf, proofFunc: String => Proof) {
+    lazy val testProof: Proof = proofFunc("TEST")
+    lazy val nonceProof: Proof = proofFunc("NONCE")
+    lazy val testProofHashed: Proof = testProof
+
+    def hash(input: Array[Byte]): Hash = vrf.Sha512(input)
+  }
+
+  object VrfProof {
+    def apply(secret: SecretKey, epochNonce: Nonce, slot: Slot): VrfProof = {
+      val vrf = new Vrf()
+      VrfProof(
+        vrf,
+        (token: String) =>
+          vrf.vrfProof(secret, epochNonce ++ secret ++ BigInt(slot).toByteArray ++ token.getBytes))
+    }
   }
 }
 
 object LeaderElectionTester extends App {
-  val random = new Random(1)
+  val random = new Random(10)
   def randomBytes(l: Int): Array[Byte] = Array.fill(l)((random.nextInt(256) - 128).toByte)
-
-  val vrf = new Vrf()
 
   val key = {
     val k = PrivateKeyCurve25519.secretGenerator.generateSecret(randomBytes(Curve25519.KeyLength))
     LeaderElectionProsomo.Key(k._2.bytes, k._1.bytes)
   }
-  val relativeStake = Ratio(1, 2)
-  val slot = 20
-  val slotDiff = 10
+
+  val relativeStake = Ratio(2, 5)
   val parentProof = randomBytes(32)
-  val vrfBuilder =
-    (secretKey: LeaderElectionProsomo.SecretKey, slot: LeaderElectionProsomo.Slot, eta: LeaderElectionProsomo.Nonce) =>
-        new LeaderElectionProsomo.VrfProof {
-          val proof: String => LeaderElectionProsomo.Proof = (customizer: String) =>
-            vrf.vrfProof(secretKey.tail, eta ++ secretKey ++ BigInt(slot).toByteArray ++ customizer.getBytes)
+  val epochNonce = randomBytes(32)
 
-          val testProof: LeaderElectionProsomo.Proof = proof("TEST")
-          val testProofHashed: Array[Byte] = vrf.vrfProofToHash(testProof)
-          val nonceProof: LeaderElectionProsomo.Proof = proof("NONCE")
-        }
-  val eta = randomBytes(32)
+  // test slots 101 - 200
+  // parent has slot 100
+  (1 to 20).foreach(x => {
+    println(x)
+    println(LeaderElectionProsomo.getThreshold(relativeStake, x).toDouble)
 
-  val result = LeaderElectionProsomo.getHit(
-    key,
-    relativeStake,
-    slot,
-    slotDiff,
-    parentProof,
-    vrfBuilder,
-    vrf.Sha512,
-    eta
-  )
+    val result = LeaderElectionProsomo.getHit(
+      key,
+      relativeStake,
+      100 + x,
+      x,
+      parentProof,
+      epochNonce
+    )
 
-  println(result)
+    println(result)
+  })
+
+  println(ProsomoMath.logOneMinus(Ratio(1, 2)).toDouble)
 }
 
 object ProsomoMath {
-  
+
+  val precision = 16
+
   def factorial(n: Int): BigInt = (1 to n).product
+
+  /** Calculates log(1-f) */
+  def logOneMinus(f: Ratio): Ratio =
+    (1 to precision).foldLeft(Ratio(0))((total, value) => total - (f.pow(value) / value))
 
 }
