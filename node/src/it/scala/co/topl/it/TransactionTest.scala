3b9ad0708d582d94b16a5178c174d81274917eba
Sean Cheatham <seancheatham@gmail.com>
2021-04-23 10:24:39 -0400

    Comments and cleanup of TransactionTest

--- node/src/it/scala/co/topl/it/TransactionTest.scala
+++ node/src/it/scala/co/topl/it/TransactionTest.scala
@@ -17,6 +17,14 @@ import org.scalatest.matchers.should.Matchers
 import scala.concurrent.duration._
 import scorex.crypto.hash.Blake2b256
 
+/**
+  * 1. Start a node from genesis
+  * 2. Extract the 10 keys from genesis.  Also generate the same 10 keys in a local keyring using the same seed.
+  * 3. Send polys from address "A" to address "B".  Verify balance change.  Verify no change for address "C".
+  * 4. Generate a new key in the local keyring.  (Address "D")
+  * 5. Send polys from address "B" to address "D".  Verify balance change.
+  * 6. Send polys from address "D" to a burn address (doesn't exist in any keyring).  Verify balance change.
+  */
 class TransactionTest
     extends AnyFreeSpec
     with Matchers
@@ -25,6 +33,8 @@ class TransactionTest
     with EitherValues
     with TryValues {
 
+  private val nodeGroupName = "TransactionTest"
+
   private val keyRing: KeyRing[PrivateKeyCurve25519, KeyfileCurve25519] =
     KeyRing.empty[PrivateKeyCurve25519, KeyfileCurve25519]()(
       networkPrefix,
@@ -32,16 +42,23 @@ class TransactionTest
       KeyfileCurve25519Companion
     )
 
-  private val nodeGroupName = "TransactionTest"
+  keyRing.generateNewKeyPairs(10, Some(nodeGroupName))
+
+  private val nodeConfig =
+    ConfigFactory.parseString(
+      raw"""bifrost.network.knownPeers = []
+           |bifrost.rpcApi.namespaceSelector.debug = true
+           |bifrost.forging.privateTestnet.genesisSeed = "$nodeGroupName"
+           |""".stripMargin
+    )
+
+  private val burnAddress = {
+    val bytes: Array[Byte] = Array(networkPrefix, PublicKeyPropositionCurve25519.typePrefix) ++ Array.fill(32)(2: Byte)
+    val checksum = Blake2b256(bytes).take(AddressEncoder.checksumLength)
+    AddressEncoder.validateAddress(bytes ++ checksum, networkPrefix).value
+  }
 
   "A single node can handle transactions" in {
-    val nodeConfig =
-      ConfigFactory.parseString(
-        raw"""bifrost.network.knownPeers = []
-             |bifrost.rpcApi.namespaceSelector.debug = true
-             |bifrost.forging.privateTestnet.genesisSeed = "$nodeGroupName"
-             |""".stripMargin
-      )
     val node: BifrostDockerNode =
       dockerSupport.createNode("bifrostTestNode", nodeGroupName)
 
@@ -52,53 +69,44 @@ class TransactionTest
     node.waitForStartup().futureValue(Timeout(30.seconds)).value
 
     logger.info("Fetching addresses from Node")
-    val addresses: List[Address] =
+    val addresses @ addressA :: addressB :: addressC :: _: List[Address] =
       node.run(ToplRpc.Admin.ListOpenKeyfiles.rpc)(ToplRpc.Admin.ListOpenKeyfiles.Params()).value.unlocked.toList
 
-    clearKeyRing()
-    genKeys()
+    keyRing.addresses shouldBe addresses.toSet
 
     sendAndAwaitTransaction(node)(
       name = "tx1",
-      sender = NonEmptyChain(addresses(0)),
-      recipients = NonEmptyChain((addresses(1), 10)),
-      changeAddress = addresses(0)
+      sender = NonEmptyChain(addressA),
+      recipients = NonEmptyChain((addressB, 10)),
+      changeAddress = addressA
     )
 
-    balanceFor(node)(addresses(0)).Balances.Polys shouldBe Int128(999990)
-    balanceFor(node)(addresses(1)).Balances.Polys shouldBe Int128(1000010)
-    balanceFor(node)(addresses(2)).Balances.Polys shouldBe Int128(1000000)
+    balanceFor(node)(addressA).Balances.Polys shouldBe Int128(999990)
+    balanceFor(node)(addressB).Balances.Polys shouldBe Int128(1000010)
+    balanceFor(node)(addressC).Balances.Polys shouldBe Int128(1000000)
 
-    val newKey = keyRing.generateNewKeyPairs().success.value.head
+    val addressD = keyRing.generateNewKeyPairs().success.value.head
 
     sendAndAwaitTransaction(node)(
       name = "tx2",
-      sender = NonEmptyChain(addresses(0)),
-      recipients = NonEmptyChain((newKey.publicImage.address, 10)),
-      changeAddress = addresses(0)
+      sender = NonEmptyChain(addressA),
+      recipients = NonEmptyChain((addressD.publicImage.address, 10)),
+      changeAddress = addressA
     )
 
-    balanceFor(node)(addresses(0)).Balances.Polys shouldBe Int128(999980)
-    balanceFor(node)(newKey.publicImage.address).Balances.Polys shouldBe Int128(10)
-
-    val burnAddress = {
-      val bytes: Array[Byte] = Array(networkPrefix, PublicKeyPropositionCurve25519.typePrefix) ++ Array.fill(32)(2: Byte)
-      val checksum = Blake2b256(bytes).take(AddressEncoder.checksumLength)
-      AddressEncoder.validateAddress(bytes ++ checksum, networkPrefix).value
-    }
+    balanceFor(node)(addressA).Balances.Polys shouldBe Int128(999980)
+    balanceFor(node)(addressD.publicImage.address).Balances.Polys shouldBe Int128(10)
 
     sendAndAwaitTransaction(node)(
       name = "tx3",
-      sender = NonEmptyChain(newKey.publicImage.address),
+      sender = NonEmptyChain(addressD.publicImage.address),
       recipients = NonEmptyChain((burnAddress, 10)),
-      changeAddress = addresses(0)
+      changeAddress = addressA
     )
 
     balanceFor(node)(burnAddress).Balances.Polys shouldBe Int128(10)
+    balanceFor(node)(addressD.publicImage.address).Balances.Polys shouldBe Int128(0)
   }
-
-  private def genKeys(): Unit = keyRing.generateNewKeyPairs(10, Some(nodeGroupName))
-  private def clearKeyRing(): Unit = keyRing.addresses.map(keyRing.removeFromKeyring)
 
   private def awaitBlocks(node: BifrostDockerNode)(count: Int): Unit = {
     val currentHeight =
@@ -106,7 +114,6 @@ class TransactionTest
 
     logger.info(s"Waiting $count blocks")
     node.pollUntilHeight(currentHeight + count).futureValue(Timeout((count * 4).seconds)).value
-
   }
 
   private def sendAndAwaitTransaction(node: BifrostDockerNode)(
