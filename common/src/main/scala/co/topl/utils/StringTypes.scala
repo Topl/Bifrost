3f3ffb496d26abe9900b47d3226cf0e991296ff3
Branden Vennes <b.n.vennes@gmail.com>
2021-05-18 12:02:56 -0700

    Return Validated in StringTypes #1308

--- common/src/main/scala/co/topl/utils/StringTypes.scala
+++ common/src/main/scala/co/topl/utils/StringTypes.scala
@@ -1,6 +1,6 @@
 package co.topl.utils
 
-import cats.data.{Validated, ValidatedNec}
+import cats.data.{NonEmptyChain, Validated, ValidatedNec}
 import cats.implicits._
 import cats.{Eq, Show}
 import co.topl.utils.Extensions.StringOps
@@ -14,20 +14,20 @@ import scala.language.implicitConversions
 
 object StringTypes {
 
-  type ValidationResult[A] = ValidatedNec[StringValidationFailure, A]
+  type StringValidationResult[A] = ValidatedNec[StringValidationFailure, A]
 
   @newtype
   class UTF8String(val value: String)
 
   object UTF8String {
 
-    def validated(from: String): ValidationResult[UTF8String] =
-      Either.cond(from.getValidUTF8Bytes.isDefined, from.coerce, InvalidCharacterSet())
+    def validated(from: String): StringValidationResult[UTF8String] =
+      Validated.condNec(from.getValidUTF8Bytes.isDefined, from.coerce, InvalidCharacterSet())
 
     def unsafe(from: String): UTF8String = from.coerce
   }
 
-  trait UTF8StringImplicits {
+  trait UTF8StringInstances {
     implicit val eqUTF8String: Eq[UTF8String] = (a: UTF8String, b: UTF8String) => a.value === b.value
 
     implicit val showUTF8String: Show[UTF8String] = (value: UTF8String) => value.value
@@ -35,7 +35,7 @@ object StringTypes {
     implicit val jsonUTF8StringEncoder: Encoder[UTF8String] = (t: UTF8String) => t.value.asJson
 
     implicit val jsonUTF8StringDecoder: Decoder[UTF8String] =
-      Decoder.decodeString.emap(UTF8String.validated(_).leftMap(_ => "Value is not a UTF-8 string"))
+      Decoder.decodeString.emap(UTF8String.validated(_).toEither.leftMap(_ => "Value is not a UTF-8 string"))
 
     implicit val keyEncodeUTF8String: KeyEncoder[UTF8String] = showUTF8String.show
 
@@ -47,13 +47,13 @@ object StringTypes {
 
   object Latin1String {
 
-    def validated(from: String): ValidationResult[Latin1String] =
-      Either.cond(from.getValidLatin1Bytes.isDefined, from.coerce, InvalidCharacterSet())
+    def validated(from: String): StringValidationResult[Latin1String] =
+      Validated.condNec(from.getValidLatin1Bytes.isDefined, from.coerce, InvalidCharacterSet())
 
     def unsafe(from: String): Latin1String = from.coerce
   }
 
-  trait Latin1StringImplicits {
+  trait Latin1StringInstances {
     implicit val eqLatin1String: Eq[Latin1String] = (a: Latin1String, b: Latin1String) => a.value === b.value
 
     implicit val showLatin1String: Show[Latin1String] = (value: Latin1String) => value.value
@@ -61,7 +61,7 @@ object StringTypes {
     implicit val encodeLatin1String: Encoder[Latin1String] = (t: Latin1String) => t.value.asJson
 
     implicit val decodeLatin1String: Decoder[Latin1String] =
-      Decoder.decodeString.emap(Latin1String.validated(_).leftMap(_ => "Value is not a Latin-1 string"))
+      Decoder.decodeString.emap(Latin1String.validated(_).toEither.leftMap(_ => "Value is not a Latin-1 string"))
 
     implicit val keyEncodeLatin1String: KeyEncoder[Latin1String] = showLatin1String.show
 
@@ -73,17 +73,17 @@ object StringTypes {
 
   object Base58String {
 
-    def validated(from: String): ValidationResult[Base58String] =
-      for {
-        validUtf8 <- UTF8String.validated(from)
+    def validated(from: String): StringValidationResult[Base58String] =
+      (for {
+        validUtf8 <- UTF8String.validated(from).toEither
         isValidBase58 = Base58.isValidBase58(validUtf8)
-        validBase58 <- Either.cond(isValidBase58, validUtf8.coerce, InvalidCharacterSet())
-      } yield validBase58
+        validBase58 <- Either.cond(isValidBase58, validUtf8.coerce, NonEmptyChain(InvalidCharacterSet()))
+      } yield validBase58).toValidated
 
     def unsafe(from: String): Base58String = UTF8String.unsafe(from).coerce
   }
 
-  trait Base58StringImplicits {
+  trait Base58StringInstances {
 
     implicit val eqBase58String: Eq[Base58String] = (a: Base58String, b: Base58String) =>
       a.value.value === b.value.value
@@ -94,7 +94,7 @@ object StringTypes {
 
     implicit val decodeBase58String: Decoder[Base58String] =
       Decoder.decodeString
-        .emap(Base58String.validated(_).leftMap(_ => "Value is not Base 58"))
+        .emap(Base58String.validated(_).toEither.leftMap(_ => "Value is not Base 58"))
 
     implicit val keyEncodeBase58String: KeyEncoder[Base58String] = showBase58String.show
 
@@ -106,19 +106,19 @@ object StringTypes {
 
   object Base16String {
 
-    def validated(from: String): ValidationResult[Base16String] =
-      for {
-        validUtf8 <- UTF8String.validated(from)
-        isValidBase16 = Base16.isValidBase16(validUtf8)
-        validBase16 <- Either.cond(isValidBase16, validUtf8.coerce, InvalidCharacterSet())
-      } yield validBase16
+    def validated(from: String): StringValidationResult[Base16String] =
+      (for {
+        validUtf8 <- UTF8String.validated(from).toEither
+        isValidBase16 = Base16.isValid(validUtf8)
+        validBase16 <- Either.cond(isValidBase16, validUtf8.coerce, NonEmptyChain(InvalidCharacterSet()))
+      } yield validBase16).toValidated
 
-    def validated(from: Array[Char]): ValidationResult[Base16String] = validated(new String(from))
+    def validated(from: Array[Char]): StringValidationResult[Base16String] = validated(new String(from))
 
     def unsafe(from: String): Base16String = UTF8String.unsafe(from).coerce
   }
 
-  trait Base16StringImplicits {
+  trait Base16StringInstances {
 
     implicit val eqBase16String: Eq[Base16String] = (a: Base16String, b: Base16String) =>
       a.value.value === b.value.value
@@ -128,7 +128,7 @@ object StringTypes {
     implicit val encodeBase16String: Encoder[Base16String] = (t: Base16String) => t.value.value.asJson
 
     implicit val decodeBase16String: Decoder[Base16String] =
-      Decoder.decodeString.emap(Base16String.validated(_).leftMap(_ => "Value is not a Base 16 string"))
+      Decoder.decodeString.emap(Base16String.validated(_).toEither.leftMap(_ => "Value is not a Base 16 string"))
 
     implicit val keyEncodeBase16String: KeyEncoder[Base16String] = showBase16String.show
 
@@ -139,9 +139,9 @@ object StringTypes {
   final case class InvalidCharacterSet() extends StringValidationFailure
 
   object implicits
-      extends UTF8StringImplicits
-      with Latin1StringImplicits
-      with Base58StringImplicits
-      with Base16StringImplicits
+      extends UTF8StringInstances
+      with Latin1StringInstances
+      with Base58StringInstances
+      with Base16StringInstances
 
 }
