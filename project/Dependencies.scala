e06184f6991750627a8ecaf037abccdda540b4c9
Scala Steward <me@scala-steward.org>
2021-06-16 17:35:20 +0200

    Update scala-logging to 3.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val graalVersion = "21.1.0"
 
   val logging = Seq(
-    "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.3",
+    "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.4",
     "ch.qos.logback"              % "logback-classic" % "1.2.3",
     "ch.qos.logback"              % "logback-core"    % "1.2.3",
     "org.slf4j"                   % "slf4j-api"       % "1.7.30"
