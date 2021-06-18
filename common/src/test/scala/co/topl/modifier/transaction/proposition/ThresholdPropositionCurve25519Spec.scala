6c8e9d4a968b9140729dccfe1b42eb9da849e5fa
jingsting <jingfei1024@gmail.com>
2021-06-18 10:21:47 -0500

    Formatting

--- common/src/test/scala/co/topl/modifier/transaction/proposition/ThresholdPropositionCurve25519Spec.scala
+++ common/src/test/scala/co/topl/modifier/transaction/proposition/ThresholdPropositionCurve25519Spec.scala
@@ -14,13 +14,14 @@ class ThresholdPropositionCurve25519Spec
     with CommonGenerators {
 
   property("Any signature from set validates") {
-    forAll(oneOfNPropositionCurve25519Gen) { case (keySet: Set[PrivateKeyCurve25519], mn: ThresholdPropositionCurve25519) =>
-      val message = nonEmptyBytesGen.sample.getOrElse(Array.fill(positiveMediumIntGen.sample.get)(1: Byte))
-      val signatures = keySet.map(_.sign(message))
+    forAll(oneOfNPropositionCurve25519Gen) {
+      case (keySet: Set[PrivateKeyCurve25519], mn: ThresholdPropositionCurve25519) =>
+        val message = nonEmptyBytesGen.sample.getOrElse(Array.fill(positiveMediumIntGen.sample.get)(1: Byte))
+        val signatures = keySet.map(_.sign(message))
 
-      signatures
-        .map(s => ThresholdSignatureCurve25519(Set(s)))
-        .forall(_.isValid(mn, message)) shouldBe true
+        signatures
+          .map(s => ThresholdSignatureCurve25519(Set(s)))
+          .forall(_.isValid(mn, message)) shouldBe true
     }
   }
 }
