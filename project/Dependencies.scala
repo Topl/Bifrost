d8001d8b48442f8c601a8528825230bbdd25818d
Scala Steward <me@scala-steward.org>
2021-06-18 08:45:31 +0200

    Update slf4j-api to 1.7.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.3",
     "ch.qos.logback"              % "logback-classic" % "1.2.3",
     "ch.qos.logback"              % "logback-core"    % "1.2.3",
-    "org.slf4j"                   % "slf4j-api"       % "1.7.30"
+    "org.slf4j"                   % "slf4j-api"       % "1.7.31"
   )
 
   val test = Seq(
