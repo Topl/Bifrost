e7ad53ad88eb653ebee70914c62672acd2d289f2
jingsting <jingfei1024@gmail.com>
2021-06-22 13:41:52 -0500

    Test both type of addresses in utilsRPC

--- node/src/test/scala/co/topl/api/UtilsRPCSpec.scala
+++ node/src/test/scala/co/topl/api/UtilsRPCSpec.scala
@@ -20,11 +20,13 @@ import scala.util.{Failure, Success}
 class UtilsRPCSpec extends AnyWordSpec with Matchers with RPCMockState with EitherValues {
 
   val seedLength: Int = 10
-  var address: Address = _
+  var addressCurve25519: Address = _
+  var addressEd25519: Address = _
 
   override def beforeAll(): Unit = {
     super.beforeAll()
-    address = keyRingCurve25519.addresses.head
+    addressCurve25519 = keyRingCurve25519.addresses.head
+    addressEd25519 = keyRingEd25519.addresses.head
   }
 
   "Utils RPC" should {
@@ -100,120 +102,145 @@ class UtilsRPCSpec extends AnyWordSpec with Matchers with RPCMockState with Eith
     }
 
     "Generate AssetCode with given issuer and shortName" in {
-      val requestBody = ByteString(s"""
-           |{
-           |   "jsonrpc": "2.0",
-           |   "id": "1",
-           |   "method": "util_generateAssetCode",
-           |   "params": [{
-           |      "version": 1,
-           |      "issuer": "$address",
-           |      "shortName": "testcode"
-           |   }]
-           |}
-      """.stripMargin)
+      testGenerateAssetCode(addressCurve25519)
+      testGenerateAssetCode(addressEd25519)
 
-      httpPOST(requestBody) ~> route ~> check {
-        val res: Json = parse(responseAs[String]).value
-        val oldAssetCode: AssetCode = AssetCode(1: Byte, address, "testcode")
+      def testGenerateAssetCode(address: Address): Unit = {
+        val requestBody = ByteString(s"""
+          |{
+          |   "jsonrpc": "2.0",
+          |   "id": "1",
+          |   "method": "util_generateAssetCode",
+          |   "params": [{
+          |      "version": 1,
+          |      "issuer": "$address",
+          |      "shortName": "testcode"
+          |   }]
+          |}
+        """.stripMargin)
 
-        val genAssetCode: AssetCode = res.hcursor.downField("result").get[AssetCode]("assetCode").value
+        httpPOST(requestBody) ~> route ~> check {
+          val res: Json = parse(responseAs[String]).value
+          val oldAssetCode: AssetCode = AssetCode(1: Byte, address, "testcode")
 
-        res.hcursor.downField("error").values shouldBe None
-        oldAssetCode.toString shouldEqual genAssetCode.toString
+          val genAssetCode: AssetCode = res.hcursor.downField("result").get[AssetCode]("assetCode").value
+
+          res.hcursor.downField("error").values shouldBe None
+          oldAssetCode.toString shouldEqual genAssetCode.toString
+        }
       }
     }
 
     "Return the same address and network if the given address and network type are valid and matching" in {
-      val requestBody = ByteString(s"""
-           |{
-           |   "jsonrpc": "2.0",
-           |   "id": "1",
-           |   "method": "util_checkValidAddress",
-           |   "params": [{
-           |      "network": "private",
-           |      "address": "$address"
-           |   }]
-           |}
-      """.stripMargin)
+      testCheckValidAddress(addressCurve25519)
+      testCheckValidAddress(addressEd25519)
 
-      httpPOST(requestBody) ~> route ~> check {
-        val res: Json = parse(responseAs[String]).value
-        val resAddress = res.hcursor.downField("result").get[Address]("address").value
-        val network = res.hcursor.downField("result").get[String]("network").value
+      def testCheckValidAddress(address: Address): Unit = {
+        val requestBody = ByteString(s"""
+          |{
+          |   "jsonrpc": "2.0",
+          |   "id": "1",
+          |   "method": "util_checkValidAddress",
+          |   "params": [{
+          |      "network": "private",
+          |      "address": "$address"
+          |   }]
+          |}
+        """.stripMargin)
 
-        res.hcursor.downField("error").values shouldBe None
-        network shouldEqual "private"
-        resAddress shouldEqual address
+        httpPOST(requestBody) ~> route ~> check {
+          val res: Json = parse(responseAs[String]).value
+          val resAddress = res.hcursor.downField("result").get[Address]("address").value
+          val network = res.hcursor.downField("result").get[String]("network").value
+
+          res.hcursor.downField("error").values shouldBe None
+          network shouldEqual "private"
+          resAddress shouldEqual address
+        }
       }
     }
 
     "Returns the address and the current network type, which should be private for tests, if only address is given" in {
-      val requestBody = ByteString(s"""
-           |{
-           |   "jsonrpc": "2.0",
-           |   "id": "1",
-           |   "method": "util_checkValidAddress",
-           |   "params": [{
-           |      "address": "$address"
-           |   }]
-           |}
-      """.stripMargin)
+      testCheckValidAddress(addressCurve25519)
+      testCheckValidAddress(addressEd25519)
 
-      httpPOST(requestBody) ~> route ~> check {
-        val res: Json = parse(responseAs[String]).value
-        val network = res.hcursor.downField("result").get[String]("network").value
-        val resAddress = res.hcursor.downField("result").get[Address]("address").value
+      def testCheckValidAddress(address: Address): Unit = {
+        val requestBody = ByteString(s"""
+          |{
+          |   "jsonrpc": "2.0",
+          |   "id": "1",
+          |   "method": "util_checkValidAddress",
+          |   "params": [{
+          |      "address": "$address"
+          |   }]
+          |}
+        """.stripMargin)
 
-        res.hcursor.downField("error").values shouldBe None
-        network shouldEqual "private"
-        resAddress shouldEqual address
+        httpPOST(requestBody) ~> route ~> check {
+          val res: Json = parse(responseAs[String]).value
+          val network = res.hcursor.downField("result").get[String]("network").value
+          val resAddress = res.hcursor.downField("result").get[Address]("address").value
+
+          res.hcursor.downField("error").values shouldBe None
+          network shouldEqual "private"
+          resAddress shouldEqual address
+        }
       }
     }
 
     "Complain that the network type doesn't match if the received address and network type are not matching" in {
-      val requestBody = ByteString(s"""
-           |{
-           |   "jsonrpc": "2.0",
-           |   "id": "1",
-           |   "method": "util_checkValidAddress",
-           |   "params": [{
-           |      "network": "toplnet",
-           |      "address": "$address"
-           |   }]
-           |}
-      """.stripMargin)
+      testCheckValidAddress(addressCurve25519)
+      testCheckValidAddress(addressEd25519)
 
-      httpPOST(requestBody) ~> route ~> check {
-        val res: Json = parse(responseAs[String]).value
-        val code = res.hcursor.downField("error").get[Int]("code").value
-        val message = res.hcursor.downField("error").get[String]("message").value
+      def testCheckValidAddress(address: Address): Unit = {
+        val requestBody = ByteString(s"""
+          |{
+          |   "jsonrpc": "2.0",
+          |   "id": "1",
+          |   "method": "util_checkValidAddress",
+          |   "params": [{
+          |      "network": "toplnet",
+          |      "address": "$address"
+          |   }]
+          |}
+        """.stripMargin)
 
-        code shouldEqual InvalidParametersError.Code
-        message shouldEqual InvalidParametersError.Message
+        httpPOST(requestBody) ~> route ~> check {
+          val res: Json = parse(responseAs[String]).value
+          val code = res.hcursor.downField("error").get[Int]("code").value
+          val message = res.hcursor.downField("error").get[String]("message").value
+
+          code shouldEqual InvalidParametersError.Code
+          message shouldEqual InvalidParametersError.Message
+        }
       }
     }
 
     "Reject request if the network type doesn't exist" in {
-      val requestBody = ByteString(s"""
-           |{
-           |   "jsonrpc": "2.0",
-           |   "id": "1",
-           |   "method": "util_checkValidAddress",
-           |   "params": [{
-           |      "network": "nonexistentnetwork",
-           |      "address": "$address"
-           |   }]
-           |}
-      """.stripMargin)
+      testCheckValidAddress(addressCurve25519)
+      testCheckValidAddress(addressEd25519)
 
-      httpPOST(requestBody) ~> route ~> check {
-        val res: Json = parse(responseAs[String]).value
-        val code = res.hcursor.downField("error").get[Int]("code").value
-        val message = res.hcursor.downField("error").get[String]("message").value
+      def testCheckValidAddress(address: Address): Unit = {
+        val requestBody = ByteString(s"""
+          |{
+          |   "jsonrpc": "2.0",
+          |   "id": "1",
+          |   "method": "util_checkValidAddress",
+          |   "params": [{
+          |      "network": "nonexistentnetwork",
+          |      "address": "$address"
+          |   }]
+          |}
+        """.stripMargin)
 
-        code shouldEqual ToplRpcErrors.InvalidNetworkSpecified.code
-        message shouldEqual ToplRpcErrors.InvalidNetworkSpecified.message
+        httpPOST(requestBody) ~> route ~> check {
+          val res: Json = parse(responseAs[String]).value
+          val code = res.hcursor.downField("error").get[Int]("code").value
+          val message = res.hcursor.downField("error").get[String]("message").value
+
+          code shouldEqual ToplRpcErrors.InvalidNetworkSpecified.code
+          message shouldEqual ToplRpcErrors.InvalidNetworkSpecified.message
+        }
       }
     }
   }
