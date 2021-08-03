bf4e1d6695ea6a04bc97e1da5343cf76ec2775c4
Scala Steward <me@scala-steward.org>
2021-08-03 23:43:23 +0200

    Update scalactic to 3.2.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
 
   val test = Seq(
     "org.scalatest"      %% "scalatest"         % "3.2.8"   % "test",
-    "org.scalactic"      %% "scalactic"         % "3.2.8"   % "test",
+    "org.scalactic"      %% "scalactic"         % "3.2.9"   % "test",
     "org.scalacheck"     %% "scalacheck"        % "1.15.4"  % "test",
     "org.scalatestplus"  %% "scalacheck-1-14"   % "3.2.2.0" % "test",
     "com.spotify"         % "docker-client"     % "8.16.0"  % "test",
