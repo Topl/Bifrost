86ac1ee2516f5453f7d32c07a165bc54b7377186
Scala Steward <me@scala-steward.org>
2021-08-15 03:18:31 +0200

    Update breeze to 1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,7 @@ object Dependencies {
   val misc = Seq(
     "com.chuusai"     %% "shapeless" % "2.3.5",
     "com.iheart"      %% "ficus"     % "1.5.0",
-    "org.scalanlp"    %% "breeze"    % "1.1",
+    "org.scalanlp"    %% "breeze"    % "1.3",
     "io.netty"         % "netty"     % "3.10.6.Final",
     "com.google.guava" % "guava"     % "30.1.1-jre",
     "io.estatico"     %% "newtype"   % "0.4.4"
