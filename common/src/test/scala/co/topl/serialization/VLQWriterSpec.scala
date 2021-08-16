23c818c5458d66bbd2b3d190ae4da1f1b2007287
Branden Vennes <b.vennes@topl.me>
2021-08-16 15:30:23 -0500

    Adds more tests to VLQReader/VLQWriter specs #1472

--- common/src/test/scala/co/topl/serialization/VLQWriterSpec.scala
+++ common/src/test/scala/co/topl/serialization/VLQWriterSpec.scala
@@ -1,35 +1,97 @@
 package co.topl.serialization
 
 import co.topl.utils.CommonGenerators
-import co.topl.utils.serialization.{stringCharacterSet, VLQByteStringWriter}
+import co.topl.utils.serialization.Writer.Aux
+import co.topl.utils.serialization.{stringCharacterSet, VLQByteStringWriter, VLQWriter}
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-class VLQWriterSpec extends AnyFlatSpec with Matchers with ScalaCheckDrivenPropertyChecks with CommonGenerators {
+import java.nio.charset.StandardCharsets
+
+class VLQWriterSpec
+    extends AnyFlatSpec
+    with MockFactory
+    with Matchers
+    with ScalaCheckDrivenPropertyChecks
+    with CommonGenerators {
+
+  val mockVlqWriter: VLQWriter = new VLQWriter {
+    override def toBytes: Array[Byte] = ???
+    override type CH = this.type
+    override def length(): Int = ???
+    override def newWriter(): Aux[this.type] = ???
+    override def putChunk(chunk: this.type): this.type = this
+    override def put(x:          Byte): this.type = this
+    override def putBoolean(x:   Boolean): this.type = this
+    override def putBytes(xs:    Array[Byte]): this.type = this
+    override def result(): this.type = this
+  }
 
   "VLQWriter putByteString" should "throw an illegal argument exception when input bytes is length 256" in {
-    val vlqWriter = new VLQByteStringWriter()
     val inputString = new String(Array.fill(256)(0x02).map(_.toByte), stringCharacterSet)
 
-    an[IllegalArgumentException] should be thrownBy vlqWriter.putByteString(inputString)
+    an[IllegalArgumentException] should be thrownBy mockVlqWriter.putByteString(inputString)
   }
 
   it should "not throw an illegal argument exception when input bytes is length 255" in {
-    val vlqWriter = new VLQByteStringWriter()
     val inputString = new String(Array.fill(255)(0x01).map(_.toByte), stringCharacterSet)
 
     // should not throw
-    vlqWriter.putByteString(inputString)
+    mockVlqWriter.putByteString(inputString)
+  }
+
+  it should "save bytes in UTF-8 format" in {
+    val inputString = "test"
+
+    val writer: VLQWriter = new VLQWriter {
+      override def toBytes: Array[Byte] = ???
+      override type CH = this.type
+      override def length(): Int = ???
+      override def newWriter(): Aux[this.type] = ???
+      override def putChunk(chunk: this.type): this.type = this
+      override def put(x:          Byte): this.type = this
+      override def putBoolean(x:   Boolean): this.type = this
+      override def putBytes(xs: Array[Byte]): this.type = {
+        new String(xs, StandardCharsets.UTF_8) shouldBe inputString
+        this
+      }
+      override def result(): this.type = this
+    }
+
+    writer.putByteString(inputString)
   }
 
   "VLQWriter putIntString"
     .should("not throw an illegal argument exception when input bytes size is 5000")
     .in {
-      val vlqWriter = new VLQByteStringWriter()
       val inputString = new String(Array.fill(5000)(0x04).map(_.toByte), stringCharacterSet)
 
       // should not throw
-      vlqWriter.putIntString(inputString)
+      mockVlqWriter.putIntString(inputString)
     }
+
+  it should "save bytes in UTF-8 format" in {
+    val inputString = "test"
+
+    val writer: VLQWriter = new VLQWriter {
+      override def toBytes: Array[Byte] = ???
+      override type CH = this.type
+      override def length(): Int = ???
+      override def newWriter(): Aux[this.type] = ???
+      override def putChunk(chunk: this.type): this.type = this
+      override def put(x:          Byte): this.type = this
+      override def putBoolean(x:   Boolean): this.type = this
+      override def putBytes(xs: Array[Byte]): this.type = {
+        new String(xs, StandardCharsets.UTF_8) shouldBe inputString
+        this
+      }
+
+      override def putULong(x: Long): this.type = this
+      override def result(): this.type = this
+    }
+
+    writer.putIntString(inputString)
+  }
 }
