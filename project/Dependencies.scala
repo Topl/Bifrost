92c4b154fe0d1c67dd8115dd20c2980e5bdaebe3
Scala Steward <me@scala-steward.org>
2021-09-07 23:22:28 +0200

    Update kamon-bundle, kamon-core, ... to 2.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaVersion = "2.6.16"
   val akkaHttpVersion = "10.2.6"
   val circeVersion = "0.14.1"
-  val kamonVersion = "2.1.21"
+  val kamonVersion = "2.2.3"
   val graalVersion = "21.1.0"
 
   val logging = Seq(
