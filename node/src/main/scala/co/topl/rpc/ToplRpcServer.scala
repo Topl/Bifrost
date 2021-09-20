d199bcaa3ce2a8110c87128ec60352f04f1bba2e
Sean Cheatham <seancheatham@gmail.com>
2021-09-20 11:12:36 -0400

    Introduced RPC "Builder" pattern.  Fix node "mainClass" in build.sbt

--- node/src/main/scala/co/topl/rpc/ToplRpcServer.scala
+++ node/src/main/scala/co/topl/rpc/ToplRpcServer.scala
@@ -2,9 +2,8 @@ package co.topl.rpc
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import cats.data.NonEmptyChain
-import co.topl.akkahttprpc.implicits.server.rpcToServer
-import co.topl.akkahttprpc.{MethodNotFoundError, RpcDirectives, RpcErrorRejection, ThrowableData}
+import cats.implicits._
+import co.topl.akkahttprpc.{RpcDirectives, RpcServer, ThrowableData}
 import co.topl.rpc.handlers.ToplRpcHandlers
 import co.topl.settings._
 import co.topl.utils.NetworkType.NetworkPrefix
@@ -17,89 +16,64 @@ class ToplRpcServer(handlers: ToplRpcHandlers, appContext: AppContext)(implicit
 
   implicit val networkPrefix: NetworkPrefix = appContext.networkType.netPrefix
 
-  val debugRoutes: Option[NonEmptyChain[Route]] =
-    if (appContext.settings.rpcApi.namespaceSelector.namespaceStates(DebugNamespace))
-      Some(
-        NonEmptyChain(
-          ToplRpc.Debug.Delay.rpc.serve(handlers.debug.delay),
-          ToplRpc.Debug.MyBlocks.rpc.serve(handlers.debug.myBlocks),
-          ToplRpc.Debug.Generators.rpc.serve(handlers.debug.generators),
-          ToplRpc.Debug.IdsFromHeight.rpc.serve(handlers.debug.idsFromHeight)
-        )
-      )
-    else None
+  val debugRoutes: RpcServer.Builder =
+    if (appContext.settings.rpcApi.namespaceSelector.namespaceStates(DebugNamespace)) {
+      RpcServer.Builder.empty
+        .append(ToplRpc.Debug.Delay.rpc)(handlers.debug.delay)
+        .append(ToplRpc.Debug.MyBlocks.rpc)(handlers.debug.myBlocks)
+        .append(ToplRpc.Debug.Generators.rpc)(handlers.debug.generators)
+        .append(ToplRpc.Debug.IdsFromHeight.rpc)(handlers.debug.idsFromHeight)
+    } else RpcServer.Builder.empty
 
-  val utilRoutes: Option[NonEmptyChain[Route]] =
+  val utilRoutes: RpcServer.Builder =
     if (appContext.settings.rpcApi.namespaceSelector.namespaceStates(UtilNamespace)) {
-      Some(
-        NonEmptyChain(
-          ToplRpc.Util.Seed.rpc.serve(handlers.utils.seed),
-          ToplRpc.Util.SeedOfLength.rpc.serve(handlers.utils.seedOfLength),
-          ToplRpc.Util.CheckValidAddress.rpc.serve(handlers.utils.checkValidAddress),
-          ToplRpc.Util.GenerateAssetCode.rpc.serve(handlers.utils.generateAssetCode),
-          ToplRpc.Util.HashBlake2b256.rpc.serve(handlers.utils.hashBlake2b256)
-        )
-      )
-    } else None
+      RpcServer.Builder.empty
+        .append(ToplRpc.Util.Seed.rpc)(handlers.utils.seed)
+        .append(ToplRpc.Util.SeedOfLength.rpc)(handlers.utils.seedOfLength)
+        .append(ToplRpc.Util.CheckValidAddress.rpc)(handlers.utils.checkValidAddress)
+        .append(ToplRpc.Util.GenerateAssetCode.rpc)(handlers.utils.generateAssetCode)
+        .append(ToplRpc.Util.HashBlake2b256.rpc)(handlers.utils.hashBlake2b256)
+    } else RpcServer.Builder.empty
 
-  val nodeViewRoutes: Option[NonEmptyChain[Route]] =
+  val nodeViewRoutes: RpcServer.Builder =
     if (appContext.settings.rpcApi.namespaceSelector.namespaceStates(ToplNamespace)) {
-      Some(
-        NonEmptyChain(
-          ToplRpc.NodeView.Head.rpc.serve(handlers.nodeView.head),
-          ToplRpc.NodeView.Balances.rpc.serve(handlers.nodeView.balances),
-          ToplRpc.NodeView.TransactionById.rpc.serve(handlers.nodeView.transactionById),
-          ToplRpc.NodeView.BlockById.rpc.serve(handlers.nodeView.blockById),
-          ToplRpc.NodeView.BlockByHeight.rpc.serve(handlers.nodeView.blockByHeight),
-          ToplRpc.NodeView.Mempool.rpc.serve(handlers.nodeView.mempool),
-          ToplRpc.NodeView.TransactionFromMempool.rpc.serve(handlers.nodeView.transactionFromMempool),
-          ToplRpc.NodeView.Info.rpc.serve(handlers.nodeView.info)
-        )
-      )
-    } else None
+      RpcServer.Builder.empty
+        .append(ToplRpc.NodeView.Head.rpc)(handlers.nodeView.head)
+        .append(ToplRpc.NodeView.Balances.rpc)(handlers.nodeView.balances)
+        .append(ToplRpc.NodeView.TransactionById.rpc)(handlers.nodeView.transactionById)
+        .append(ToplRpc.NodeView.BlockById.rpc)(handlers.nodeView.blockById)
+        .append(ToplRpc.NodeView.BlockByHeight.rpc)(handlers.nodeView.blockByHeight)
+        .append(ToplRpc.NodeView.Mempool.rpc)(handlers.nodeView.mempool)
+        .append(ToplRpc.NodeView.TransactionFromMempool.rpc)(handlers.nodeView.transactionFromMempool)
+        .append(ToplRpc.NodeView.Info.rpc)(handlers.nodeView.info)
+    } else RpcServer.Builder.empty
 
-  val transactionRoutes: Option[NonEmptyChain[Route]] =
+  val transactionRoutes: RpcServer.Builder =
     if (appContext.settings.rpcApi.namespaceSelector.namespaceStates(ToplNamespace)) {
-      Some(
-        NonEmptyChain(
-          ToplRpc.Transaction.RawAssetTransfer.rpc.serve(handlers.transaction.rawAssetTransfer),
-          ToplRpc.Transaction.RawArbitTransfer.rpc.serve(handlers.transaction.rawArbitTransfer),
-          ToplRpc.Transaction.RawPolyTransfer.rpc.serve(handlers.transaction.rawPolyTransfer),
-          ToplRpc.Transaction.BroadcastTx.rpc.serve(handlers.transaction.broadcastTx)
-        )
-      )
-    } else None
+      RpcServer.Builder.empty
+        .append(ToplRpc.Transaction.RawAssetTransfer.rpc)(handlers.transaction.rawAssetTransfer)
+        .append(ToplRpc.Transaction.RawArbitTransfer.rpc)(handlers.transaction.rawArbitTransfer)
+        .append(ToplRpc.Transaction.RawPolyTransfer.rpc)(handlers.transaction.rawPolyTransfer)
+        .append(ToplRpc.Transaction.BroadcastTx.rpc)(handlers.transaction.broadcastTx)
+    } else RpcServer.Builder.empty
 
-  val adminRoutes: Option[NonEmptyChain[Route]] =
+  val adminRoutes: RpcServer.Builder =
     if (appContext.settings.rpcApi.namespaceSelector.namespaceStates(AdminNamespace)) {
-      Some(
-        NonEmptyChain(
-          ToplRpc.Admin.UnlockKeyfile.rpc.serve(handlers.admin.unlockKeyfile),
-          ToplRpc.Admin.LockKeyfile.rpc.serve(handlers.admin.lockKeyfile),
-          ToplRpc.Admin.GenerateKeyfile.rpc.serve(handlers.admin.generateKeyfile),
-          ToplRpc.Admin.ImportSeedPhrase.rpc.serve(handlers.admin.importSeedPhrase),
-          ToplRpc.Admin.ListOpenKeyfiles.rpc.serve(handlers.admin.listOpenKeyfiles),
-          ToplRpc.Admin.StartForging.rpc.serve(handlers.admin.startForging),
-          ToplRpc.Admin.StopForging.rpc.serve(handlers.admin.stopForging),
-          ToplRpc.Admin.UpdateRewardsAddress.rpc.serve(handlers.admin.updateRewardsAddress),
-          ToplRpc.Admin.GetRewardsAddress.rpc.serve(handlers.admin.getRewardsAddress)
-        )
-      )
-    } else None
+      RpcServer.Builder.empty
+        .append(ToplRpc.Admin.UnlockKeyfile.rpc)(handlers.admin.unlockKeyfile)
+        .append(ToplRpc.Admin.LockKeyfile.rpc)(handlers.admin.lockKeyfile)
+        .append(ToplRpc.Admin.GenerateKeyfile.rpc)(handlers.admin.generateKeyfile)
+        .append(ToplRpc.Admin.ImportSeedPhrase.rpc)(handlers.admin.importSeedPhrase)
+        .append(ToplRpc.Admin.ListOpenKeyfiles.rpc)(handlers.admin.listOpenKeyfiles)
+        .append(ToplRpc.Admin.StartForging.rpc)(handlers.admin.startForging)
+        .append(ToplRpc.Admin.StopForging.rpc)(handlers.admin.stopForging)
+        .append(ToplRpc.Admin.UpdateRewardsAddress.rpc)(handlers.admin.updateRewardsAddress)
+        .append(ToplRpc.Admin.GetRewardsAddress.rpc)(handlers.admin.getRewardsAddress)
+    } else RpcServer.Builder.empty
 
   val route: Route =
     handleRejections(rejectionHandler)(
-      NonEmptyChain.fromSeq(
-        (debugRoutes ++ utilRoutes ++ nodeViewRoutes ++ transactionRoutes ++ adminRoutes).toList
-          .flatMap(_.toNonEmptyList.toList)
-      ) match {
-        case Some(chain) =>
-          chain.reduceLeft(_ ~ _)
-        case _ =>
-          rpcContext { context =>
-            reject(RpcErrorRejection(MethodNotFoundError(context.method)))
-          }
-      }
+      debugRoutes.combine(utilRoutes).combine(nodeViewRoutes).combine(transactionRoutes).combine(adminRoutes).route
     )
 
 }
