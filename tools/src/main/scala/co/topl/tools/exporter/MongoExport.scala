cd08dd0a1bdf58c8ade9c1b6a4fa13b297f92c51
jingsting <jingfei1024@gmail.com>
2021-10-05 11:41:28 -0500

    Added method to get heights of the existing blocks in a range

--- tools/src/main/scala/co/topl/tools/exporter/MongoExport.scala
+++ tools/src/main/scala/co/topl/tools/exporter/MongoExport.scala
@@ -1,5 +1,7 @@
 package co.topl.tools.exporter
 
+import org.mongodb.scala.model.Filters.{and, gte, lte}
+import org.mongodb.scala.model.Projections
 import org.mongodb.scala.result.InsertManyResult
 import org.mongodb.scala.{Document, MongoClient, MongoDatabase}
 
@@ -20,6 +22,13 @@ class MongoExport(uri: String, database: String, collection: String, dt: DataTyp
   override def insert(ele: Seq[String]): Future[InsertManyResult] = db
     .getCollection(collection)
     .insertMany(ele.map(Document(_)))
+    .toFuture()
+
+  def getExistingHeights(start: Long, end: Long): Future[Seq[Long]] = db
+    .getCollection(collection)
+    .find(and(gte("height", start), lte("height", end)))
+    .projection(Projections.fields(Projections.include("height"), Projections.excludeId()))
+    .map(_.head._2.asInt32().getValue.toLong)
     .toFuture()
 
   override def close(): Unit = client.close()
