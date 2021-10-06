74adfccf76a0ef9e070747d3c4cdf466034b2a78
jingsting <jingfei1024@gmail.com>
2021-10-05 21:48:46 -0500

    Refactored some procedures into methods

--- node/src/main/scala/co/topl/nodeView/ChainReplicator.scala
+++ node/src/main/scala/co/topl/nodeView/ChainReplicator.scala
@@ -4,15 +4,18 @@ import akka.actor.typed.eventstream.EventStream
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors, TimerScheduler}
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.util.Timeout
+import cats.data.OptionT
+import cats.implicits._
 import co.topl.modifier.block.Block
 import co.topl.nodeView.NodeViewHolder.Events.SemanticallySuccessfulModifier
 import co.topl.settings.ChainReplicatorSettings
 import co.topl.tools.exporter.{DataType, MongoExport}
 import io.circe.syntax.EncoderOps
+import org.mongodb.scala.result.InsertManyResult
 import org.slf4j.Logger
 
-import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
 object ChainReplicator {
@@ -25,19 +28,7 @@ object ChainReplicator {
 
     private[nodeView] case class GotNewBlock(block: Block) extends ReceivableMessage
 
-    private[nodeView] case class ExportBlocks(blocks: Seq[Block]) extends ReceivableMessage
-
-    private[nodeView] case class InsertionSuccess(result: Int) extends ReceivableMessage
-
-    private[nodeView] case class InsertionFailure(error: String) extends ReceivableMessage
-
     private[nodeView] case class CheckMissingBlocks(maxHeight: Long) extends ReceivableMessage
-
-    private[nodeView] case class CheckHeightsSuccess(result: Seq[Long]) extends ReceivableMessage
-
-    private[nodeView] case class CheckHeightsFailure(error: String) extends ReceivableMessage
-
-    private[nodeView] case class getBlocksNodeViewFailure(error: String) extends ReceivableMessage
 
     private[nodeView] case object CheckDatabaseComplete extends ReceivableMessage
 
@@ -108,84 +99,76 @@ private class ChainReplicator(
             )
           }
         }
+
       case ReceivableMessages.Terminate(reason) =>
         throw reason
     }
 
   private def active(blockCheckStart: Long, timers: TimerScheduler[ReceivableMessage]): Behavior[ReceivableMessage] =
-    Behaviors.receiveMessagePartial[ReceivableMessage] {
-      case ReceivableMessages.GotNewBlock(block) =>
-        log.info(s"${Console.GREEN}Got new block at height: ${block.height}${Console.RESET}")
-        context.self ! ReceivableMessages.ExportBlocks(Seq(block))
+    Behaviors.receivePartial[ReceivableMessage] {
+      case (context, ReceivableMessages.GotNewBlock(block)) =>
+        implicit val ec: ExecutionContext = context.executionContext
         if (settings.checkMissingBlock)
           timers.startSingleTimer(ReceivableMessages.CheckMissingBlocks(block.height), 1.seconds)
-        Behaviors.same
-
-      case ReceivableMessages.ExportBlocks(blocks) =>
-        if (blocks.nonEmpty) {
-          val blocksString = blocks.map(_.asJson(blockEncoder).toString)
-          context.pipeToSelf(mongo.insert(blocksString)) {
-            case Success(result) => ReceivableMessages.InsertionSuccess(result.getInsertedIds.size)
-            case Failure(e)      => ReceivableMessages.InsertionFailure(e.toString)
-          }
+        exportBlocks(Seq(block)).onComplete {
+          case Success(_) => log.info(s"${Console.GREEN}Inserted new block at height: ${block.height}${Console.RESET}")
+          case Failure(e) => log.error(s"${Console.RED}$e${Console.RESET}")
         }
         Behaviors.same
 
-      case ReceivableMessages.InsertionSuccess(result) =>
-        // cleanup the log info
-        log.info(s"${Console.GREEN}Successfully inserted $result blocks into AppView${Console.RESET}")
-        Behaviors.same
-
-      case ReceivableMessages.InsertionFailure(failure) =>
-        log.error(s"${Console.RED}$failure${Console.RESET}")
-        Behaviors.same
-
-      case ReceivableMessages.CheckMissingBlocks(maxHeight) =>
+      case (context, ReceivableMessages.CheckMissingBlocks(maxHeight)) =>
+        implicit val ec: ExecutionContext = context.executionContext
         val blockCheckEnd: Long = blockCheckStart + settings.numberOfBlocksToCheck
         if (maxHeight >= blockCheckEnd) {
-          val existingHeightsFuture =
-            mongo.getExistingHeights(blockCheckStart, blockCheckEnd)
-          context.pipeToSelf(existingHeightsFuture) {
-            case Success(result) => ReceivableMessages.CheckHeightsSuccess(result)
-            case Failure(e)      => ReceivableMessages.CheckHeightsFailure(e.toString)
+          val existingHeightsFuture = mongo.getExistingHeights(blockCheckStart, blockCheckEnd)
+          val result: OptionT[Future, InsertManyResult] = for {
+            existingHeights  <- OptionT[Future, Seq[Long]](existingHeightsFuture.map(_.some))
+            blocksToAdd      <- OptionT[Future, Seq[Block]](missingBlocks(blockCheckStart, existingHeights))
+            insertManyResult <- OptionT[Future, InsertManyResult](exportBlocks(blocksToAdd).map(_.some))
+          } yield insertManyResult
+          result.value.onComplete {
+            case Success(Some(x)) =>
+              log.info(
+                s"${Console.GREEN}Successfully inserted ${x.getInsertedIds.size} blocks into AppView${Console.RESET}"
+              )
+            case Failure(err) => log.error(s"${Console.RED}$err${Console.RESET}")
+            case _            => log.info(s"${Console.GREEN}No missing blocks found${Console.RESET}")
           }
+          active(blockCheckEnd, timers)
+        } else {
+          Behaviors.same
         }
-        Behaviors.same
 
-      case ReceivableMessages.CheckHeightsSuccess(existingHeights) =>
-        val exisitingHeightsSet = existingHeights.toSet
-        val blockCheckEnd: Long = blockCheckStart + settings.numberOfBlocksToCheck
-        val missingBlockHeights = (blockCheckStart to blockCheckEnd).filterNot(exisitingHeightsSet.contains)
-        if (missingBlockHeights.nonEmpty) {
-          val getBlockAtHeightFuture = exportBlocksAtHeight(missingBlockHeights)(_)
-          context.pipeToSelf(withNodeView(getBlockAtHeightFuture)) {
-            case Success(exportBlocks) => exportBlocks
-            case Failure(e)            => ReceivableMessages.getBlocksNodeViewFailure(e.toString)
-          }
-        }
-        active(blockCheckEnd + 1, timers)
-
-      case ReceivableMessages.getBlocksNodeViewFailure(failure) =>
-        log.error(s"${Console.RED}$failure${Console.RESET}")
-        Behaviors.same
-
-      case ReceivableMessages.CheckHeightsFailure(failure) =>
-        log.error(s"${Console.RED}$failure${Console.RESET}")
-        Behaviors.same
-
-      case ReceivableMessages.Terminate(reason) =>
+      case (_, ReceivableMessages.Terminate(reason)) =>
         throw reason
     }
 
-  private def exportBlocksAtHeight(
-    blockHeights: Seq[Long]
-  )(nodeView:     ReadableNodeView): ReceivableMessages.ExportBlocks = {
-    val blocks = blockHeights.flatMap(nodeView.history.modifierByHeight)
-    log.info(s"${Console.GREEN}Inserting blocks at height: $blockHeights${Console.RESET}")
-    ReceivableMessages.ExportBlocks(blocks)
+  private def exportBlocks(blocks: Seq[Block]): Future[InsertManyResult] = {
+    implicit val ec: ExecutionContext = context.executionContext
+    val blocksString = blocks.map(_.asJson(blockEncoder).toString)
+    mongo.insert(blocksString)
   }
 
-  private def withNodeView[T](f: ReadableNodeView => T): Future[T] = {
+  private def missingBlocks(
+    blockCheckStart: Long,
+    existingHeights: Seq[Long]
+  ): Future[Option[Seq[Block]]] = {
+    val exisitingHeightsSet = existingHeights.toSet
+    val blockCheckEnd: Long = blockCheckStart + settings.numberOfBlocksToCheck
+    val missingBlockHeights = (blockCheckStart to blockCheckEnd).filterNot(exisitingHeightsSet.contains)
+    (withNodeView(blocksAtHeight(missingBlockHeights)(_)))
+  }
+
+  private private def blocksAtHeight(
+    blockHeights: Seq[Long]
+  )(nodeView:     ReadableNodeView): Option[Seq[Block]] = {
+    // TODO: Jing - add function in nodeView.history to get multiple modifier at heights
+    val blocks = blockHeights.flatMap(nodeView.history.modifierByHeight)
+    if (blocks.nonEmpty) blocks.some
+    else None
+  }
+
+  private private def withNodeView[T](f: ReadableNodeView => T): Future[T] = {
     import akka.actor.typed.scaladsl.AskPattern._
 
     import scala.concurrent.duration._
