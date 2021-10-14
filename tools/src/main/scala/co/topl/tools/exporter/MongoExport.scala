16aac7f5dd8156316974681f967092655664c89b
jingsting <jingfei1024@gmail.com>
2021-10-14 03:04:44 -0500

    Reverted changes to MongoExport since a new export is used for chain replicator

--- tools/src/main/scala/co/topl/tools/exporter/MongoExport.scala
+++ tools/src/main/scala/co/topl/tools/exporter/MongoExport.scala
@@ -1,7 +1,5 @@
 package co.topl.tools.exporter
 
-import org.mongodb.scala.model.Filters.{and, gte, lte}
-import org.mongodb.scala.model.Projections
 import org.mongodb.scala.result.InsertManyResult
 import org.mongodb.scala.{Document, MongoClient, MongoDatabase}
 
@@ -17,18 +15,9 @@ class MongoExport(uri: String, database: String, collection: String, dt: DataTyp
 
   private def createDatabase(db: String): MongoDatabase = client.getDatabase(db)
 
-  def checkValidConnection(): Future[Seq[String]] = db.listCollectionNames().toFuture()
-
   override def insert(ele: Seq[String]): Future[InsertManyResult] = db
     .getCollection(collection)
     .insertMany(ele.map(Document(_)))
-    .toFuture()
-
-  def getExistingHeights(start: Long, end: Long): Future[Seq[Long]] = db
-    .getCollection(collection)
-    .find(and(gte("height", start), lte("height", end)))
-    .projection(Projections.fields(Projections.include("height"), Projections.excludeId()))
-    .map(_.head._2.asInt64().getValue)
     .toFuture()
 
   override def close(): Unit = client.close()
