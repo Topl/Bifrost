111a5836e70f268c80a30a780077f63b299a9fdb
Scala Steward <me@scala-steward.org>
2021-12-07 23:34:38 +0100

    Update logback-classic, logback-core to 1.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,8 +11,8 @@ object Dependencies {
 
   val logging = Seq(
     "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.4",
-    "ch.qos.logback"              % "logback-classic" % "1.2.6",
-    "ch.qos.logback"              % "logback-core"    % "1.2.6",
+    "ch.qos.logback"              % "logback-classic" % "1.2.7",
+    "ch.qos.logback"              % "logback-core"    % "1.2.7",
     "org.slf4j"                   % "slf4j-api"       % "1.7.32"
   )
 
