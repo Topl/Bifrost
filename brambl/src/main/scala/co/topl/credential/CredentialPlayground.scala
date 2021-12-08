2d75eb7eb23a3f86eab15c49ae6ff45ad673de07
Sean Cheatham <seancheatham@gmail.com>
2021-12-08 16:55:20 -0500

    Added CredentialPlayground app

---
+++ brambl/src/main/scala/co/topl/credential/CredentialPlayground.scala
@@ -0,0 +1,62 @@
+package co.topl.credential
+
+import cats.data.NonEmptyChain
+import co.topl.crypto.signing.{Ed25519, ExtendedEd25519}
+import co.topl.models._
+import co.topl.models.utility.HasLength.instances._
+import co.topl.models.utility.Sized
+import co.topl.typeclasses.KeyInitializer
+import co.topl.typeclasses.implicits._
+
+import scala.collection.immutable.ListMap
+import scala.util.Random
+
+object CredentialPlayground extends App {
+  implicit val ed25519: Ed25519 = new Ed25519
+  implicit val extendedEd25519: ExtendedEd25519 = ExtendedEd25519.precomputed()
+  implicit val networkPrefix: NetworkPrefix = NetworkPrefix(1: Byte)
+
+  // Exercise: Construct complex propositions and attempt to prove them using Credentials
+
+  // Example:
+  val party1SK: SecretKeys.Ed25519 = KeyInitializer[SecretKeys.Ed25519].random()
+  val party2SK: SecretKeys.Curve25519 = KeyInitializer[SecretKeys.Curve25519].random()
+
+  val party3Address: DionAddress = KeyInitializer[SecretKeys.Curve25519].random().vk.dionAddress
+
+  val proposition = party1SK.vk.asProposition.and(party2SK.vk.asProposition)
+  println(proposition)
+
+  val unprovenTransaction: Transaction.Unproven = Transaction.Unproven(
+    inputs = List((proposition.dionAddress, Random.nextLong())),
+    feeOutput = None,
+    coinOutputs = NonEmptyChain(Transaction.PolyOutput(party3Address, Sized.maxUnsafe(BigInt(10)))),
+    fee = Sized.maxUnsafe(BigInt(5)),
+    timestamp = System.currentTimeMillis(),
+    data = None,
+    minting = false
+  )
+
+  val credential = Credential.Compositional.And(
+    proposition,
+    List(
+      Credential.Knowledge.Ed25519(party1SK, unprovenTransaction),
+      Credential.Knowledge.Curve25519(party2SK, unprovenTransaction)
+    )
+  )
+
+  val proof = credential.proof
+  println(proof)
+
+  val transaction = Transaction(
+    inputs = ListMap.from(unprovenTransaction.inputs.map(_ -> (proposition, proof))),
+    feeOutput = unprovenTransaction.feeOutput,
+    coinOutputs = unprovenTransaction.coinOutputs,
+    fee = unprovenTransaction.fee,
+    timestamp = unprovenTransaction.timestamp,
+    data = unprovenTransaction.data,
+    minting = unprovenTransaction.minting
+  )
+  println(transaction)
+
+}
