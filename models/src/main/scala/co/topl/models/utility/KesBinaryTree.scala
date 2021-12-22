6083eeb14e58bc3ad54b50c5b3243c8920b5b7fc
Aaron Schutza <aaronschutza@gmail.com>
2021-12-22 10:29:22 -0600

    updated kes private key test vector, preparePR

--- models/src/main/scala/co/topl/models/utility/KesBinaryTree.scala
+++ models/src/main/scala/co/topl/models/utility/KesBinaryTree.scala
@@ -11,11 +11,12 @@ object KesBinaryTree {
     // false-true corresponds to left or right child node with respect to the parent being constructed
     // witness elements and seeds should be provided for each level of the tree in order of highest to lowest
     // sk and vk are the leaf (lowest) level private and public keys
-    type Args = (Boolean,(Bytes,Bytes,Bytes))
-    def build(sk:Bytes,vk:Bytes,args:Args*):KesBinaryTree = {
+    type Args = (Boolean, (Bytes, Bytes, Bytes))
+
+    def build(sk: Bytes, vk: Bytes, args: Args*): KesBinaryTree =
       args.length match {
         case 0 =>
-          KesBinaryTree.SigningLeaf(sk.toArray,vk.toArray)
+          KesBinaryTree.SigningLeaf(sk.toArray, vk.toArray)
         case _ =>
           if (args.head._1) {
             KesBinaryTree.MerkleNode(
@@ -23,47 +24,51 @@ object KesBinaryTree {
               args.head._2._2.toArray,
               args.head._2._3.toArray,
               KesBinaryTree.Empty,
-              this.build(sk,vk,args.tail:_*))
+              this.build(sk, vk, args.tail: _*)
+            )
           } else {
             KesBinaryTree.MerkleNode(
               args.head._2._1.toArray,
               args.head._2._2.toArray,
               args.head._2._3.toArray,
-              this.build(sk,vk,args.tail:_*),
-              KesBinaryTree.Empty)
+              this.build(sk, vk, args.tail: _*),
+              KesBinaryTree.Empty
+            )
           }
       }
-    }
   }
 
-  def areEqual(a:KesBinaryTree,b:Any): Boolean = {
-    (a,b) match {
-      case (KesBinaryTree.MerkleNode(
-      seed_a:         Array[Byte],
-      witnessLeft_a:  Array[Byte],
-      witnessRight_a: Array[Byte],
-      left_a:         KesBinaryTree,
-      right_a:        KesBinaryTree),
-      KesBinaryTree.MerkleNode(
-      seed_b:         Array[Byte],
-      witnessLeft_b:  Array[Byte],
-      witnessRight_b: Array[Byte],
-      left_b:         KesBinaryTree,
-      right_b:        KesBinaryTree)) =>
+  def areEqual(a: KesBinaryTree, b: Any): Boolean =
+    (a, b) match {
+      case (
+            KesBinaryTree.MerkleNode(
+              seed_a: Array[Byte],
+              witnessLeft_a: Array[Byte],
+              witnessRight_a: Array[Byte],
+              left_a: KesBinaryTree,
+              right_a: KesBinaryTree
+            ),
+            KesBinaryTree.MerkleNode(
+              seed_b: Array[Byte],
+              witnessLeft_b: Array[Byte],
+              witnessRight_b: Array[Byte],
+              left_b: KesBinaryTree,
+              right_b: KesBinaryTree
+            )
+          ) =>
         Bytes(seed_a) == Bytes(seed_b) &&
           Bytes(witnessLeft_a) == Bytes(witnessLeft_b) &&
-          Bytes(witnessRight_a) ==  Bytes(witnessRight_b) &&
-          areEqual(left_a,left_b) &&
-          areEqual(right_a,right_b)
-      case (KesBinaryTree.Empty,KesBinaryTree.Empty) =>
+          Bytes(witnessRight_a) == Bytes(witnessRight_b) &&
+          areEqual(left_a, left_b) &&
+          areEqual(right_a, right_b)
+      case (KesBinaryTree.Empty, KesBinaryTree.Empty) =>
         true
-      case (KesBinaryTree.SigningLeaf(sk_a,vk_a),KesBinaryTree.SigningLeaf(sk_b,vk_b)) =>
+      case (KesBinaryTree.SigningLeaf(sk_a, vk_a), KesBinaryTree.SigningLeaf(sk_b, vk_b)) =>
         Bytes(sk_a) == Bytes(sk_b) &&
           Bytes(vk_a) == Bytes(vk_b)
       case _ =>
         false
     }
-  }
 
   case class MerkleNode(
     seed:         Array[Byte],
@@ -72,11 +77,11 @@ object KesBinaryTree {
     left:         KesBinaryTree,
     right:        KesBinaryTree
   ) extends KesBinaryTree {
-    override def equals(obj: Any): Boolean = areEqual(this,obj)
+    override def equals(obj: Any): Boolean = areEqual(this, obj)
   }
 
   case class SigningLeaf(sk: Array[Byte], vk: Array[Byte]) extends KesBinaryTree {
-    override def equals(obj: Any): Boolean = areEqual(this,obj)
+    override def equals(obj: Any): Boolean = areEqual(this, obj)
   }
 
   case object Empty extends KesBinaryTree
