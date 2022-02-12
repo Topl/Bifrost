9ac850f62b3d8470c0692cf70b7f4f2404835e36
James Aman <jim.a.aman@gmail.com>
2022-02-12 00:15:24 -0600

    removing deprecated kes from merge

--- crypto/src/main/scala/co/topl/crypto/kes/CipherInfo.scala
+++
@@ -1,9 +0,0 @@
-package co.topl.crypto.kes
-
-case class CipherInfo(
-  pubKey:     Array[Byte],
-  cipherText: Array[Byte],
-  mac:        Array[Byte],
-  salt:       Array[Byte],
-  iv:         Array[Byte]
-)
