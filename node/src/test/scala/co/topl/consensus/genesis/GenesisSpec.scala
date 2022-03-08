cc027c28b250b60d2fa986d0311eab1b924e98cc
jingsting <jingfei1024@gmail.com>
2022-03-08 05:42:43 -0600

    switched to using generatedGenesis for private testnet

--- node/src/test/scala/co/topl/consensus/genesis/GenesisSpec.scala
+++ node/src/test/scala/co/topl/consensus/genesis/GenesisSpec.scala
@@ -2,7 +2,8 @@ package co.topl.consensus.genesis
 
 import co.topl.attestation.keyManagement.{KeyRing, KeyfileCurve25519, KeyfileCurve25519Companion, PrivateKeyCurve25519}
 import co.topl.modifier.transaction.{ArbitTransfer, PolyTransfer}
-import co.topl.utils.NetworkType.PrivateTestnet
+import co.topl.settings.GenesisGenerationSettings
+import co.topl.utils.NetworkType._
 import co.topl.utils.{Int128, NodeGenerators}
 import org.scalatest.Inspectors
 import org.scalatest.freespec.AnyFreeSpec
@@ -11,6 +12,8 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 class GenesisSpec extends AnyFreeSpec with Inspectors with NodeGenerators {
 
   case class GenesisTotals(coinTotal: Int128, arbitTotal: Int128, polyTotal: Int128)
+
+  val genesisGenSettings: GenesisGenerationSettings = settings.forging.genesis.flatMap(_.generated).head
 
   val keyfileCurve25519Companion: KeyfileCurve25519Companion.type = KeyfileCurve25519Companion
 
@@ -30,7 +33,10 @@ class GenesisSpec extends AnyFreeSpec with Inspectors with NodeGenerators {
       HelGenesis,
       ToplnetGenesis,
       ValhallaGenesis,
-      PrivateGenesis(privateKeyRing.addresses, settings)(PrivateTestnet.netPrefix)
+      GeneratedGenesis(privateKeyRing.addresses, genesisGenSettings)(PrivateTestnet.netPrefix),
+      GeneratedGenesis(privateKeyRing.addresses, genesisGenSettings)(Mainnet.netPrefix),
+      GeneratedGenesis(privateKeyRing.addresses, genesisGenSettings)(ValhallaTestnet.netPrefix),
+      GeneratedGenesis(privateKeyRing.addresses, genesisGenSettings)(HelTestnet.netPrefix),
     )
   ) { genesisProvider: GenesisProvider =>
     genesisProvider match {
