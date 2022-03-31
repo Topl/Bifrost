b3e25627c72166308071525535cfa3bd34ff7183
Sean Cheatham <seancheatham@gmail.com>
2022-03-31 10:53:09 -0400

    Remove `processRemoteBlockNotificationFlow` entirely

--- networking/src/main/scala/co/topl/networking/blockchain/BlockchainClientHandler.scala
+++ networking/src/main/scala/co/topl/networking/blockchain/BlockchainClientHandler.scala
@@ -1,10 +1,7 @@
 package co.topl.networking.blockchain
 
-import akka.NotUsed
 import akka.actor.typed.ActorSystem
-import akka.stream.{FlowShape, OverflowStrategy}
-import akka.stream.scaladsl.GraphDSL.Implicits._
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, Merge, Partition, Sink, Source}
+import akka.stream.scaladsl.{Keep, Sink}
 import cats.MonadThrow
 import cats.data.OptionT
 import cats.effect.kernel.Sync
@@ -12,11 +9,11 @@ import cats.effect.{Async, Concurrent}
 import cats.implicits._
 import co.topl.algebras.Store
 import co.topl.catsakka._
-import co.topl.codecs.bytes.tetra.instances._
-import co.topl.codecs.bytes.typeclasses.implicits._
 import co.topl.models._
 import co.topl.typeclasses.implicits._
 import org.typelevel.log4cats.Logger
+import co.topl.codecs.bytes.typeclasses.implicits._
+import co.topl.codecs.bytes.tetra.instances._
 
 trait BlockchainClientHandler[F[_]] {
   def useClient(client: BlockchainPeerClient[F]): F[Unit]
@@ -46,7 +43,6 @@ object BlockchainClientHandler {
               ) _
               _ <- Async[F].fromFuture(
                 remoteAdoptionsSource
-//                  .via(blockProcessorFlow)
                   .mapAsyncF(1)(blockProcessor)
                   .toMat(Sink.ignore)(Keep.right)
                   .liftTo[F]
@@ -54,79 +50,6 @@ object BlockchainClientHandler {
             } yield ()
         ): BlockchainClientHandler[F]
       )
-
-    /**
-     * Fetch each block header, body, and transactions from the peer for each notified block ID.  If the local node
-     * is unable to keep up, it may ignore "older" notifications to make room for new ones.
-     *
-     * The fetch process works recursively; if a block's parent header is also missing, it requests it from the remote peer.
-     * TODO: Does not seem to recurse all the way back to a common ancestor?
-     */
-    private def processRemoteBlockNotificationFlow[F[_]: Async: Concurrent: Logger: FToFuture](
-      client:           BlockchainPeerClient[F],
-      headerStore:      Store[F, BlockHeaderV2],
-      bodyStore:        Store[F, BlockBodyV2],
-      transactionStore: Store[F, Transaction],
-      onBlockReceived:  BlockV2 => F[Unit]
-    ): F[Flow[TypedIdentifier, Unit, NotUsed]] = Sync[F].delay {
-      val fHeader = fetchHeader(client, headerStore) _
-      val fBody = fetchBody(client, bodyStore) _
-      val fTransactions = fetchTransactions(client, transactionStore)
-
-      val fetchHeaderCheckParentFlow =
-        Flow[(TypedIdentifier, TypedIdentifier)]
-          // Fetch the header
-          .mapAsyncF(1) { case (id, originalId) => fHeader(id).tupleLeft(id).tupleRight(originalId) }
-          .mapAsyncF(1) { case ((id, header), originalId) =>
-            headerStore.contains(header.parentHeaderId).tupleLeft((id, header, originalId))
-          }
-
-      val fetchBodyFlow =
-        Flow[((TypedIdentifier, BlockHeaderV2, TypedIdentifier), Boolean)]
-          // Fetch the body
-          .mapAsyncF(1) { case ((id, _, originalId), emit) => fBody(id).tupleLeft((originalId, emit)) }
-          // Fetch the transactions
-          .mapAsyncF(1) { case ((originalId, emit), body) =>
-            fTransactions(body).as((originalId, emit))
-          }
-          .collect { case (originalId, true) => originalId }
-
-      val collectForRecursion =
-        Flow[((TypedIdentifier, BlockHeaderV2, TypedIdentifier), Boolean)]
-          .collect { case ((_, header, original), false) =>
-            (header.parentHeaderId, original)
-          }
-      val recursiveFetch =
-        GraphDSL.create() { implicit builder =>
-          val fetchHeaderCheckParentFlow_ = builder.add(fetchHeaderCheckParentFlow)
-          val fetchBodyFlow_ = builder.add(fetchBodyFlow)
-          val collectForRecursion_ = builder.add(collectForRecursion)
-
-          val merge = builder.add(Merge[(TypedIdentifier, TypedIdentifier)](2))
-          val broadcast = builder.add(Broadcast[((TypedIdentifier, BlockHeaderV2, TypedIdentifier), Boolean)](2))
-
-          merge.out ~> fetchHeaderCheckParentFlow_ ~> broadcast.in
-          broadcast.out(0) ~> fetchBodyFlow_.in
-          broadcast.out(1) ~> collectForRecursion_ ~> merge.in(1)
-
-          FlowShape(merge.in(0), fetchBodyFlow_.out)
-        }
-      Flow[TypedIdentifier]
-        // We only really care about the remote peer's "most recent" block adoption since the remote peer may
-        // switch branches while the local node is still processing the original.
-        .buffer(1, OverflowStrategy.dropHead)
-        .mapAsyncF(1)(id => headerStore.contains(id).tupleLeft(id))
-        .collect { case (id, false) => id }
-        .map(id => (id, id))
-        .via(recursiveFetch)
-        .mapAsyncF(1)(id =>
-          (OptionT(headerStore.get(id)), OptionT(bodyStore.get(id)))
-            .mapN((header, body) => BlockV2(header, body))
-            .semiflatTap(onBlockReceived)
-            .value
-            .void
-        )
-    }
 
     private def processRemoteBlockNotification[F[_]: Async: Concurrent: Logger: FToFuture](
       client:           BlockchainPeerClient[F],
