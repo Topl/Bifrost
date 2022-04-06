0122ba70b90165be6486d50012f24c37b215edf1
jingsting <jingfei1024@gmail.com>
2022-04-06 16:09:09 -0500

    fixed storage cache test with Branden

--- node/src/test/scala/co/topl/nodeView/history/StorageCacheSpec.scala
+++ node/src/test/scala/co/topl/nodeView/history/StorageCacheSpec.scala
@@ -1,9 +1,10 @@
 package co.topl.nodeView.history
 
+import co.topl.consensus.ValidBlockchainGenerator
 import co.topl.modifier.block.Block
-import co.topl.nodeView.{CacheLayerKeyValueStore, ValidTransactionGenerators}
-import co.topl.utils.{NodeGenerators, ProtocolVersionHelper, TestSettings}
-import org.scalacheck.Gen
+import co.topl.nodeView.CacheLayerKeyValueStore
+import co.topl.utils.{InMemoryKeyRingTestHelper, NodeGenerators, TestSettings}
+import org.scalatest.OptionValues
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.propspec.AnyPropSpec
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
@@ -13,13 +14,15 @@ import scala.concurrent.duration.DurationInt
 class StorageCacheSpec
     extends AnyPropSpec
     with ScalaCheckDrivenPropertyChecks
+    with ValidBlockchainGenerator
     with NodeGenerators
-    with ProtocolVersionHelper
-    with Matchers
-    with ValidTransactionGenerators {
+    with InMemoryKeyRingTestHelper
+    with TestSettings
+    with OptionValues
+    with Matchers {
 
   property("The genesis block is stored in cache") {
-    withTestHistory { history =>
+    withTestHistory(genesisBlockGen.sample.get) { history =>
       val genesisBlockId = Array.fill(33)(-1: Byte)
 
       history.storage.keyValueStore
@@ -33,112 +36,141 @@ class StorageCacheSpec
   }
 
   property("Cache should invalidate a single entry when it's rolled back in storage") {
-    withTestHistory { history =>
-      forAll(blockCurve25519Gen) { block =>
-        val bestBlockIdKey = Array.fill(33)(-1: Byte)
+    val chain: GenesisHeadChain = validChainFromGenesis(
+      keyRingCurve25519,
+      settings.application.genesis.generated.value,
+      protocolVersioner
+    )(2).sample.get
 
-        val historyAddBlock = history.append(block, Seq()).get._1
+    withTestHistory(chain.head.block) { history =>
+      val block = chain.tail.head
+      val bestBlockIdKey = Array.fill(33)(-1: Byte)
 
-        historyAddBlock.storage.keyValueStore
-          .asInstanceOf[CacheLayerKeyValueStore]
-          .cache
-          .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(bestBlockIdKey)) should not be null
+      val historyAddBlock = history.append(block, Seq()).get._1
 
-        val historyDropBlock = historyAddBlock.drop(block.id)
+      historyAddBlock.storage.keyValueStore
+        .asInstanceOf[CacheLayerKeyValueStore]
+        .cache
+        .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(bestBlockIdKey)) should not be null
 
-        /* After dropping the new block, the best block should be its parent block, the genesis block */
-        /* The block Id for best block in cache should be invalidated */
-        historyDropBlock.storage.keyValueStore.asInstanceOf[CacheLayerKeyValueStore].cache.asMap().size() shouldBe 0
-      }
+      val historyDropBlock = historyAddBlock.drop(block.id)
+
+      /* After dropping the new block, the best block should be its parent block, the genesis block */
+      /* The block Id for best block in cache should be invalidated */
+      historyDropBlock.storage.keyValueStore.asInstanceOf[CacheLayerKeyValueStore].cache.asMap().size() shouldBe 0
     }
   }
 
   property("Cache should invalidate multiple entries when it's rolled back in storage") {
-    withTestHistory { history =>
-      forAll(Gen.nonEmptyListOf(blockCurve25519Gen)) { blocks =>
-        val bestBlockIdKey = Array.fill(33)(-1: Byte)
+    val chain: GenesisHeadChain = validChainFromGenesis(
+      keyRingCurve25519,
+      settings.application.genesis.generated.value,
+      protocolVersioner
+    )(positiveTinyIntGen.sample.get.toByte).sample.get
 
-        val historyAddBlock = blocks.foldLeft(history) { (accHistory, newBlock) =>
-          accHistory.append(newBlock, Seq()).get._1
-        }
+    withTestHistory(chain.head.block) { history =>
+      val bestBlockIdKey = Array.fill(33)(-1: Byte)
 
-        historyAddBlock.storage.keyValueStore
-          .asInstanceOf[CacheLayerKeyValueStore]
-          .cache
-          .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(bestBlockIdKey)) should not be null
-
-        val historyDropBlock = blocks.foldLeft(history) { (accHistory, block) =>
-          accHistory.drop(block.id)
-        }
-
-        historyDropBlock.storage.keyValueStore.asInstanceOf[CacheLayerKeyValueStore].cache.asMap().size() shouldBe 0
+      val historyAddBlock = chain.tail.foldLeft(history) { (accHistory, newBlock) =>
+        accHistory.append(newBlock, Seq()).get._1
       }
+
+      historyAddBlock.storage.keyValueStore
+        .asInstanceOf[CacheLayerKeyValueStore]
+        .cache
+        .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(bestBlockIdKey)) should not be null
+
+      val historyDropBlock = chain.tail.reverse.foldLeft(history) { (accHistory, block) =>
+        accHistory.drop(block.id)
+      }
+
+      historyDropBlock.storage.keyValueStore.asInstanceOf[CacheLayerKeyValueStore].cache.asMap().size() shouldBe 0
+
     }
   }
 
   property("The new block updated is stored in cache") {
-    withTestHistory { history =>
-      forAll(blockCurve25519Gen) { blockTemp =>
-        val block: Block = blockTemp.copy(parentId = history.bestBlockId)
+    val chain: GenesisHeadChain = validChainFromGenesis(
+      keyRingCurve25519,
+      settings.application.genesis.generated.value,
+      protocolVersioner
+    )(2).sample.get
 
-        val updatedHistory = history.append(block, Seq()).get._1
+    withTestHistory(chain.head.block) { history =>
+      val block: Block = chain.tail.head
 
-        val blockInCache =
-          updatedHistory.storage.keyValueStore
-            .asInstanceOf[CacheLayerKeyValueStore]
-            .cache
-            .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(block.id.getIdBytes))
+      val updatedHistory = history.append(block, Seq()).get._1
 
-        blockInCache shouldEqual updatedHistory.storage.keyValueStore.get(block.id.getIdBytes)
-      }
+      val blockInCache =
+        updatedHistory.storage.keyValueStore
+          .asInstanceOf[CacheLayerKeyValueStore]
+          .cache
+          .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(block.id.getIdBytes))
+
+      blockInCache shouldEqual updatedHistory.storage.keyValueStore.get(block.id.getIdBytes)
+
     }
   }
 
   property("Appending more entries than the maximum cache size will drop a portion of existing cache") {
-    forAll(genesisBlockGen, Gen.listOfN(10, blockCurve25519Gen)){ (genesisBlock, blocks) =>
-      def customCacheHistory(cacheSize: Int): History = {
-        val cacheStore = new CacheLayerKeyValueStore(new InMemoryKeyValueStore, 30.seconds, cacheSize)
-        val storage = new Storage(cacheStore)
-        val history = new History(storage, TineProcessor(1024))
-        history.append(genesisBlock, Seq()).get._1
-      }
+    val chain: GenesisHeadChain = validChainFromGenesis(
+      keyRingCurve25519,
+      settings.application.genesis.generated.value,
+      protocolVersioner
+    )(10).sample.get
 
-      val history = customCacheHistory(5)
-      val historyWithOneBlock = history.append(blocks.head, Seq()).get._1
-      val historyWithTooManyBlocks = blocks.foldLeft(history) { (accHistory, newBlock) =>
-        accHistory.append(newBlock, Seq()).get._1
-      }
-
-      historyWithOneBlock.storage.keyValueStore
-        .asInstanceOf[CacheLayerKeyValueStore]
-        .cache
-        .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(blocks.head.id.getIdBytes)) should not be null
-
-      historyWithTooManyBlocks.storage.keyValueStore
-        .asInstanceOf[CacheLayerKeyValueStore]
-        .cache
-        .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(blocks.head.id.getIdBytes)) shouldBe null
+    def customCacheHistory(cacheSize: Int): History = {
+      val cacheStore = new CacheLayerKeyValueStore(new InMemoryKeyValueStore, 30.seconds, cacheSize)
+      val storage = new Storage(cacheStore)
+      val history = new History(storage, TineProcessor(1024))
+      history.append(chain.head.block, Seq()).get._1
     }
+
+    val history = customCacheHistory(512)
+    val historyWithBlocks = chain.tail.foldLeft(history) { (accHistory, newBlock) =>
+      accHistory.append(newBlock, Seq()).get._1
+    }
+
+    val cacheSize = historyWithBlocks.storage.keyValueStore
+      .asInstanceOf[CacheLayerKeyValueStore]
+      .cache
+      .size()
+    val historyInsufficientCache = customCacheHistory(cacheSize.toInt / 2)
+    val historyWithTooManyBlocks = chain.tail.foldLeft(historyInsufficientCache) { (accHistory, newBlock) =>
+      accHistory.append(newBlock, Seq()).get._1
+    }
+
+    historyWithBlocks.storage.keyValueStore
+      .asInstanceOf[CacheLayerKeyValueStore]
+      .cache
+      .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(chain.tail.head.id.getIdBytes)) should not be null
+
+    historyWithTooManyBlocks.storage.keyValueStore
+      .asInstanceOf[CacheLayerKeyValueStore]
+      .cache
+      .getIfPresent(new CacheLayerKeyValueStore.WrappedBytes(chain.tail.head.id.getIdBytes)) shouldBe null
   }
 
   property("blockLoader should correctly return a block from storage not found in cache") {
-    withTestHistory { history =>
-      forAll(blockCurve25519Gen){ block =>
-        val updatedHistory = history.append(block, Seq()).get._1
-        updatedHistory.storage.keyValueStore.asInstanceOf[CacheLayerKeyValueStore].cache.invalidateAll()
-        updatedHistory.modifierById(block.id).get should not be None
-      }
+    val chain: GenesisHeadChain = validChainFromGenesis(
+      keyRingCurve25519,
+      settings.application.genesis.generated.value,
+      protocolVersioner
+    )(2).sample.get
+
+    withTestHistory(chain.head.block) { history =>
+      val updatedHistory = history.append(chain.tail.head, Seq()).get._1
+      updatedHistory.storage.keyValueStore.asInstanceOf[CacheLayerKeyValueStore].cache.invalidateAll()
+      updatedHistory.modifierById(chain.tail.head.id).get should not be None
     }
   }
 
-  private def withTestHistory(test: History => Unit): Unit =
+  private def withTestHistory(genesis: Block)(test: History => Unit): Unit =
     test {
-      val genesisBlock = genesisBlockGen.sample.get
-
       val underlyingStore = new InMemoryKeyValueStore
       val cacheStore = new CacheLayerKeyValueStore(underlyingStore, 10.seconds, 512)
       val storage = new Storage(cacheStore)
       val history = new History(storage, TineProcessor(1024))
-      history.append(genesisBlock, Seq()).get._1
+      history.append(genesis, Seq()).get._1
     }
 }
