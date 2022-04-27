a370b8876501c1fd319720d80ccd0713fdd104af
Mark Grand <mgrand@markgrand.com>
2022-04-27 12:45:44 -0400

    New file

---
+++ node/src/main/scala/co/topl/db/SonarCloudAttentionGetter.scala
@@ -0,0 +1,26 @@
+package co.topl.db
+
+/**
+ * This object is totally unneeded by bifrost. I am adding it with some things that Sonar should complain about so that
+ * I can see that the integration with Sonarcloud is correctly set up.
+ *
+ * This should be deleted as soon as the Sonarcloud integration is working.
+ */
+object sonarCloudAttentionGetter {
+  def DoIt(x: Int): Unit = {
+    if (!(x!=4))
+      println(x)
+    val a = 4
+    val b = 4
+    if (a == a)
+      println(a - a)
+  }
+
+  //TODO blah
+
+  //FIXME blah
+
+  def Dont() = {
+
+  }
+}
