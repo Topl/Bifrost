382d8826fd6b70e9985c0457514ab8ebdd91ecb8
Mark Grand <m.grand@topl.me>
2022-05-17 13:55:57 -0400

    scalafmt

--- node/src/main/scala/co/topl/db/LDBFactory.scala
+++ node/src/main/scala/co/topl/db/LDBFactory.scala
@@ -160,7 +160,10 @@ object LDBFactory extends Logging {
     try Some(loader.loadClass(factoryName).getConstructor().newInstance().asInstanceOf[DBFactory])
     catch {
       case e: Throwable =>
-        log.warn(s"Failed to load database factory $factoryName. Classloader is $loader.\nClasspath is ${System.getProperty("java.class.path")}", e)
+        log.warn(
+          s"Failed to load database factory $factoryName. Classloader is $loader.\nClasspath is ${System.getProperty("java.class.path")}",
+          e
+        )
         None
     }
 }
