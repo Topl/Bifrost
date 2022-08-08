a6ffb9a6b9431d3c5574bd6985ff87d2a1e36c56
Scala Steward <scala_steward@virtuslab.com>
2022-08-08 17:30:51 +0000

    Update kamon-bundle, kamon-core, ... to 2.5.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaVersion = "2.6.18"
   val akkaHttpVersion = "10.2.9"
   val circeVersion = "0.14.1"
-  val kamonVersion = "2.5.0"
+  val kamonVersion = "2.5.6"
   val graalVersion = "21.1.0"
   val simulacrumVersion = "1.0.1"
   val catsCoreVersion = "2.6.1"
@@ -279,8 +279,8 @@ object Dependencies {
 
   lazy val loadTesting: Seq[ModuleID] =
     Seq(
-      "com.lihaoyi" %% "mainargs" % "0.2.1",
-      "com.nike.fleam" %% "fleam" % "7.0.0"
+      "com.lihaoyi"    %% "mainargs" % "0.2.1",
+      "com.nike.fleam" %% "fleam"    % "7.0.0"
     ) ++
     fleam ++
     allAkka ++
