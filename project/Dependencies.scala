5256e5de02ed10c00f02ca3e800a664fa0b12664
Scala Steward <scala_steward@virtuslab.com>
2022-08-22 16:52:36 +0000

    Update slf4j-api to 2.0.0 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5",
     "ch.qos.logback"              % "logback-classic" % "1.2.11",
     "ch.qos.logback"              % "logback-core"    % "1.2.11",
-    "org.slf4j"                   % "slf4j-api"       % "1.7.36",
+    "org.slf4j"                   % "slf4j-api"       % "2.0.0",
     catsSlf4j
   )
 
