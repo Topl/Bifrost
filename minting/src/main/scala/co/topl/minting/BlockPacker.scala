44b5f7c56fc1c9dcc3ae3e3363eedf224834e748
Sean Cheatham <seancheatham@gmail.com>
2022-08-26 12:21:06 -0400

    Simplify Iterative and BlockPacker

--- minting/src/main/scala/co/topl/minting/BlockPacker.scala
+++ minting/src/main/scala/co/topl/minting/BlockPacker.scala
@@ -1,7 +1,8 @@
 package co.topl.minting
 
 import cats.Monad
-import cats.data.{Chain, OptionT}
+import cats.data.OptionT
+import cats.effect.std.Queue
 import cats.effect.{Async, Sync}
 import cats.implicits._
 import co.topl.catsakka._
@@ -11,12 +12,13 @@ import co.topl.ledger.algebras._
 import co.topl.minting.algebras.BlockPackerAlgebra
 import co.topl.models._
 import co.topl.typeclasses.implicits._
+import org.typelevel.log4cats.Logger
 
 import scala.collection.immutable.ListSet
 
 object BlockPacker {
 
-  def make[F[_]: Async](
+  def make[F[_]: Async: Logger](
     mempool:          MempoolAlgebra[F],
     fetchTransaction: TypedIdentifier => F[Transaction],
     validateBody:     (TypedIdentifier, BlockBodyV2) => F[Boolean]
@@ -24,29 +26,23 @@ object BlockPacker {
     Sync[F].delay((parentBlockId: TypedIdentifier) =>
       for {
         mempoolTransactionIds <- mempool.read(parentBlockId)
+        _                     <- Logger[F].info(s"Considering mempool transaction IDs=${mempoolTransactionIds.toList}")
         transactions          <- mempoolTransactionIds.toList.traverse(fetchTransaction)
-        iterative: Iterative[F, BlockBodyV2.Full] =
-          if (transactions.isEmpty) {
-            new Iterative[F, BlockBodyV2.Full] {
-              private var emitted = false
-              def improve(current: BlockBodyV2.Full): F[BlockBodyV2.Full] =
-                if (emitted) Async[F].never[BlockBodyV2.Full]
-                else {
-                  emitted = true
-                  Chain.empty[Transaction].pure[F]
-                }
-            }
-          } else {
-            val remainingTransactions = scala.collection.mutable.Queue.from(transactions)
-            (selected: BlockBodyV2.Full) =>
-              if (remainingTransactions.isEmpty) Async[F].never
-              else {
-                val transaction = remainingTransactions.dequeue()
-                val fullBody = transaction +: selected
-                val body = ListSet.empty[TypedIdentifier] ++ fullBody.toList.map(_.id.asTypedBytes)
-                validateBody(parentBlockId, body).ifF(fullBody, selected)
+        iterative <-
+          Queue
+            .unbounded[F, Transaction]
+            .flatTap(queue => transactions.traverse(queue.offer))
+            .map(queue =>
+              new Iterative[F, BlockBodyV2.Full] {
+
+                def improve(current: BlockBodyV2.Full): F[BlockBodyV2.Full] =
+                  queue.take.flatMap { transaction =>
+                    val fullBody = transaction +: current
+                    val body = ListSet.empty[TypedIdentifier] ++ fullBody.toList.map(_.id.asTypedBytes)
+                    validateBody(parentBlockId, body).ifF(fullBody, current)
+                  }
               }
-          }
+            )
       } yield iterative
     )
 
