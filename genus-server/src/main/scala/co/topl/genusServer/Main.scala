c5e6d2dae427c5eeb7a79827b73aefe31664d6be
mgrand <m.grand@topl.me>
2022-09-30 10:32:16 -0400

    custom message

--- genus-server/src/main/scala/co/topl/genusServer/Main.scala
+++ genus-server/src/main/scala/co/topl/genusServer/Main.scala
@@ -1,4 +1,4 @@
-package co.topl.genus
+package co.topl.genusServer
 
 import cats.effect._
 
@@ -8,7 +8,7 @@ import cats.effect._
 object Main extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
-    println("Hello world!")
+    println("Hello world from Genus server!")
     //println(s"BuildInfo: ${co.topl.buildinfo.genusServer.BuildInfo.toString}")
     IO.pure(ExitCode.Success)
   }
