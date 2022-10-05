7d085770fe5cba05c8490172ad8d0d241c504e20
mgrand <m.grand@topl.me>
2022-10-05 08:22:20 -0400

    package content

--- genus-library/src/main/scala/co/topl/genusLibrary/Genus.scala
+++ genus-library/src/main/scala/co/topl/genusLibrary/Genus.scala
@@ -1,8 +1,13 @@
 package co.topl.genusLibrary
 
 /**
- *
+ * This is the main class of Genus. It captures blocks in an OrientDB database and provides ways to index and query the
+ * blocks, transactions and boxes.
  */
 class Genus {
+
+}
+
+object Genus {
 
 }
--- genus-library/src/main/scala/co/topl/genusLibrary/package.scala
+++ genus-library/src/main/scala/co/topl/genusLibrary/package.scala
@@ -2,5 +2,7 @@ package co.topl
 
 /**
  * This package contains the logic for genus services and their database.
+ * <p>
+ * The main class to use is the [[Genus]] class.
  */
 package object genusLibrary {}
