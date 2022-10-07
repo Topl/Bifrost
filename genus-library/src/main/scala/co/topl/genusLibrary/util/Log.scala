7d7669c71583d7506f90f5f9e0f611514bdbd538
mgrand <m.grand@topl.me>
2022-10-07 18:47:43 -0400

    ifFailure

--- genus-library/src/main/scala/co/topl/genusLibrary/util/Log.scala
+++ genus-library/src/main/scala/co/topl/genusLibrary/util/Log.scala
@@ -2,6 +2,8 @@ package co.topl.genusLibrary.util
 
 import com.typesafe.scalalogging.Logger
 
+import scala.util.{Failure, Try}
+
 /**
  * Log, at info level, the result of an expression like this:
  * {{{
@@ -29,5 +31,20 @@ object Log {
     val result:T = body
     logger.debug (message, result)
     result
+  }
+
+  /**
+   * Log the given Try if it is a Failure
+   *
+   * @param message the message to log
+   * @param t The Try that will be logged if it is a Failure
+   * @tparam T The type of the Try
+   * @return the Try
+   */
+  def ifFailure[T](message: String)(t: Try[T])(implicit logger: Logger): Try[T] = {
+    t.recoverWith(f => {
+      logger.error(message, f)
+      Failure(f)
+    })
   }
 }
