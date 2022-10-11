e6cab3b1cbf27e736d759cbaa048856551d77850
mgrand <m.grand@topl.me>
2022-10-11 12:35:14 -0400

    Response to Sean's feedback

--- genus-library/src/main/scala/co/topl/genusLibrary/Genus.scala
+++ genus-library/src/main/scala/co/topl/genusLibrary/Genus.scala
@@ -1,12 +1,10 @@
 package co.topl.genusLibrary
 
 import co.topl.genusLibrary.util.Log
+import co.topl.genusLibrary.util.Log._
 import com.typesafe.scalalogging.Logger
 
-import scala.reflect.ClassTag.Nothing
-import scala.runtime.Nothing$
-import scala.util.{Failure, Success, Try}
-import Log._
+import scala.util.Try
 
 /**
  * This is the main class of Genus. It captures blocks in an OrientDB database and provides ways to index and query the
@@ -14,52 +12,35 @@ import Log._
  */
 class Genus {
   import Genus.logger
-  private var orientDB = OrientDBFacade()
+
+  private val orientDB = OrientDBFacade()
 
   private def shutDown(): Try[Unit] = {
     logger.info("Genus shutting down")
-    orientDB.shutdown()
-    Success(())
+    Try(orientDB.shutdown())
   }
 }
 
 object Genus {
   private implicit val logger: Logger = Logger(classOf[Genus])
 
-  logger.info("Constructing Genus instance")
-
   /**
-   * This variable refers to the running instance of Genus when it is defined.
-   * Call the startup method to define it.
+   * This variable refers to the running instance of Genus.
    */
-  private var instance = Option.empty[Genus]
-
-  private def ensureActive[T](thunk: => T): Try[T] = {
-    instance.map( _ => Success(thunk)).getOrElse(Failure( GenusException("")))
-  }
+  private lazy val instance = new Genus
 
   /**
    * This method returns the active instance of Genus. If one does not exist, it is created.
    */
   def getGenus:Try[Genus] = {
     Log.debug("getGenus called: {}") {
-      this.synchronized {
-        instance.map(Success(_)).getOrElse(Try{
-          instance = Some(new Genus)
-          instance.get
-        }.logIfFailure("Failed to create Genus instance"))
-      }
+      Try(instance)
+        .logIfFailure("Failed to create Genus instance")
     }
   }
 
   def shutDown(): Try[Unit] = {
-    Log.info("shutDown() called") {
-      this.synchronized {
-        ensureActive {
-          instance.get.shutDown()
-          instance = None
-        }
-      }
-    }
+    logger.info("shutDown() called")
+    instance.shutDown()
   }
 }
