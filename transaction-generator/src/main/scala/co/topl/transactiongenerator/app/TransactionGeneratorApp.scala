8c42556555d71c49a3b56b286e93b53205b285ea
Sean Cheatham <seancheatham@gmail.com>
2022-10-25 14:48:24 -0400

    Create ApplicationConfig for TransactionGenerator.  Include defaults for currently hardcoded settings.

---
+++ transaction-generator/src/main/scala/co/topl/transactiongenerator/app/TransactionGeneratorApp.scala
@@ -0,0 +1,126 @@
+package co.topl.transactiongenerator.app
+
+import cats.Show
+import cats.data._
+import cats.effect._
+import cats.effect.std.Random
+import cats.implicits._
+import co.topl.algebras.ToplRpc
+import co.topl.codecs.bytes.tetra.instances._
+import co.topl.codecs.bytes.typeclasses.implicits._
+import co.topl.common.application._
+import co.topl.grpc.ToplGrpc
+import co.topl.interpreters._
+import co.topl.models._
+import co.topl.transactiongenerator.interpreters._
+import co.topl.typeclasses.implicits._
+import com.typesafe.config.ConfigFactory
+import fs2._
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+
+import scala.collection.immutable.ListSet
+import scala.concurrent.duration._
+
+object TransactionGeneratorApp
+    extends IOBaseApp[Args, ApplicationConfig](
+      createArgs = args => Args.parserArgs.constructOrThrow(args),
+      createConfig = IOBaseApp.createTypesafeConfig,
+      parseConfig = (args, conf) => ApplicationConfig.unsafe(args, conf)
+    ) {
+
+  implicit private val logger: Logger[F] =
+    Slf4jLogger.getLoggerFromClass[F](this.getClass)
+
+  override def run: IO[Unit] =
+    for {
+      _                            <- Logger[F].info(show"Launching Transaction Generator with appConfig=$appConfig")
+      implicit0(random: Random[F]) <- Random.javaSecuritySecureRandom[F]
+      // Initialize gRPC Clients
+      clientAddresses <- parseClientAddresses
+      _               <- Logger[F].info(show"Initializing clients=$clientAddresses")
+      clients = clientAddresses.traverse { case (host, port) =>
+        ToplGrpc.Client.make[F](host, port, tls = false)
+      }
+      // Turn the list of clients into a single client (randomly chosen per-call)
+      _ <- clients
+        .evalMap(MultiToplRpc.make[F, NonEmptyChain])
+        .use(client =>
+          for {
+            // Assemble a base wallet of available UTxOs
+            _ <- Logger[F].info(show"Initializing wallet")
+            wallet <- ToplRpcWalletInitializer
+              .make[F](
+                client,
+                appConfig.transactionGenerator.parallelism.fetchHeader,
+                appConfig.transactionGenerator.parallelism.fetchBody,
+                appConfig.transactionGenerator.parallelism.fetchTransaction
+              )
+              .flatMap(_.initialize)
+            _ <- Logger[F].info(show"Initialized wallet with spendableBoxCount=${wallet.spendableBoxes.size}")
+            // Produce a stream of Transactions from the base wallet
+            targetTps = appConfig.transactionGenerator.broadcaster.tps
+            _ <- Logger[F].info(show"Generating and broadcasting transactions at tps=$targetTps")
+            transactionStream <- Fs2TransactionGenerator
+              .make[F](
+                wallet,
+                appConfig.transactionGenerator.parallelism.generateTx,
+                appConfig.transactionGenerator.generator.maxWalletSize,
+                appConfig.transactionGenerator.generator.dataLength
+              )
+              .flatMap(_.generateTransactions)
+            // Broadcast the transactions and run the background mempool stream
+            _ <- (
+              runBroadcastStream(transactionStream, client, targetTps),
+              runMempoolStream(client, appConfig.transactionGenerator.mempool.period)
+            ).parTupled
+          } yield ()
+        )
+    } yield ()
+
+  private def parseClientAddresses: F[NonEmptyChain[(String, Int)]] =
+    IO.fromEither(
+      NonEmptyChain
+        .fromSeq(appConfig.transactionGenerator.rpc.clients)
+        .toRight[Exception](new IllegalArgumentException("No RPC clients specified"))
+        .flatMap(_.traverse(_.split(':').toList match {
+          case host :: port :: Nil =>
+            port.toIntOption.toRight(new IllegalArgumentException("Invalid RPC port provided")).map((host, _))
+          case _ => Left(new IllegalArgumentException("Invalid RPC config provided"))
+        }))
+    )
+
+  /**
+   * Broadcasts each transaction from the input stream
+   */
+  private def runBroadcastStream(transactionStream: Stream[F, Transaction], client: ToplRpc[F], targetTps: Double) =
+    transactionStream
+      // Send 1 transaction per _this_ duration
+      .metered((1_000_000_000d / targetTps).nanos)
+      // Broadcast+log the transaction
+      .evalTap(transaction =>
+        Logger[F].debug(show"Broadcasting transaction id=${transaction.id.asTypedBytes}") >>
+        client.broadcastTransaction(transaction) >>
+        Logger[F].info(show"Broadcasted transaction id=${transaction.id.asTypedBytes}")
+      )
+      .onError { case e =>
+        Stream.eval(Logger[F].error(e)("Stream failed"))
+      }
+      .compile
+      .drain
+
+  implicit private val showMempool: Show[Set[TypedIdentifier]] =
+    catsStdShowForSet(showTypedIdentifier)
+
+  /**
+   * Periodically poll and log the state of the mempool.
+   */
+  private def runMempoolStream(client: ToplRpc[F], period: FiniteDuration) =
+    Stream
+      .awakeEvery[F](period)
+      .evalMap(_ => client.currentMempool())
+      .evalTap(transactionIds => Logger[F].info(show"Current mempool=$transactionIds"))
+      .compile
+      .drain
+
+}
--- transaction-generator/src/main/scala/co/topl/transactiongenerator/interpreters/ToplRpcWalletInitializer.scala
+++ transaction-generator/src/main/scala/co/topl/transactiongenerator/interpreters/ToplRpcWalletInitializer.scala
@@ -18,13 +18,23 @@ object ToplRpcWalletInitializer {
    * "Recovers" a wallet using the given ToplRpc.  The recovery mechanism streams all blocks and transactions, and captures
    * applicable UTxOs in the wallet.
    */
-  def make[F[_]: Async](toplRpc: ToplRpc[F]): F[WalletInitializer[F]] =
+  def make[F[_]: Async](
+    toplRpc:                     ToplRpc[F],
+    fetchHeaderParallelism:      Int,
+    fetchBodyParallelism:        Int,
+    fetchTransactionParallelism: Int
+  ): F[WalletInitializer[F]] =
     Async[F].delay {
       new WalletInitializer[F] {
         def initialize: F[Wallet] =
           for {
-            transactionStream <- transactionStream(toplRpc)
-            wallet            <- transactionStream.scan(emptyWallet)(applyTransaction(_)(_)).compile.last
+            transactionStream <- transactionStream(
+              toplRpc,
+              fetchHeaderParallelism,
+              fetchBodyParallelism,
+              fetchTransactionParallelism
+            )
+            wallet <- transactionStream.scan(emptyWallet)(applyTransaction(_)(_)).compile.last
           } yield wallet.get
       }
     }
@@ -32,47 +42,51 @@ object ToplRpcWalletInitializer {
   /**
    * Emits a stream of transactions, starting from the big-bang block and moving forward
    */
-  private def transactionStream[F[_]: Async](toplRpc: ToplRpc[F]): F[Stream[F, Transaction]] =
+  private def transactionStream[F[_]: Async](
+    toplRpc:                     ToplRpc[F],
+    fetchHeaderParallelism:      Int,
+    fetchBodyParallelism:        Int,
+    fetchTransactionParallelism: Int
+  ): F[Stream[F, Transaction]] =
     for {
-      headersStream <- blockStream(toplRpc)
-      stream = headersStream.flatMap(header =>
-        Stream
-          .evals(
-            OptionT(toplRpc.fetchBlockBody(header.id))
-              .getOrRaise(new IllegalStateException("Block body not found"))
-              .map(_.toList)
-          )
-          .parEvalMap(4)(transactionId =>
-            OptionT(toplRpc.fetchTransaction(transactionId))
-              .getOrRaise(new IllegalStateException("Transaction not found"))
-          )
-      )
+      blockIds <- blockIdStream(toplRpc, fetchHeaderParallelism)
+      stream = blockIds
+        .parEvalMap(fetchBodyParallelism)(blockId =>
+          OptionT(toplRpc.fetchBlockBody(blockId))
+            .getOrRaise(new IllegalStateException("Block body not found"))
+            .map(_.toList)
+        )
+        .flatMap(Stream.iterable)
+        .parEvalMap(fetchTransactionParallelism)(transactionId =>
+          OptionT(toplRpc.fetchTransaction(transactionId))
+            .getOrRaise(new IllegalStateException("Transaction not found"))
+        )
     } yield stream
 
   /**
-   * Start from the big-bang block, and emit a stream of forward-traversing block headers
+   * Start from the big-bang block, and emit a stream of forward-traversing block IDs
    */
-  private def blockStream[F[_]: Async](toplRpc: ToplRpc[F]): F[Stream[F, BlockHeaderV2]] =
+  private def blockIdStream[F[_]: Async](
+    toplRpc:                ToplRpc[F],
+    fetchHeaderParallelism: Int
+  ): F[Stream[F, TypedIdentifier]] =
     for {
       bigBangId <- OptionT(toplRpc.blockIdAtHeight(1))
         .getOrRaise(new IllegalStateException("Unknown Big Bang ID"))
-      bigBang <- OptionT(toplRpc.fetchBlockHeader(bigBangId))
-        .getOrRaise(new IllegalStateException("Unknown Big Bang Block"))
       headId <- OptionT(toplRpc.blockIdAtDepth(0)).getOrRaise(new IllegalStateException("Unknown Canonical Head ID"))
       head <- OptionT(toplRpc.fetchBlockHeader(headId))
         .getOrRaise(new IllegalStateException("Unknown Canonical Head Block"))
       stream =
         if (bigBangId != headId)
-          Stream(bigBang) ++
+          Stream(bigBangId) ++
           Stream
-            .range(2, head.height)
-            .evalMap(height =>
+            .range[F, Long](2, head.height)
+            .parEvalMap(fetchHeaderParallelism)(height =>
               OptionT(toplRpc.blockIdAtHeight(height))
-                .flatMapF(toplRpc.fetchBlockHeader)
                 .getOrRaise(new IllegalStateException("Block not found at height"))
             ) ++
-          Stream(head)
-        else Stream(head)
+          Stream(headId)
+        else Stream(headId)
     } yield stream
 
 }
