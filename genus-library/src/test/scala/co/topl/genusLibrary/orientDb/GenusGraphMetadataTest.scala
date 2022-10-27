e5042acac6190bfa788a3a70a76d6ca411dad2b1
mgrand <m.grand@topl.me>
2022-10-27 17:53:02 -0400

    Resolved merge conflicts

--- genus-library/src/test/scala/co/topl/genusLibrary/orientDb/GenusGraphMetadataTest.scala
+++ genus-library/src/test/scala/co/topl/genusLibrary/orientDb/GenusGraphMetadataTest.scala
@@ -5,6 +5,7 @@ import co.topl.models._
 import co.topl.models.utility._
 import co.topl.models.utility.HasLength.instances.bytesLength
 
+import scala.collection.immutable.ListSet
 import scala.util.Random
 
 class GenusGraphMetadataTest extends munit.FunSuite {
@@ -43,7 +44,7 @@ class GenusGraphMetadataTest extends munit.FunSuite {
     eta = eta
   )
 
-  val stakingAddressOperator = StakingAddresses.Operator(VerificationKeys.Ed25519(zeroBytes(Lengths.`32`)))
+  private val stakingAddressOperator: StakingAddresses.Operator = StakingAddresses.Operator(VerificationKeys.Ed25519(zeroBytes(Lengths.`32`)))
 
   test("typedBytes Serialization") {
     val byteArray = Random.nextBytes(TypedBytesLength)
@@ -83,4 +84,21 @@ class GenusGraphMetadataTest extends munit.FunSuite {
       "Round trip serialization of StakingAddress operator"
     )
   }
+//
+//  test("BlockBodyV2 round-trip Serialization") {
+//    val evidenceLength = implicitly[Evidence.Length].value
+//
+//    val blockBodyV2 = (0 to 3).foldLeft(ListSet.empty[TypedIdentifier]) {
+//      case (transactions, _) =>
+//        val byteArray = Random.nextBytes(evidenceLength)
+//        val transactionId = TypedBytes(IdentifierTypes.Block.BodyV2, Bytes(byteArray))
+//        transactions + transactionId
+//    }
+//
+//    assertEquals(
+//      byteArrayToBlockBodyV2(blockBodyV2ToByteArray(blockBodyV2)).toSeq,
+//      blockBodyV2.toSeq,
+//      "Round trip serialization of BlockBodyV2"
+//    )
+//  }
 }
