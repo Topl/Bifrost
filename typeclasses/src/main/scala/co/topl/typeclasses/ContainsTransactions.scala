1e462ecd7b7a00e16e9038cbd6ed369f19b7e18c
Oleg Aleksandrov <o.aleksandrov@topl.me>
2022-11-09 15:13:44 -0500

    txRoot now correctly build transaction root by using Merkle Tree

--- typeclasses/src/main/scala/co/topl/typeclasses/ContainsTransactions.scala
+++ typeclasses/src/main/scala/co/topl/typeclasses/ContainsTransactions.scala
@@ -1,12 +1,21 @@
 package co.topl.typeclasses
 
+import cats.Foldable
+import cats.data.Chain
+import cats.implicits._
+import co.topl.codecs.bytes.tetra.TetraIdentifiableInstances._
+import co.topl.codecs.bytes.typeclasses.Identifiable.ops._
+import co.topl.crypto.accumulators.LeafData
+import co.topl.crypto.accumulators.merkle.MerkleTree
+import co.topl.crypto.hash.Blake2b
+import co.topl.crypto.hash.digest.Digest32
+import co.topl.crypto.hash.implicits._
+import co.topl.models.utility.HasLength.instances._
+import co.topl.models.utility.Lengths._
 import co.topl.models.utility.{Lengths, Sized}
 import co.topl.models.{BlockV1, BloomFilter, Bytes, Transaction}
 import simulacrum.{op, typeclass}
-import co.topl.models.utility.HasLength.instances._
-import Lengths._
-import cats.{Foldable, Traverse}
-import cats.implicits._
+import co.topl.typeclasses.implicits._
 
 /**
  * Satisfies that T contains transactions
@@ -14,9 +23,11 @@ import cats.implicits._
 @typeclass trait ContainsTransactions[T] {
   @op("transactions") def transactionsOf(t: T): Seq[Transaction]
 
-  @op("merkleTree") def merkleTreeOf(t: T): Sized.Strict[Bytes, Lengths.`32`.type] =
-    // TODO
-    Sized.strictUnsafe[Bytes, Lengths.`32`.type](Bytes(Array.fill[Byte](32)(1)))
+  @op("merkleTree") def merkleTreeOf(t: T): Sized.Strict[Bytes, Lengths.`32`.type] = {
+    val rootHash =
+      MerkleTree[Blake2b, Digest32](transactionsOf(t).map(tx => LeafData(tx.id.asTypedBytes.allBytes.toArray))).rootHash.bytes
+    Sized.strictUnsafe[Bytes, Lengths.`32`.type](Bytes(rootHash))
+  }
 
   @op("bloomFilter") def bloomFilterOf(t: T): BloomFilter =
     // TODO
@@ -34,5 +45,6 @@ object ContainsTransactions {
 
     implicit val blockV1ContainsTransactions: ContainsTransactions[BlockV1] = _.transactions
   }
+
   object Instances extends Instances
 }
