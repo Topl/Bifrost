9fb27b117b133c7badee30718af149ad3c485b6c
Ignacio Javier Fernández Soto <i.soto@topl.me>
2022-11-09 15:11:44 -0300

    TSDK-135 Fixed docs of some algebras

--- genus-library/src/main/scala/co/topl/genusLibrary/algebras/ChainHeightAlgebra.scala
+++ genus-library/src/main/scala/co/topl/genusLibrary/algebras/ChainHeightAlgebra.scala
@@ -1,13 +1,13 @@
 package co.topl.genusLibrary.algebras
 
 /**
- * Represents the current height of the largest chain in the data store.
+ * Represents the current height of the chain in the data store.
  * @tparam F the effect-ful context to retrieve the value in
  */
 trait ChainHeightAlgebra[F[_]] {
 
   /**
-   * Retrieves the current height of the largest chain.
+   * Retrieves the current height of the chain in the data store.
    * @return the current height
    */
   def get: StoreResponse[F, Long]
