c2e275a87ef7126b72923a6267b836de87a6d846
Ignacio Javier Fernández Soto <i.soto@topl.me>
2022-11-10 17:55:29 -0300

    TSDK-135 Added comments

--- genus-library/src/main/scala/co/topl/genusLibrary/algebras/ChainUpdaterAlgebra.scala
+++ genus-library/src/main/scala/co/topl/genusLibrary/algebras/ChainUpdaterAlgebra.scala
@@ -2,6 +2,13 @@ package co.topl.genusLibrary.algebras
 
 /**
  * Updater of the chain in the data store.
+ *
+ * Single-responsibility principle for this algebra and the rest of algebras is enforced,
+ * making each trait having a single responsibility.
+ * It gives the developer much more flexibility on how to implement the traits.
+ * For Topl's particular use case, one class/object should be created from each trait.
+ * Concrete methods will be rather large and may be hard to read on a single file.
+ *
  * @tparam F the effect-ful context to retrieve the value in
  */
 trait ChainUpdaterAlgebra[F[_]] {
