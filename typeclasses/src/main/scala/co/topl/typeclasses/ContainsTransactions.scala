90d379377ea4600258db6a120b064c7b32de6c53
Oleg Aleksandrov <115494748+o-aleksandrov-topl@users.noreply.github.com>
2022-11-10 12:27:07 -0500

    txRoot now correctly build transaction root by using Merkle Tree (#2582)
    * txRoot now correctly build transaction root by using Merkle Tree
    
    * Fixed cycled dependency issue

--- typeclasses/src/main/scala/co/topl/typeclasses/ContainsTransactions.scala
+++ typeclasses/src/main/scala/co/topl/typeclasses/ContainsTransactions.scala
@@ -1,12 +1,20 @@
 package co.topl.typeclasses
 
+import cats.Foldable
+import cats.implicits._
+import co.topl.codecs.bytes.tetra.TetraIdentifiableInstances._
+import co.topl.codecs.bytes.typeclasses.Identifiable.ops._
+import co.topl.crypto.accumulators.LeafData
+import co.topl.crypto.accumulators.merkle.MerkleTree
+import co.topl.crypto.hash.Blake2b
+import co.topl.crypto.hash.digest.Digest32
+import co.topl.crypto.hash.implicits._
+import co.topl.models.utility.HasLength.instances._
+import co.topl.models.utility.Lengths._
 import co.topl.models.utility.{Lengths, Sized}
 import co.topl.models.{BlockV1, BloomFilter, Bytes, Transaction}
+import co.topl.typeclasses.IdentityOps._
 import simulacrum.{op, typeclass}
-import co.topl.models.utility.HasLength.instances._
-import Lengths._
-import cats.{Foldable, Traverse}
-import cats.implicits._
 
 /**
  * Satisfies that T contains transactions
@@ -14,9 +22,11 @@ import cats.implicits._
 @typeclass trait ContainsTransactions[T] {
   @op("transactions") def transactionsOf(t: T): Seq[Transaction]
 
-  @op("merkleTree") def merkleTreeOf(t: T): Sized.Strict[Bytes, Lengths.`32`.type] =
-    // TODO
-    Sized.strictUnsafe[Bytes, Lengths.`32`.type](Bytes(Array.fill[Byte](32)(1)))
+  @op("merkleTree") def merkleTreeOf(t: T): Sized.Strict[Bytes, Lengths.`32`.type] = {
+    val leafs = transactionsOf(t).map(tx => LeafData(tx.id.asTypedBytes.allBytes.toArray))
+    val rootHash = MerkleTree[Blake2b, Digest32](leafs).rootHash.bytes
+    Sized.strictUnsafe[Bytes, Lengths.`32`.type](Bytes(rootHash))
+  }
 
   @op("bloomFilter") def bloomFilterOf(t: T): BloomFilter =
     // TODO
@@ -34,5 +44,6 @@ object ContainsTransactions {
 
     implicit val blockV1ContainsTransactions: ContainsTransactions[BlockV1] = _.transactions
   }
+
   object Instances extends Instances
 }
