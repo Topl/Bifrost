14a756c12d4790ff72fb6f5a3679b301fd1915af
Fernando <f.torterolo@topl.me>
2022-12-06 08:57:51 -0300

    BN-691 v1 implementation (#2626)

--- blockchain/src/test/scala/co/topl/blockchain/MempoolBroadcasterSpec.scala
+++ blockchain/src/test/scala/co/topl/blockchain/MempoolBroadcasterSpec.scala
@@ -1,14 +1,15 @@
 package co.topl.blockchain
 
 import akka.actor.ActorSystem
-import akka.stream.testkit.scaladsl.TestSink
 import akka.testkit.{TestKit, TestKitBase}
+
 import cats.effect.IO
 import cats.implicits._
 import co.topl.ledger.algebras.MempoolAlgebra
 import co.topl.models.ModelGenerators._
 import co.topl.models.TypedIdentifier
 import co.topl.typeclasses.implicits._
+import fs2.concurrent.Topic
 import munit.{CatsEffectSuite, ScalaCheckEffectSuite}
 import org.scalacheck.effect.PropF
 import org.scalamock.munit.AsyncMockFactory
@@ -23,12 +24,15 @@ class MempoolBroadcasterSpec extends CatsEffectSuite with ScalaCheckEffectSuite 
         for {
           delegate <- mock[MempoolAlgebra[F]].pure[F]
           _ = (delegate.add _).expects(transactionId).once().returning(IO.unit)
-          (underTest, source) <- MempoolBroadcaster.make(delegate)
-          sub = source.runWith(TestSink.probe)
-          _ = sub.request(1)
-          _ <- underTest.add(transactionId)
-          id = sub.expectNext()
-          _ = IO(id === transactionId).assert
+          txsAdoptionsTopic <- Topic[F, TypedIdentifier]
+          (underTest)       <- MempoolBroadcaster.make(delegate, txsAdoptionsTopic)
+          _ <- txsAdoptionsTopic.subscribeAwait(Int.MaxValue).use { stream =>
+            for {
+              _ <- underTest.add(transactionId)
+              i <- stream.take(1).compile.toVector
+              _ = IO(i.head === transactionId).assert
+            } yield ()
+          }
         } yield ()
       }
     }
