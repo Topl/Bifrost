1ea972f150bad12c0f2112b5e103b46ff21cb0a2
Ignacio Javier Fernández Soto <30632132+ignaciofernandezsoto@users.noreply.github.com>
2023-02-07 14:45:27 -0300

    TSDK-289 | Create Vertex Fetcher (#2760)
    * TSDK-289 Created VertexFetcher, its implementation and tests
    
    * TSDK-289 Refactored GraphHeaderMediator to use VertexFetcher

--- genus-library/src/test/scala/co/topl/genusLibrary/interpreter/mediator/GraphHeaderMediatorSpec.scala
+++ genus-library/src/test/scala/co/topl/genusLibrary/interpreter/mediator/GraphHeaderMediatorSpec.scala
@@ -4,12 +4,10 @@ import cats.effect.IO
 import cats.effect.kernel.Async
 import cats.implicits._
 import co.topl.consensus.models.BlockHeader
-import co.topl.genusLibrary.failure.{Failure, Failures}
+import co.topl.genusLibrary.failure.Failure
 import co.topl.genusLibrary.model.BlockData
-import co.topl.genusLibrary.orientDb.GenusGraphMetadata.{blockBodySchema, blockHeaderSchema}
 import co.topl.genusLibrary.orientDb.wrapper.{WrappedEdge, WrappedVertex}
-import co.topl.genusLibrary.orientDb.{GraphTxDAO, StoreFacade}
-import co.topl.genusLibrary.utils.BlockUtils
+import co.topl.genusLibrary.orientDb.{GraphTxDAO, StoreFacade, VertexFetcher}
 import co.topl.models.generators.consensus.ModelGenerators._
 import co.topl.models.generators.node.ModelGenerators.arbitraryNodeBody
 import co.topl.node.models.BlockBody
@@ -18,32 +16,31 @@ import org.scalacheck.effect.PropF
 import org.scalamock.munit.AsyncMockFactory
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
-import scodec.bits.ByteVector
 
 class GraphHeaderMediatorSpec extends CatsEffectSuite with ScalaCheckEffectSuite with AsyncMockFactory {
 
   type F[A] = IO[A]
 
   private class GraphTxDAOMock extends GraphTxDAO[F](null)
+  private trait VertexFetcherMock extends VertexFetcher[F]
 
   implicit private val logger: Logger[F] = Slf4jLogger.getLoggerFromClass[F](this.getClass)
 
   private val dbFacade = mock[StoreFacade]
-  private val blockUtils = mock[BlockUtils]
+  private val vertexFetcher = mock[VertexFetcherMock]
 
-  private val headerMediator = new GraphHeaderMediator[F](dbFacade, blockUtils)
+  private val headerMediator = new GraphHeaderMediator[F](dbFacade, vertexFetcher)
 
-  test("On no current header vertex, a NoCurrentHeaderVertexFailure should be returned") {
+  test("On no current header vertex failure, said failure should be returned") {
 
     PropF.forAllF {
       (
-        header:  BlockHeader,
-        blockId: Array[Byte]
+        header: BlockHeader,
       ) =>
         withMock {
           val graphTxDao = mock[GraphTxDAOMock]
 
-          val leftFailure = Failures.NoCurrentHeaderVertexFailure(ByteVector(blockId)).asLeft[Unit]
+          val failure = mock[Failure]
 
           (dbFacade
             .getGraph[F](_: Async[F], _: Logger[F]))
@@ -51,44 +48,38 @@ class GraphHeaderMediatorSpec extends CatsEffectSuite with ScalaCheckEffectSuite
             .returns(graphTxDao)
             .once()
 
-          (blockUtils.getBlockId _)
+          (vertexFetcher.fetchHeader _)
             .expects(header)
-            .returns(blockId)
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", blockId, *)
-            .returns(Option.empty[WrappedVertex].pure[F])
+            .returns(failure.asLeft[WrappedVertex].pure[F])
             .once()
 
           (graphTxDao.withEffectfulTransaction[Unit] _)
             .expects(*)
-            .onCall((f: F[Either[Failure, Unit]]) => f.ensure(new IllegalArgumentException())(_ == leftFailure))
+            .onCall((f: F[Either[Failure, Unit]]) =>
+              f.ensure(new IllegalArgumentException())(_ == failure.asLeft[Unit])
+            )
             .once()
 
           assertIO(
             headerMediator.mediate(BlockData(header = header, body = null, transactions = null)),
-            leftFailure
+            failure.asLeft[Unit]
           )
         }
     }
 
   }
 
-  test("On no previous header vertex, a NoCurrentHeaderVertexFailure should be returned") {
+  test("On no previous header vertex failure, said failure should be returned") {
 
     PropF.forAllF {
       (
-        header:        BlockHeader,
-        blockId:       Array[Byte],
-        parentBlockId: Array[Byte]
+        header: BlockHeader
       ) =>
         withMock {
           val graphTxDao = mock[GraphTxDAOMock]
           val currentHeaderVertex = mock[WrappedVertex]
 
-          val leftFailure = Failures.NoPreviousHeaderVertexFailure(ByteVector(parentBlockId)).asLeft[Unit]
+          val failure = mock[Failure]
 
           (dbFacade
             .getGraph[F](_: Async[F], _: Logger[F]))
@@ -96,131 +87,47 @@ class GraphHeaderMediatorSpec extends CatsEffectSuite with ScalaCheckEffectSuite
             .returns(graphTxDao)
             .once()
 
-          (blockUtils.getBlockId _)
+          (vertexFetcher.fetchHeader _)
             .expects(header)
-            .returns(blockId)
+            .returns(currentHeaderVertex.asRight[Failure].pure[F])
             .once()
 
-          (blockUtils.getParentBlockId _)
+          (vertexFetcher.fetchPreviousHeader _)
             .expects(header)
-            .returns(parentBlockId)
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", blockId, *)
-            .returns(currentHeaderVertex.some.pure[F])
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", parentBlockId, *)
-            .returns(Option.empty[WrappedVertex].pure[F])
+            .returns(failure.asLeft[WrappedVertex].pure[F])
             .once()
 
           (graphTxDao.withEffectfulTransaction[Unit] _)
             .expects(*)
-            .onCall((f: F[Either[Failure, Unit]]) => f.ensure(new IllegalArgumentException())(_ == leftFailure))
+            .onCall((f: F[Either[Failure, Unit]]) =>
+              f.ensure(new IllegalArgumentException())(_ == failure.asLeft[Unit])
+            )
             .once()
 
           assertIO(
             headerMediator.mediate(BlockData(header = header, body = null, transactions = null)),
-            leftFailure
+            failure.asLeft[Unit]
           )
         }
     }
 
-  }
-
-  test("On no body vertex, a NoCurrentBodyVertexFailure should be returned") {
-
-    PropF.forAllF {
-      (
-        header:        BlockHeader,
-        body:          BlockBody,
-        blockId:       Array[Byte],
-        parentBlockId: Array[Byte],
-        bodyArray:     Array[Byte]
-      ) =>
-        withMock {
-          val graphTxDao = mock[GraphTxDAOMock]
-          val currentHeaderVertex = mock[WrappedVertex]
-          val previousHeaderVertex = mock[WrappedVertex]
-
-          val leftFailure = Failures.NoCurrentBodyVertexFailure(ByteVector(bodyArray)).asLeft[Unit]
-
-          (dbFacade
-            .getGraph[F](_: Async[F], _: Logger[F]))
-            .expects(*, *)
-            .returns(graphTxDao)
-            .once()
-
-          (blockUtils.getBlockId _)
-            .expects(header)
-            .returns(blockId)
-            .once()
-
-          (blockUtils.getParentBlockId _)
-            .expects(header)
-            .returns(parentBlockId)
-            .once()
-
-          (blockUtils.blockBodyToByteArray _)
-            .expects(body)
-            .returns(bodyArray)
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", blockId, *)
-            .returns(currentHeaderVertex.some.pure[F])
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", parentBlockId, *)
-            .returns(previousHeaderVertex.some.pure[F])
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockBodySchema.name, "transactionIds", bodyArray, *)
-            .returns(Option.empty[WrappedVertex].pure[F])
-            .once()
-
-          (graphTxDao.withEffectfulTransaction[Unit] _)
-            .expects(*)
-            .onCall((f: F[Either[Failure, Unit]]) => f.ensure(new IllegalArgumentException())(_ == leftFailure))
-            .once()
-
-          assertIO(
-            headerMediator.mediate(BlockData(header = header, body = body, transactions = null)),
-            leftFailure
-          )
-        }
-    }
   }
 
   test(
-    "On header, previous header and body vertices, " +
-    "on all edges created successfully but with transaction failure, it should be bubbled up"
+    "On header and previous header vertices, " +
+    "on the edge created successfully but with transaction failure, it should be bubbled up"
   ) {
 
     PropF.forAllF {
       (
-        header:        BlockHeader,
-        body:          BlockBody,
-        blockId:       Array[Byte],
-        parentBlockId: Array[Byte],
-        bodyArray:     Array[Byte]
+        header: BlockHeader,
+        body:   BlockBody
       ) =>
         withMock {
           val graphTxDao = mock[GraphTxDAOMock]
           val currentHeaderVertex = mock[WrappedVertex]
           val previousHeaderVertex = mock[WrappedVertex]
-          val bodyVertex = mock[WrappedVertex]
           val headersEdge = mock[WrappedEdge]
-          val headerBodyEdge = mock[WrappedEdge]
 
           val leftFailure = mock[Failure].asLeft[Unit]
 
@@ -230,49 +137,20 @@ class GraphHeaderMediatorSpec extends CatsEffectSuite with ScalaCheckEffectSuite
             .returns(graphTxDao)
             .once()
 
-          (blockUtils.getBlockId _)
+          (vertexFetcher.fetchHeader _)
             .expects(header)
-            .returns(blockId)
+            .returns(currentHeaderVertex.asRight[Failure].pure[F])
             .once()
 
-          (blockUtils.getParentBlockId _)
+          (vertexFetcher.fetchPreviousHeader _)
             .expects(header)
-            .returns(parentBlockId)
-            .once()
-
-          (blockUtils.blockBodyToByteArray _)
-            .expects(body)
-            .returns(bodyArray)
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", blockId, *)
-            .returns(currentHeaderVertex.some.pure[F])
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", parentBlockId, *)
-            .returns(previousHeaderVertex.some.pure[F])
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockBodySchema.name, "transactionIds", bodyArray, *)
-            .returns(bodyVertex.some.pure[F])
+            .returns(previousHeaderVertex.asRight[Failure].pure[F])
             .once()
 
           (graphTxDao
             .addEdge(_: WrappedVertex, _: WrappedVertex, _: Option[String]))
             .expects(previousHeaderVertex, currentHeaderVertex, None)
             .returns(headersEdge)
-            .once()
-
-          (graphTxDao
-            .addEdge(_: WrappedVertex, _: WrappedVertex, _: Option[String]))
-            .expects(currentHeaderVertex, bodyVertex, None)
-            .returns(headerBodyEdge)
             .once()
 
           (graphTxDao.withEffectfulTransaction[Unit] _)
@@ -292,25 +170,20 @@ class GraphHeaderMediatorSpec extends CatsEffectSuite with ScalaCheckEffectSuite
   }
 
   test(
-    "On header, previous header and body vertices, " +
-    "on all edges created successfully but with transaction success, original return should be bubbled up"
+    "On header and previous header, " +
+    "on the edge created successfully but with transaction success, original return should be bubbled up"
   ) {
 
     PropF.forAllF {
       (
-        header:        BlockHeader,
-        body:          BlockBody,
-        blockId:       Array[Byte],
-        parentBlockId: Array[Byte],
-        bodyArray:     Array[Byte]
+        header: BlockHeader,
+        body:   BlockBody
       ) =>
         withMock {
           val graphTxDao = mock[GraphTxDAOMock]
           val currentHeaderVertex = mock[WrappedVertex]
           val previousHeaderVertex = mock[WrappedVertex]
-          val bodyVertex = mock[WrappedVertex]
           val headersEdge = mock[WrappedEdge]
-          val headerBodyEdge = mock[WrappedEdge]
 
           (dbFacade
             .getGraph[F](_: Async[F], _: Logger[F]))
@@ -318,49 +191,20 @@ class GraphHeaderMediatorSpec extends CatsEffectSuite with ScalaCheckEffectSuite
             .returns(graphTxDao)
             .once()
 
-          (blockUtils.getBlockId _)
+          (vertexFetcher.fetchHeader _)
             .expects(header)
-            .returns(blockId)
+            .returns(currentHeaderVertex.asRight[Failure].pure[F])
             .once()
 
-          (blockUtils.getParentBlockId _)
+          (vertexFetcher.fetchPreviousHeader _)
             .expects(header)
-            .returns(parentBlockId)
-            .once()
-
-          (blockUtils.blockBodyToByteArray _)
-            .expects(body)
-            .returns(bodyArray)
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", blockId, *)
-            .returns(currentHeaderVertex.some.pure[F])
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockHeaderSchema.name, "blockId", parentBlockId, *)
-            .returns(previousHeaderVertex.some.pure[F])
-            .once()
-
-          (dbFacade
-            .getVertexByField[F](_: String, _: String, _: AnyRef)(_: Async[F]))
-            .expects(blockBodySchema.name, "transactionIds", bodyArray, *)
-            .returns(bodyVertex.some.pure[F])
+            .returns(previousHeaderVertex.asRight[Failure].pure[F])
             .once()
 
           (graphTxDao
             .addEdge(_: WrappedVertex, _: WrappedVertex, _: Option[String]))
             .expects(previousHeaderVertex, currentHeaderVertex, None)
             .returns(headersEdge)
-            .once()
-
-          (graphTxDao
-            .addEdge(_: WrappedVertex, _: WrappedVertex, _: Option[String]))
-            .expects(currentHeaderVertex, bodyVertex, None)
-            .returns(headerBodyEdge)
             .once()
 
           (graphTxDao.withEffectfulTransaction[Unit] _)
