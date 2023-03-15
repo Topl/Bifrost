936de670352676d711818577f662bf285c409040
Fernando <f.torterolo@topl.me>
2023-03-15 16:04:21 -0300

    ChainSelectionTest, hot fix (#2851)
    * ChainSelectionTest, increase on 5 minutes
    
    Timing out job: https://github.com/Topl/Bifrost/actions/runs/4406408852/jobs/7718698418
    
    * tested via codespaces
    
    * revert to 4 nodes, fix local index mistake
    
    * Disconnected nodes can forge independently and later sync up to a proper chain 334.478s

--- byzantine-tests/src/it/scala/co/topl/tetra/it/ChainSelectionTest.scala
+++ byzantine-tests/src/it/scala/co/topl/tetra/it/ChainSelectionTest.scala
@@ -9,28 +9,26 @@ import org.typelevel.log4cats.Logger
 import scala.concurrent.duration._
 
 /**
- * This test creates ~4 nodes that share the same seed but are not configured to talk to each other (meaning they
+ * This test creates ~3 nodes that share the same seed but are not configured to talk to each other (meaning they
  * are siloed from one another).  After allowing each node to forge its own unique chain, the nodes are restarted with
- * communication enabled.  The nodes are expected to share the blocks they've forged with the other nodes, and each
+ * communication enabled.  The nodes are expected to share the blocks they've produced by the other nodes, and each
  * node should independently agree that one chain is the best.
  */
 class ChainSelectionTest extends IntegrationSuite {
 
-  override def munitTimeout: Duration = 20.minutes
+  override def munitTimeout: Duration = 25.minutes
 
   test("Disconnected nodes can forge independently and later sync up to a proper chain") {
     val epochSlotLength = 500 // (50/4) * (100/15) * 6
     val bigBang = Instant.now().plusSeconds(30)
-    val config0 = DefaultConfig(bigBang, stakerCount = 4, localStakerIndex = 0, knownPeers = Nil)
-    val config1 = DefaultConfig(bigBang, stakerCount = 4, localStakerIndex = 1, knownPeers = Nil)
-    val config2 = DefaultConfig(bigBang, stakerCount = 4, localStakerIndex = 2, knownPeers = Nil)
-    val config3 = DefaultConfig(bigBang, stakerCount = 4, localStakerIndex = 3, knownPeers = Nil)
+    val config0 = DefaultConfig(bigBang, stakerCount = 3, localStakerIndex = 0, knownPeers = Nil)
+    val config1 = DefaultConfig(bigBang, stakerCount = 3, localStakerIndex = 1, knownPeers = Nil)
+    val config2 = DefaultConfig(bigBang, stakerCount = 3, localStakerIndex = 2, knownPeers = Nil)
 
     val nodesWithknownPeers = List(
-      DefaultConfig(bigBang, stakerCount = 4, localStakerIndex = 1, knownPeers = Nil),
-      DefaultConfig(bigBang, stakerCount = 4, localStakerIndex = 1, knownPeers = List("ChainSelectionTest-node0")),
-      DefaultConfig(bigBang, stakerCount = 4, localStakerIndex = 2, knownPeers = List("ChainSelectionTest-node1")),
-      DefaultConfig(bigBang, stakerCount = 4, localStakerIndex = 3, knownPeers = List("ChainSelectionTest-node2"))
+      DefaultConfig(bigBang, stakerCount = 3, localStakerIndex = 0, knownPeers = Nil),
+      DefaultConfig(bigBang, stakerCount = 3, localStakerIndex = 1, knownPeers = List("ChainSelectionTest-node0")),
+      DefaultConfig(bigBang, stakerCount = 3, localStakerIndex = 2, knownPeers = List("ChainSelectionTest-node1"))
     )
 
     val resource = for {
@@ -39,8 +37,8 @@ class ChainSelectionTest extends IntegrationSuite {
       node1 <- dockerSupport.createNode("ChainSelectionTest-node0", "ChainSelectionTest", config0)
       node2 <- dockerSupport.createNode("ChainSelectionTest-node1", "ChainSelectionTest", config1)
       node3 <- dockerSupport.createNode("ChainSelectionTest-node2", "ChainSelectionTest", config2)
-      node4 <- dockerSupport.createNode("ChainSelectionTest-node3", "ChainSelectionTest", config3)
-      nodes = List(node1, node2, node3, node4)
+      
+      nodes = List(node1, node2, node3)
       _ <- nodes.parTraverse(_.startContainer[F]).toResource
       _ <- nodes.parTraverse(_.rpcClient[F].use(_.waitForRpcStartUp)).toResource
       _ <- Logger[F].info("Waiting for nodes to reach target epoch.  This may take several minutes.").toResource
@@ -48,23 +46,23 @@ class ChainSelectionTest extends IntegrationSuite {
         .parTraverse(node =>
           node
             .rpcClient[F]
-            .use(_.adoptedHeaders.takeWhile(_.slot < (epochSlotLength)).timeout(10.minutes).compile.lastOrError)
+            .use(_.adoptedHeaders.takeWhile(_.slot < (epochSlotLength)).timeout(15.minutes).compile.lastOrError)
         )
         .toResource
 
-      _ <- blockHeaders.size.pure[F].assertEquals(4).toResource
-      _ <- blockHeaders.distinct.size.pure[F].assertEquals(4).toResource
+      _ <- blockHeaders.size.pure[F].assertEquals(3).toResource
+      _ <- blockHeaders.distinct.size.pure[F].assertEquals(3).toResource
 
-      // Stop, configure with KnownPeers, and run again
+      // Stop, configure with KnownPeers, restart containers and run again
       _ <- nodes.parTraverse(_.stop[F]).toResource
       _ <- nodes.zip(nodesWithknownPeers).parTraverse { case (node, config) => node.configure[F](config) }.toResource
       _ <- nodes.parTraverse(_.restartContainer[F]).toResource
       _ <- nodes.parTraverse(_.rpcClient[F].use(_.waitForRpcStartUp)).toResource
-      _ <- Logger[F].info("Waiting for nodes to reach target epoch.  This may take several minutes.").toResource
+      _ <- Logger[F].info("ChainSelectionTest Waiting for nodes to reach target epoch.  This may take several minutes.").toResource
       thirdEpochHeads <- nodes
         .parTraverse(
           _.rpcClient[F]
-            .use(_.adoptedHeaders.takeWhile(_.slot < (epochSlotLength * 3)).timeout(15.minutes).compile.lastOrError)
+            .use(_.adoptedHeaders.takeWhile(_.slot < (epochSlotLength * 3)).timeout(20.minutes).compile.lastOrError)
         )
         .toResource
       _ <- Logger[F].info("Nodes have reached target epoch").toResource
