fa2c564efcc4ca56d6d423b486343dde8c35f22a
Sean Cheatham <SeanCheatham@users.noreply.github.com>
2023-03-16 15:58:10 -0400

    Remove unused codecs (#2855)

--- tetra-byte-codecs/src/main/scala/co/topl/codecs/bytes/tetra/TetraScodecCodecs.scala
+++ tetra-byte-codecs/src/main/scala/co/topl/codecs/bytes/tetra/TetraScodecCodecs.scala
@@ -1,18 +1,13 @@
 package co.topl.codecs.bytes.tetra
 
-import cats.implicits._
 import co.topl.codecs.bytes.scodecs._
 import co.topl.consensus.models._
 import co.topl.crypto.{models => nodeCryptoModels}
 import co.topl.models._
-import co.topl.models.utility.HasLength.instances._
 import co.topl.models.utility._
-import com.google.protobuf.ByteString
 import scodec.codecs.discriminated
 import scodec.codecs.lazily
-import scodec.Attempt
 import scodec.Codec
-import scodec.Err
 import shapeless.::
 import shapeless.HList
 import shapeless.HNil
@@ -24,30 +19,11 @@ object TetraScodecCodecs extends TetraScodecCodecs
 
 trait TetraScodecCodecs {
 
-  implicit def maxSizedCodec[T: Codec: HasLength, L <: Length](implicit l: L): Codec[Sized.Max[T, L]] =
-    Codec[T]
-      .exmapc(t =>
-        Attempt.fromEither(
-          Sized.max(t).leftMap(e => Err(s"Invalid length: expected ${l.value} but got ${e.length}"))
-        )
-      )(t => Attempt.successful(t.data))
-
-  implicit def strictSizedBytesCodec[L <: Length](implicit l: L): Codec[Sized.Strict[Bytes, L]] =
-    byteStringCodecSized(l.value)
-      .exmapc[Sized.Strict[Bytes, L]](t =>
-        Attempt.fromEither(
-          Sized.strict(t).leftMap(e => Err(s"Invalid length: expected ${l.value} but got ${e.length}"))
-        )
-      )(t => Attempt.successful(t.data))
-
-  implicit val byteStringCodec: Codec[Bytes] =
-    arrayCodec[Byte].xmap(arr => ByteString.copyFrom(arr), _.toArray)
-
   // todo: JAA - consider implications of variable vs. fixed length (BigInt vs. Int128)
   // (I think this is never transmitted so probably safe if we used built in BigInt)
 
   implicit val bigIntCodec: Codec[BigInt] =
-    arrayCodec[Byte].xmapc(t => BigInt(t))(t => t.toByteArray)
+    byteArrayCodec.xmapc(t => BigInt(t))(t => t.toByteArray)
 
   implicit val ratioCodec: Codec[Ratio] =
     (bigIntCodec :: bigIntCodec)
@@ -64,25 +40,19 @@ trait TetraScodecCodecs {
   implicit val unknownFieldSetCodec: Codec[scalapb.UnknownFieldSet] =
     emptyCodec(scalapb.UnknownFieldSet.empty)
 
-  implicit val networkPrefixCodec: Codec[NetworkPrefix] =
-    Codec[Byte].xmap(NetworkPrefix(_), _.value)
-
-  implicit val rhoCodec: Codec[Rho] =
-    Codec[Sized.Strict[Bytes, Lengths.`64`.type]].xmap(Rho(_), _.sizedBytes)
-
   implicit val nodeCryptoKesBinaryTreeCodec: Codec[nodeCryptoModels.KesBinaryTree] = {
     val kesBinaryTreeEmptyCodec: Codec[nodeCryptoModels.KesBinaryTree.Empty] =
       emptyCodec(nodeCryptoModels.KesBinaryTree.Empty())
 
     val nodeCryptoKesBinaryTreeLeafCodec: Codec[nodeCryptoModels.KesBinaryTree.SigningLeaf] =
-      (arrayCodec[Byte] :: arrayCodec[Byte])
+      (byteArrayCodecSized(32) :: byteArrayCodecSized(32))
         .as[nodeCryptoModels.KesBinaryTree.SigningLeaf]
 
     val nodeCryptoKesBinaryTreeNodeCodec: Codec[nodeCryptoModels.KesBinaryTree.MerkleNode] =
       lazily(
-        (arrayCodec[Byte] :: arrayCodec[Byte] :: arrayCodec[
-          Byte
-        ] :: nodeCryptoKesBinaryTreeCodec :: nodeCryptoKesBinaryTreeCodec)
+        (byteArrayCodecSized(32) :: byteArrayCodecSized(32) :: byteArrayCodecSized(
+          32
+        ) :: nodeCryptoKesBinaryTreeCodec :: nodeCryptoKesBinaryTreeCodec)
           .as[nodeCryptoModels.KesBinaryTree.MerkleNode]
       )
 
