7e3ead2c5f914518569b8bac0a4a9de615e02efc
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-03-30 15:05:24 +0200

    Dependency updates in dev (#2884)
    * Update grpc-netty-shaded to 1.54.0 in dev
    
    * Update grpc-services to 1.54.0 in dev
    
    * Update scalafmt-core to 3.7.3 in dev
    
    * Reformat with scalafmt 3.7.3
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.7.3' to .git-blame-ignore-revs
    
    * Update sbt-wartremover to 3.0.13 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -253,8 +253,8 @@ object Dependencies {
     mUnitTest ++
     protobufSpecs ++
     Seq(
-      "io.grpc" % "grpc-netty-shaded" % "1.53.0",
-      "io.grpc" % "grpc-services"     % "1.53.0"
+      "io.grpc" % "grpc-netty-shaded" % "1.54.0",
+      "io.grpc" % "grpc-services"     % "1.54.0"
     )
 
   lazy val levelDbStore: Seq[ModuleID] =
