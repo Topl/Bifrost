7da7ae3721fd51257c8264161979d496eb5b4d71
Sean Cheatham <SeanCheatham@users.noreply.github.com>
2023-04-03 14:58:27 -0400

    Repopulate Eligibility Cache (#2901)
    * Create EligibilityCache.repopulate function
    
    * Update EligibilityCache.repopulate to use a stream implementation

--- consensus/src/main/scala/co/topl/consensus/interpreters/EligibilityCache.scala
+++ consensus/src/main/scala/co/topl/consensus/interpreters/EligibilityCache.scala
@@ -2,7 +2,9 @@ package co.topl.consensus.interpreters
 
 import cats.effect._
 import cats.effect.implicits._
+import cats.implicits._
 import co.topl.consensus.algebras.EligibilityCacheAlgebra
+import co.topl.consensus.models._
 import co.topl.models._
 import com.google.protobuf.ByteString
 
@@ -29,6 +31,35 @@ object EligibilityCache {
       def tryInclude(vrfVK: Bytes, slot: Slot): F[Boolean] =
         ref.modify(_.tryInclude(vrfVK, slot))
     }
+
+  /**
+   * When a node is launched, eligibilities from adopted blocks should be added to the cache.
+   *
+   * @param underlying    The underlying cache to populate
+   * @param maximumLength The maximum number of entries in the underlying cache
+   * @param canonicalHead The current head of the chain
+   * @param fetchHeader   Header lookup function (to traverse ancestors)
+   */
+  def repopulate[F[_]: Async](
+    underlying:    EligibilityCacheAlgebra[F],
+    maximumLength: Int,
+    canonicalHead: BlockHeader,
+    fetchHeader:   BlockId => F[BlockHeader]
+  ): F[Unit] = {
+    import fs2._
+    // A stream of block headers from the canonical head back to the genesis block
+    // The canonical head is not included in this stream.
+    val ancestors =
+      Stream.unfoldEval(canonicalHead)(header =>
+        if (header.height <= 1) none[(BlockHeader, BlockHeader)].pure[F]
+        else fetchHeader(header.parentHeaderId).map(parentH => (parentH, parentH).some)
+      )
+    (Stream(canonicalHead) ++ ancestors)
+      .take(maximumLength)
+      .evalMap(header => underlying.tryInclude(header.eligibilityCertificate.vrfVK, header.slot))
+      .compile
+      .drain
+  }
 
   /**
    * The internal state of the cache
--- consensus/src/test/scala/co/topl/consensus/interpreters/EligibilityCacheSpec.scala
+++ consensus/src/test/scala/co/topl/consensus/interpreters/EligibilityCacheSpec.scala
@@ -2,11 +2,15 @@ package co.topl.consensus.interpreters
 
 import cats.effect.IO
 import cats.effect.implicits._
+import cats.implicits._
 import munit.CatsEffectSuite
 import munit.ScalaCheckEffectSuite
 import org.scalamock.munit.AsyncMockFactory
 import co.topl.models.ModelGenerators._
+import co.topl.models.generators.consensus.ModelGenerators._
 import co.topl.models.utility.Lengths
+import co.topl.codecs.bytes.tetra.instances._
+import co.topl.consensus.algebras.EligibilityCacheAlgebra
 
 class EligibilityCacheSpec extends CatsEffectSuite with ScalaCheckEffectSuite with AsyncMockFactory {
   type F[A] = IO[A]
@@ -58,6 +62,80 @@ class EligibilityCacheSpec extends CatsEffectSuite with ScalaCheckEffectSuite wi
       } yield ()
 
     resource.use_
+  }
+
+  test("EligibilityCache.Repopulate") {
+    withMock {
+      val baseHeader = arbitraryHeader.arbitrary.first
+      // Genesis "parent"
+      val h0 =
+        baseHeader
+          .update(_.height := 0L)
+          .update(_.slot := -1L)
+          .update(_.eligibilityCertificate.vrfVK := generateVrfVK())
+      val h0Id = h0.id
+      // Genesis
+      val h1 =
+        baseHeader
+          .update(_.height := 1L)
+          .update(_.slot := 0L)
+          .update(_.eligibilityCertificate.vrfVK := generateVrfVK())
+      val h1Id = h1.id
+      val h2 =
+        baseHeader
+          .update(_.parentHeaderId := h1Id)
+          .update(_.height := 2L)
+          .update(_.slot := 10L)
+          .update(_.eligibilityCertificate.vrfVK := generateVrfVK())
+      val h2Id = h2.id
+      val h3 =
+        baseHeader
+          .update(_.parentHeaderId := h2Id)
+          .update(_.height := 3L)
+          .update(_.slot := 15L)
+          .update(_.eligibilityCertificate.vrfVK := generateVrfVK())
+      val h3Id = h3.id
+      val h4 =
+        baseHeader
+          .update(_.parentHeaderId := h3Id)
+          .update(_.height := 4L)
+          .update(_.slot := 20L)
+          .update(_.eligibilityCertificate.vrfVK := generateVrfVK())
+      val h4Id = h4.id
+      val headers = Map(h0Id -> h0, h1Id -> h1, h2Id -> h2, h3Id -> h3, h4Id -> h4)
+      val underlying = mock[EligibilityCacheAlgebra[F]]
+      (underlying
+        .tryInclude(_, _))
+        .expects(h4.eligibilityCertificate.vrfVK, h4.slot)
+        .once()
+        .returning(true.pure[F])
+      (underlying
+        .tryInclude(_, _))
+        .expects(h3.eligibilityCertificate.vrfVK, h3.slot)
+        .once()
+        .returning(true.pure[F])
+      (underlying
+        .tryInclude(_, _))
+        .expects(h2.eligibilityCertificate.vrfVK, h2.slot)
+        .once()
+        .returning(true.pure[F])
+      (underlying
+        .tryInclude(_, _))
+        .expects(h1.eligibilityCertificate.vrfVK, h1.slot)
+        .once()
+        .returning(true.pure[F])
+      // The genesis parent should never be called
+      (underlying
+        .tryInclude(_, _))
+        .expects(h0.eligibilityCertificate.vrfVK, h0.slot)
+        .never()
+      val testResource =
+        for {
+          _ <- EligibilityCache.repopulate[F](underlying, 10, h4, headers(_).pure[F]).toResource
+        } yield ()
+
+      testResource.use_
+    }
   }
 
   private def generateVrfVK() = genSizedStrictBytes[Lengths.`32`.type]().first.data
