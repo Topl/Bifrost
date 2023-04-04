b3ff1e2641ea4eff161333a9af0cd123af9dee3e
Sean Cheatham <seancheatham@gmail.com>
2023-04-04 09:49:04 -0400

    EligibilityCacheAlgebra scaladoc

--- consensus/src/main/scala/co/topl/consensus/algebras/EligibilityCacheAlgebra.scala
+++ consensus/src/main/scala/co/topl/consensus/algebras/EligibilityCacheAlgebra.scala
@@ -11,6 +11,7 @@ trait EligibilityCacheAlgebra[F[_]] {
 
   /**
    * Attempt to include the given eligibility in the cache
+   * @param blockId the block ID associated with the eligibility
    * @param vrfVK The staker's VRF VK
    * @param slot The slot for which the eligibility was generated
    * @return true if the eligibility was inserted, false if it was already in the cache
