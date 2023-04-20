e3f79787227d71512b2c447fed682a2b8d65d50c
Sean Cheatham <seancheatham@gmail.com>
2023-04-20 14:02:36 -0400

    Update scaladoc for OrientThread

--- genus-library/src/main/scala/co/topl/genusLibrary/orientDb/OrientThread.scala
+++ genus-library/src/main/scala/co/topl/genusLibrary/orientDb/OrientThread.scala
@@ -7,7 +7,9 @@ import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
 
 /**
- * Provides an interface for executing OrientDB operations on a dedicated thread
+ * Provides an interface for executing OrientDB operations on a dedicated thread.  OrientDB is "extra thread-safe", meaning
+ * it requires the database to be registered on the current thread for several types of interactions.  This OrientThread
+ * helper assists with running computations on a thread that is properly registered.
  */
 trait OrientThread[F[_]] {
 
