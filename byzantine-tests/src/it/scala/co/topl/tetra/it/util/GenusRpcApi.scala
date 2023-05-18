f5ee3ed165992b70c835af5eb7d3dd6caf700d74
Fernando <f.torterolo@topl.me>
2023-05-18 11:48:27 -0300

    add Genus genesis block to sanityCheck (#2947)

---
+++ byzantine-tests/src/it/scala/co/topl/tetra/it/util/GenusRpcApi.scala
@@ -0,0 +1,29 @@
+package co.topl.tetra.it.util
+
+import cats.effect._
+import cats.implicits._
+import co.topl.algebras.ToplGenusRpc
+
+import fs2.Stream
+import org.typelevel.log4cats.Logger
+import scala.concurrent.duration._
+
+class GenusRpcApi[F[_]](val client: ToplGenusRpc[F]) extends AnyVal {
+
+  def waitForRpcStartUp(implicit asyncF: Async[F], loggerF: Logger[F]): F[Unit] =
+    for {
+      _ <- Logger[F].info("Waiting for Genus RPC to start up")
+      _ <-
+        Stream
+          .retry(
+            client
+              .blockIdAtHeight(1),
+            250.milli,
+            identity,
+            200
+          )
+          .compile
+          .lastOrError
+      _ <- Logger[F].info("Genesis block timestamp reached.  Node is ready.")
+    } yield ()
+}
