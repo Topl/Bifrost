d7556ddc74bfc0385fbe07d0f638f7036dcb20a7
Fernando Torterolo <f.torterolo@topl.me>
2023-05-23 18:27:13 -0300

    review comments

--- blockchain/src/test/scala/co/topl/blockchain/StakerInitializersSpec.scala
+++ blockchain/src/test/scala/co/topl/blockchain/StakerInitializersSpec.scala
@@ -28,7 +28,8 @@ class StakerInitializersSpec extends CatsEffectSuite with ScalaCheckEffectSuite 
         bigBangOutputs
           .map(_.value)
           .contains(
-            Value().withTopl(Value.TOPL(Ratio(DefaultTotalStake, 1: BigInt).round, operator.head.stakingAddress.some))
+            Value.defaultInstance
+              .withTopl(Value.TOPL(Ratio(DefaultTotalStake, 1: BigInt).round, operator.head.stakingAddress.some))
           )
           .pure[F]
       )
@@ -36,14 +37,78 @@ class StakerInitializersSpec extends CatsEffectSuite with ScalaCheckEffectSuite 
         bigBangOutputs
           .map(_.value)
           .contains(
-            Value().withRegistration(Value.Registration(operator.head.registration, operator.head.stakingAddress))
+            Value.defaultInstance
+              .withRegistration(Value.Registration(operator.head.registration, operator.head.stakingAddress))
           )
           .pure[F]
       )
     } yield ()
   }
 
-// TODO ask some suggestion about which kind of unit testing should we create
-//  test("Big bang outputs for PrivateTestnet, currentTime, n staker")
-//  test("Big bang outputs for other Testnet, currentTime, 1 staker")
+  test("Big bang outputs for PrivateTestnet, currentTime + 5 seg, 1 staker") {
+    for {
+      implicit0(networkPrefix: NetworkPrefix) <- NetworkPrefix(1: Byte).pure[F]
+      timestamp                               <- (System.currentTimeMillis() + 5000).pure[F]
+      operator                                <- PrivateTestnet.stakerInitializers(timestamp, 1).pure[F]
+      bigBangOutputs <- Async[F].delay(operator.head.bigBangOutputs(Ratio(DefaultTotalStake, 1: BigInt).round))
+
+      _ <- assertIO(bigBangOutputs.size.pure[F], 2)
+      _ <- assertIOBoolean(bigBangOutputs.forall(_.address == operator.head.lockAddress).pure[F])
+      _ <- assertIOBoolean(
+        bigBangOutputs
+          .map(_.value)
+          .contains(
+            Value.defaultInstance
+              .withTopl(Value.TOPL(Ratio(DefaultTotalStake, 1: BigInt).round, operator.head.stakingAddress.some))
+          )
+          .pure[F]
+      )
+      _ <- assertIOBoolean(
+        bigBangOutputs
+          .map(_.value)
+          .contains(
+            Value.defaultInstance
+              .withRegistration(Value.Registration(operator.head.registration, operator.head.stakingAddress))
+          )
+          .pure[F]
+      )
+    } yield ()
+  }
+
+  test("Big bang outputs for PrivateTestnet, currentTime, 10 stakers") {
+    for {
+      implicit0(networkPrefix: NetworkPrefix) <- NetworkPrefix(1: Byte).pure[F]
+      timestamp                               <- System.currentTimeMillis().pure[F]
+      operator                                <- PrivateTestnet.stakerInitializers(timestamp, 2).pure[F]
+      _ <- operator
+        .zip(LazyList.from(1))
+        .traverse { case (operator, index) =>
+          val bigBangOutputs = operator.bigBangOutputs(Ratio(DefaultTotalStake, index: BigInt).round)
+
+          assertIO(bigBangOutputs.size.pure[F], 2) &>
+          assertIOBoolean(bigBangOutputs.forall(_.address == operator.lockAddress).pure[F]) &>
+          assertIOBoolean(
+            bigBangOutputs
+              .map(_.value)
+              .contains(
+                Value.defaultInstance
+                  .withTopl(Value.TOPL(Ratio(DefaultTotalStake, index: BigInt).round, operator.stakingAddress.some))
+              )
+              .pure[F]
+          ) &>
+          assertIOBoolean(
+            bigBangOutputs
+              .map(_.value)
+              .contains(
+                Value.defaultInstance
+                  .withRegistration(Value.Registration(operator.registration, operator.stakingAddress))
+              )
+              .pure[F]
+          )
+
+        }
+        .void
+    } yield ()
+  }
+
 }
