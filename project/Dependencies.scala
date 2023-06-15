81ae8d1d4e8660f88a2737e7c2554a0655966944
Sean Cheatham <SeanCheatham@users.noreply.github.com>
2023-06-15 13:22:36 -0400

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,8 +10,8 @@ object Dependencies {
   val fs2Version = "3.7.0"
   val logback = "1.4.7"
   val orientDbVersion = "3.2.19"
-  val protobufSpecsVersion = "73d1844" // scala-steward:off
-  val bramblScVersion = "1379dab" // scala-steward:off
+  val protobufSpecsVersion = "e03a093" // scala-steward:off
+  val bramblScVersion = "d5bc746" // scala-steward:off
   val quivr4sVersion = "1e48130" // scala-steward:off
 
   val catsSlf4j =
