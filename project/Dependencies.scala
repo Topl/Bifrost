9426dfcf2dd9263b7aa2407235b08fa2dccfd0d8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-07-19 16:02:43 +0200

    Dependency updates (#3000)
    * Update sbt-bloop to 1.5.9 in dev
    
    * Update google-cloud-storage to 2.24.0 in dev
    
    * Update scalafmt-core to 3.7.10 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -162,7 +162,7 @@ object Dependencies {
       fs2IO,
       pureConfig,
       kubernetes,
-      "com.google.cloud" % "google-cloud-storage" % "2.23.0"
+      "com.google.cloud" % "google-cloud-storage" % "2.24.0"
     )
 
   lazy val actor: Seq[sbt.ModuleID] = fs2All
