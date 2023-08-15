92fffd74aa028f2f9295a570224edd364a05445b
Scala Steward <scala_steward@virtuslab.com>
2023-08-15 21:36:33 +0000

    Update google-cloud-storage to 2.26.1 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -161,7 +161,7 @@ object Dependencies {
       fs2IO,
       pureConfig,
       kubernetes,
-      "com.google.cloud" % "google-cloud-storage" % "2.26.0"
+      "com.google.cloud" % "google-cloud-storage" % "2.26.1"
     )
 
   lazy val actor: Seq[sbt.ModuleID] = fs2All
