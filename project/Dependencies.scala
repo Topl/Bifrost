b5435642514e11c22ac91deceedc5c66ff5f89a5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-08-28 21:52:58 +0200

    Dependency updates (#3022)
    * Update orientdb-client to 3.2.22 in dev
    
    * Update grpc-netty-shaded to 1.57.2 in dev
    
    * Revert commit(s) 24ca66ea2, 8a4428cb1
    
    * Update orientdb-client to 3.2.22 in dev
    
    * Update grpc-netty-shaded to 1.57.2 in dev
    
    * Update sbt to 1.9.4 in dev
    
    ---------
    
    Co-authored-by: Fernando <f.torterolo@topl.me>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,8 +9,8 @@ object Dependencies {
   val catsEffectVersion = "3.5.1"
   val fs2Version = "3.8.0"
   val logback = "1.4.11"
-  val orientDbVersion = "3.2.21"
-  val ioGrpcVersion = "1.57.1"
+  val orientDbVersion = "3.2.22"
+  val ioGrpcVersion = "1.57.2"
   val http4sVersion = "0.23.23"
   val protobufSpecsVersion = "2.0.0-alpha3+2-69b81d1f-SNAPSHOT" // scala-steward:off // TODO requires protobuf release
   val bramblScVersion = "2.0.0-alpha4" // scala-steward:off
