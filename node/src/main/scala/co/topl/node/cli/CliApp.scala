44d88b67372f2358319b19d985ece743647dd4c6
Sean Cheatham <seancheatham@gmail.com>
2023-09-08 09:43:03 -0400

    Implement InitTestnetCommand

--- node/src/main/scala/co/topl/node/cli/CliApp.scala
+++ node/src/main/scala/co/topl/node/cli/CliApp.scala
@@ -1,6 +1,6 @@
 package co.topl.node.cli
 
-import cats.effect.{IO, Resource}
+import cats.effect.{IO, Resource, Sync}
 import cats.implicits._
 import co.topl.config.ApplicationConfig
 
@@ -9,40 +9,42 @@ class ConfiguredCliApp(appConfig: ApplicationConfig) {
 
   type F[+A] = IO[A]
 
-  implicit private val c: Console[F] = Console[F]
+  implicit private val c: Console[F] = Console.make[F]
 
-  val run: F[Unit] = applicationResource.use_
-
-  private val applicationResource: Resource[F, Unit] =
-    c.println("Welcome to the Bifrost CLI").toResource >>
-    (readUserCommand >>= handleUserCommand).value.iterateWhile(_ != StageResult.Exit).void.toResource
+  def run: F[Unit] = applicationResource.use_
 
   private val readUserCommand =
-    StageResultT[F, CliApp.Command](
-      (
-        c.println("Please enter a command. [ QUIT | register | proposal]") >>
-        readLowercaseInput
-      ).flatMap {
-        case "" | "quit" =>
-          CliApp.Command.Quit.some.widen[CliApp.Command].pure[F]
-        case "register" =>
-          CliApp.Command.Register.some.widen[CliApp.Command].pure[F]
-        case "proposal" =>
-          CliApp.Command.Proposal.some.widen[CliApp.Command].pure[F]
-        case v =>
-          c.println(s"Invalid command: `$v`").as(none[CliApp.Command])
-      }.untilDefinedM
-        .map(StageResult.Success(_))
-    )
+    (
+      writeMessage[F]("Please enter a command. [ QUIT | register | proposal | init-testnet]") >>
+        readLowercasedInput
+    ).semiflatMap {
+      case "" | "quit" =>
+        CliApp.Command.Quit.some.widen[CliApp.Command].pure[F]
+      case "register" =>
+        CliApp.Command.Register.some.widen[CliApp.Command].pure[F]
+      case "proposal" =>
+        CliApp.Command.Proposal.some.widen[CliApp.Command].pure[F]
+      case "init-testnet" =>
+        CliApp.Command.InitTestnet.some.widen[CliApp.Command].pure[F]
+      case v =>
+        c.println(s"Invalid command: `$v`").as(none[CliApp.Command])
+    }.untilDefinedM
+
+  private val applicationResource: Resource[F, Unit] =
+    Sync[F]
+      .defer(
+        (writeMessage[F]("Welcome to the Bifrost CLI") >> readUserCommand >>= handleUserCommand).value
+          .iterateWhile(_ != StageResult.Exit)
+          .void
+      )
+      .toResource
 
   private def handleUserCommand(command: CliApp.Command): StageResultT[F, Unit] =
     command match {
-      case CliApp.Command.Quit =>
-        StageResultT[F, Unit](c.println("Mischief Managed.").as(StageResult.Exit))
-      case CliApp.Command.Register =>
-        new RegistrationCommand[F](appConfig).command
-      case CliApp.Command.Proposal =>
-        ProposalCommandImpl.make.command
+      case CliApp.Command.Quit        => QuitCommand[F]
+      case CliApp.Command.Register    => RegistrationCommand[F](appConfig)
+      case CliApp.Command.Proposal    => ProposalCommand[F]
+      case CliApp.Command.InitTestnet => InitTestnetCommand[F](appConfig)
     }
 
 }
@@ -54,5 +56,6 @@ object CliApp {
     case object Quit extends Command
     case object Register extends Command
     case object Proposal extends Command
+    case object InitTestnet extends Command
   }
 }
--- node/src/main/scala/co/topl/node/cli/package.scala
+++ node/src/main/scala/co/topl/node/cli/package.scala
@@ -1,7 +1,6 @@
 package co.topl.node
 
-import cats.Monad
-import cats.effect.Async
+import cats.effect.{Async, Sync}
 import cats.effect.std.Console
 import cats.implicits._
 import fs2.Chunk
@@ -9,17 +8,43 @@ import fs2.io.file.{Files, Path}
 
 package object cli {
 
+  def writeMessage[F[_]: Sync: Console](message: String): StageResultT[F, Unit] =
+    StageResultT.liftF[F, Unit](Sync[F].defer(Console[F].println(message)))
+
   /**
    * Prompt a user for input and trim any excess spaces
    */
-  def readInput[F[_]: Console: Monad]: F[String] =
-    Console[F].print("> ") >> Console[F].readLine.map(_.trim)
+  def readInput[F[_]: Sync: Console]: StageResultT[F, String] =
+    StageResultT.liftF(Sync[F].defer(Console[F].print("> ") >> Console[F].readLine.map(_.trim)))
 
   /**
    * Prompt a user for input trim any excess spaces, and lowercase the result
    */
-  def readLowercaseInput[F[_]: Console: Monad]: F[String] =
+  def readLowercasedInput[F[_]: Sync: Console]: StageResultT[F, String] =
     readInput.map(_.toLowerCase)
+
+  /**
+   * Prompts a user for an enumeration of inputs, and repeats the process until a valid input is received.
+   * @param prompt The message to give to the user
+   * @param choices A list of (lowercase) choices
+   * @param default An optional default value to use if no value is provided.
+   *                If the default is part of `choices`, it will be uppercased in the printed message
+   * @return The user's selection
+   */
+  def readLowercasedChoice[F[_]: Sync: Console](
+    prompt: String
+  )(choices: List[String], default: Option[String] = None): StageResultT[F, String] = {
+    val stringifiedChoices =
+      choices.map(c => if (default.contains(c)) c.toUpperCase else c).map(c => s" $c ").mkString("[", "|", "]")
+    (writeMessage[F](s"$prompt $stringifiedChoices") >> readLowercasedInput[F])
+      .flatMap(response =>
+        if (response.isEmpty) {
+          default.fold(writeMessage[F]("No input provided.").as(none[String]))(d => StageResultT.liftF(d.some.pure[F]))
+        } else if (choices.contains(response)) StageResultT.liftF(response.some.pure[F])
+        else writeMessage[F](s"Invalid input: $response").as(none[String])
+      )
+      .untilDefinedM
+  }
 
   /**
    * Write the given file to disk, and log the operation.  Files are saved to the configured staking directory.
@@ -28,14 +53,16 @@ package object cli {
    * @param logName  The name to use in the log operation
    * @param fileName The name of the file to save (within the staking directory)
    */
-  def write[F[_]: Console: Async](directory: Path)(contents: Array[Byte])(logName: String, fileName: String) =
+  def writeFile[F[_]: Async: Console](
+    directory: Path
+  )(contents: Array[Byte])(logName: String, fileName: String): StageResultT[F, Unit] =
     StageResultT.liftF[F, Unit] {
       val destination = directory / fileName
       Console[F].println(show"Writing $logName to $destination") >>
-      destination.parent.traverse(Files.forAsync[F].createDirectories) >>
+      Files.forAsync[F].createDirectories(directory) >>
       fs2.Stream
         .chunk(Chunk.array(contents))
-        .through(Files[F].writeAll(destination))
+        .through(Files.forAsync[F].writeAll(destination))
         .compile
         .drain
     }
