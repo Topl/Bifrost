92d510600f56af93dffd4647afcb94b53bb87168
Scala Steward <scala_steward@virtuslab.com>
2023-10-04 04:02:47 +0000

    Update cats-effect to 3.5.2 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val kamonVersion = "2.6.4"
   val simulacrumVersion = "1.0.1"
   val catsCoreVersion = "2.10.0"
-  val catsEffectVersion = "3.5.1"
+  val catsEffectVersion = "3.5.2"
   val fs2Version = "3.9.2"
   val logback = "1.4.11"
   val orientDbVersion = "3.2.23"
