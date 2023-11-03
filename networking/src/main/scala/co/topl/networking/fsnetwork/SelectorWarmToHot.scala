1d2cfbc4c7be9f53ae23c8193003cf665916cb9b
Oleg Aleksandrov <o.aleksandrov@topl.me>
2023-11-03 10:24:20 +0000

    Fixed unit test

--- networking/src/main/scala/co/topl/networking/fsnetwork/SelectorWarmToHot.scala
+++ networking/src/main/scala/co/topl/networking/fsnetwork/SelectorWarmToHot.scala
@@ -1,23 +1,22 @@
 package co.topl.networking.fsnetwork
 
-import co.topl.networking.p2p.RemoteAddress
 import scala.util.Random
 
 abstract class SelectorWarmToHot[F[_]] {
-  def select(hosts: Set[Peer[F]], countToReceive: Int): Set[RemoteAddress]
+  def select(hosts: Map[HostId, Peer[F]], countToReceive: Int): Set[HostId]
 }
 
 class ReputationRandomBasedSelectorWarmToHot[F[_]] extends SelectorWarmToHot[F] {
 
-  def select(hosts: Set[Peer[F]], countToReceive: Int): Set[RemoteAddress] = {
-    val random: Seq[RemoteAddress] =
-      Random.shuffle(hosts.flatMap(_.asRemoteAddress)).take(countToReceive).toSeq
+  def select(hosts: Map[HostId, Peer[F]], countToReceive: Int): Set[HostId] = {
+    val random: Seq[HostId] =
+      Random.shuffle(hosts.keySet).take(countToReceive).toSeq
 
-    val perfReputation: Seq[RemoteAddress] =
-      hosts.toSeq.sortBy(_.perfRep).takeRight(countToReceive).flatMap(_.asRemoteAddress)
+    val perfReputation: Seq[HostId] =
+      hosts.toSeq.sortBy(_._2.perfRep).takeRight(countToReceive).map(_._1)
 
-    val blockReputation: Seq[RemoteAddress] =
-      hosts.toSeq.sortBy(_.blockRep).takeRight(countToReceive).flatMap(_.asRemoteAddress)
+    val blockReputation: Seq[HostId] =
+      hosts.toSeq.sortBy(_._2.blockRep).takeRight(countToReceive).map(_._1)
 
     Random.shuffle(random ++ perfReputation ++ blockReputation).take(countToReceive).toSet
   }
