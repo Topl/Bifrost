69a3ec1f1d4372953e18277655b855916c69c083
Scala Steward <scala_steward@virtuslab.com>
2023-11-06 22:03:13 +0000

    Update circe-yaml to 0.15.1 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -103,7 +103,7 @@ object Dependencies {
   val fs2IO = "co.fs2"                     %% "fs2-io"               % fs2Version
   val fs2ReactiveStreams = "co.fs2"        %% "fs2-reactive-streams" % fs2Version
   val pureConfig = "com.github.pureconfig" %% "pureconfig"           % "0.17.4"
-  val circeYaml = "io.circe"               %% "circe-yaml"           % "0.15.0-RC1"
+  val circeYaml = "io.circe"               %% "circe-yaml"           % "0.15.1"
   val kubernetes = "io.kubernetes"          % "client-java"          % "19.0.0"
 
   val http4s = Seq(
