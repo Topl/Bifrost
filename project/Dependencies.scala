8348755922a2e07b9d4abff97e245490793b9d13
Scala Steward <scala_steward@virtuslab.com>
2023-11-06 22:02:45 +0000

    Update fs2-core to 3.9.3 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val simulacrumVersion = "1.0.1"
   val catsCoreVersion = "2.10.0"
   val catsEffectVersion = "3.5.2"
-  val fs2Version = "3.9.2"
+  val fs2Version = "3.9.3"
   val logback = "1.4.11"
   val orientDbVersion = "3.2.23"
   val ioGrpcVersion = "1.59.0"
