1077f4871de50c47f4e010619f93e1c3e0d3c316
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-11-08 14:12:52 +0100

    Dependency updates (#3091)
    * Update fs2-core to 3.9.3 in dev
    
    * Update sbt-assembly to 2.1.4 in dev
    
    * Update google-cloud-storage to 2.29.1 in dev
    
    * Update orientdb-client to 3.2.24 in dev
    
    * Update circe-yaml to 0.15.1 in dev
    
    * Update sbt-wartremover to 3.1.5 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,9 +7,9 @@ object Dependencies {
   val simulacrumVersion = "1.0.1"
   val catsCoreVersion = "2.10.0"
   val catsEffectVersion = "3.5.2"
-  val fs2Version = "3.9.2"
+  val fs2Version = "3.9.3"
   val logback = "1.4.11"
-  val orientDbVersion = "3.2.23"
+  val orientDbVersion = "3.2.24"
   val ioGrpcVersion = "1.59.0"
   val http4sVersion = "0.23.23"
   val protobufSpecsVersion = "2.0.0-alpha5" // scala-steward:off
@@ -103,7 +103,7 @@ object Dependencies {
   val fs2IO = "co.fs2"                     %% "fs2-io"               % fs2Version
   val fs2ReactiveStreams = "co.fs2"        %% "fs2-reactive-streams" % fs2Version
   val pureConfig = "com.github.pureconfig" %% "pureconfig"           % "0.17.4"
-  val circeYaml = "io.circe"               %% "circe-yaml"           % "0.15.0-RC1"
+  val circeYaml = "io.circe"               %% "circe-yaml"           % "0.15.1"
   val kubernetes = "io.kubernetes"          % "client-java"          % "19.0.0"
 
   val http4s = Seq(
@@ -165,7 +165,7 @@ object Dependencies {
       fs2IO,
       pureConfig,
       kubernetes,
-      "com.google.cloud" % "google-cloud-storage" % "2.29.0"
+      "com.google.cloud" % "google-cloud-storage" % "2.29.1"
     )
 
   lazy val actor: Seq[sbt.ModuleID] = fs2All
