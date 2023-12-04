4557ae79ec964e3c4b4aa7e2d984c15b2738ec95
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-12-04 19:08:18 +0100

    Update logback-classic to 1.4.14 in dev (#3140)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val catsCoreVersion = "2.10.0"
   val catsEffectVersion = "3.5.2"
   val fs2Version = "3.9.3"
-  val logback = "1.4.13"
+  val logback = "1.4.14"
   val orientDbVersion = "3.2.25"
   val ioGrpcVersion = "1.59.1"
   val http4sVersion = "0.23.24"
