5e58f8b34c8a949d6791bae6bfa89a771dd505ef
Sean Cheatham <seancheatham@gmail.com>
2023-12-12 16:58:20 -0500

    Modify StakingInit to search by StakingAddress

--- blockchain/src/main/scala/co/topl/blockchain/StakingInit.scala
+++ blockchain/src/main/scala/co/topl/blockchain/StakingInit.scala
@@ -17,8 +17,8 @@ import co.topl.consensus.algebras._
 import co.topl.consensus.models._
 import co.topl.interpreters.CatsSecureStore
 import co.topl.minting.algebras.StakingAlgebra
-import co.topl.minting.interpreters.{OperationalKeyMaker, Staking, VrfCalculator}
-import co.topl.node.models.FullBlock
+import co.topl.minting.interpreters._
+import co.topl.node.models.BlockBody
 import co.topl.typeclasses.implicits._
 import com.google.protobuf.ByteString
 import fs2.Chunk
@@ -68,14 +68,24 @@ object StakingInit {
     protocol:                 ApplicationConfig.Bifrost.Protocol,
     vrfConfig:                VrfConfig,
     protocolVersion:          ProtocolVersion,
-    blockFinder:              TransactionId => F[BlockHeader],
     metadata:                 NodeMetadataAlgebra[F],
-    fetchHeader:              BlockId => F[Option[BlockHeader]],
-    genesisBlock:             FullBlock
+    localChain:               LocalChainAlgebra[F],
+    fetchHeader:              BlockId => F[BlockHeader],
+    fetchBody:                BlockId => F[BlockBody],
+    fetchTransaction:         TransactionId => F[IoTransaction],
+    configuredStakingAddress: Option[StakingAddress]
   ): Resource[F, StakingAlgebra[F]] =
     for {
-      _ <- Logger[F].info(show"Loading registered staker from disk at path=$stakingDir").toResource
-      genesisNetworkId = genesisBlock.fullBody.transactions.flatMap(_.outputs).map(_.address).head.network
+      _         <- Logger[F].info(show"Loading registered staker from disk at path=$stakingDir").toResource
+      genesisId <- localChain.genesis.map(_.slotId.blockId).toResource
+      genesisNetworkId <- OptionT(
+        fetchBody(genesisId)
+          .flatMap(
+            _.transactionIds.collectFirstSomeM(transactionId =>
+              fetchTransaction(transactionId).map(_.outputs.headOption.map(_.address.network))
+            )
+          )
+      ).getOrRaise(new IllegalStateException("Could not determine networkId from genesis")).toResource
       _ <- Async[F]
         .raiseWhen(genesisNetworkId != rewardAddress.network)(
           new IllegalArgumentException("Configured staking rewardAddress belongs to a different network")
@@ -95,20 +105,26 @@ object StakingInit {
         )
         .toResource
       readFile = (p: Path) => Files.forAsync[F].readAll(p).compile.to(Chunk)
-      vrfSK       <- readFile(stakingDir / VrfKeyName).map(_.toArray).toResource
-      vrfVK       <- cryptoResources.ed25519VRF.use(e => Sync[F].delay(e.getVerificationKey(vrfSK))).toResource
-      transaction <- readFile(stakingDir / RegistrationTxName).map(_.toArray).map(IoTransaction.parseFrom).toResource
-      registration <- OptionT
-        .fromOption[F](transaction.outputs.headOption.flatMap(_.value.value.topl.flatMap(_.registration)))
-        .getOrRaise(new IllegalArgumentException("Registration Transaction is invalid"))
-        .toResource
+      vrfSK <- readFile(stakingDir / VrfKeyName).map(_.toArray).toResource
+      vrfVK <- cryptoResources.ed25519VRF.use(e => Sync[F].delay(e.getVerificationKey(vrfSK))).toResource
+      stakingAddress <- OptionT
+        .fromOption[Resource[F, *]](configuredStakingAddress)
+        .getOrElseF(
+          // In older implementations, the IoTransaction containing the registration must exist on disk.
+          // To support this behavior, fallback to reading it if the user doesn't configure the staking address
+          OptionT
+            .liftF(readFile(stakingDir / RegistrationTxName).map(_.toArray).map(IoTransaction.parseFrom).toResource)
+            .subflatMap(transaction =>
+              transaction.outputs.headOption.flatMap(_.value.value.topl.flatMap(_.registration.map(_.address)))
+            )
+            .getOrRaise(new IllegalArgumentException("Registration Transaction is invalid"))
+        )
       staking <- makeStaking(
         stakingDir,
         ByteString.copyFrom(vrfSK),
         ByteString.copyFrom(vrfVK),
-        registration.address,
+        stakingAddress,
         rewardAddress,
-        transaction,
         clock,
         etaCalculation,
         consensusValidationState,
@@ -117,9 +133,11 @@ object StakingInit {
         protocol,
         vrfConfig,
         protocolVersion,
-        blockFinder,
         metadata,
-        fetchHeader
+        localChain,
+        fetchHeader,
+        fetchBody,
+        fetchTransaction
       )
     } yield staking
 
@@ -132,7 +150,6 @@ object StakingInit {
     vrfVK:                    ByteString,
     stakingAddress:           StakingAddress,
     rewardAddress:            LockAddress,
-    registrationTransaction:  IoTransaction,
     clock:                    ClockAlgebra[F],
     etaCalculation:           EtaCalculationAlgebra[F],
     consensusValidationState: ConsensusValidationStateAlgebra[F],
@@ -141,26 +158,36 @@ object StakingInit {
     protocol:                 ApplicationConfig.Bifrost.Protocol,
     vrfConfig:                VrfConfig,
     protocolVersion:          ProtocolVersion,
-    localChain: LocalChainAlgebra[F],
-    consensusState: ConsensusValidationStateAlgebra[F]
+    metadata:                 NodeMetadataAlgebra[F],
+    localChain:               LocalChainAlgebra[F],
+    fetchHeader:              BlockId => F[BlockHeader],
+    fetchBody:                BlockId => F[BlockBody],
+    fetchTransaction:         TransactionId => F[IoTransaction]
   ): Resource[F, StakingAlgebra[F]] =
     for {
-      registrationHeader <-
+      (registrationHeader, registrationTransaction) <-
         // Check the metadata store to see if there already exists a cached entry.  First, double-check that the cached
         // entry is associated with the expected transaction ID
         OptionT(metadata.readStakingRegistrationTransactionId)
-          .filter(_ == registrationTransaction.id)
-          // If the transaction ID matches, also fetch the corresponding Block ID
-          .flatMap(_ => OptionT(metadata.readStakingRegistrationBlockId))
-          .flatMapF(fetchHeader)
+          .semiflatMap(fetchTransaction)
+          .filter(containsActivationRegistrationFor(stakingAddress))
+          .flatMap(registrationTransaction =>
+            // If the transaction ID matches, also fetch the corresponding Block ID
+            OptionT(metadata.readStakingRegistrationBlockId)
+              .filterF(
+                fetchBody(_).map(_.transactionIds.contains(registrationTransaction.id))
+              )
+              .semiflatMap(fetchHeader)
+              .tupleRight(registrationTransaction)
+          )
           // If the metadata store did not contain an entry, go and find the header on the chain
           .getOrElseF(
-            blockFinder(registrationTransaction.id)
+            findStakerRegistration(localChain, fetchHeader, fetchBody, fetchTransaction)(stakingAddress)
               // Once the header has been found, cache its ID in the metadata store
-              .flatTap(header =>
-                metadata.setStakingRegistrationTransactionId(registrationTransaction.id) *>
+              .flatTap { case (header, transaction) =>
+                metadata.setStakingRegistrationTransactionId(transaction.id) *>
                 metadata.setStakingRegistrationBlockId(header.id)
-              )
+              }
           )
           .toResource
       _ <- Logger[F]
@@ -228,4 +255,46 @@ object StakingInit {
       )
     } yield staking
 
+  /**
+   * Scans the canonical chain for the staker's activation registration transaction.
+   * @return a tuple (The containg header, the registration transaction)
+   */
+  def findStakerRegistration[F[_]: Async](
+    localChain:       LocalChainAlgebra[F],
+    fetchHeader:      BlockId => F[BlockHeader],
+    fetchBody:        BlockId => F[BlockBody],
+    fetchTransaction: TransactionId => F[IoTransaction]
+  )(stakingAddress: StakingAddress): F[(BlockHeader, IoTransaction)] =
+    OptionT(
+      fs2.Stream
+        .eval(localChain.head)
+        .map(_.slotId.blockId)
+        .flatMap(head =>
+          fs2.Stream.unfoldLoopEval(head)(id =>
+            // TODO optimization: Stop scanning if the headers become older than the maximum KES evolutions would permit
+            fetchHeader(id).map(h => (h, Option.when(h.height > BigBang.Height)(h.parentHeaderId)))
+          )
+        )
+        .flatMap(header =>
+          fs2.Stream
+            .evalSeq(fetchBody(header.id).map(_.transactionIds))
+            .evalMap(fetchTransaction)
+            // Find the "activation transaction", meaning the first transaction that registered the address
+            // Stake-shifting transactions (i.e. existing registration, but new stake quantity) are skipped
+            .find(containsActivationRegistrationFor(stakingAddress))
+            .tupleLeft(header)
+        )
+        .head
+        .compile
+        .last
+    ).getOrRaise(
+      new IllegalStateException(
+        "StakingAddress not found in canonical chain.  Make sure your registration transaction is broadcasted" +
+        " and confirmed before launching the node with staking enabled."
+      )
+    )
+
+  private def containsActivationRegistrationFor(stakingAddress: StakingAddress)(transaction: IoTransaction) =
+    transaction.outputs.exists(_.value.value.topl.exists(_.registration.exists(_.address == stakingAddress))) &&
+    !transaction.inputs.exists(_.value.value.topl.exists(_.registration.exists(_.address == stakingAddress)))
 }
