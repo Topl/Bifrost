45492ebf9850d8e9c44511bc7e7e19a4b322200f
Scala Steward <scala_steward@virtuslab.com>
2024-03-06 22:59:05 +0000

    Update kamon-cats-io-3 to 2.7.1 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val circeVersion = "0.14.6"
-  val kamonVersion = "2.7.0"
+  val kamonVersion = "2.7.1"
   val simulacrumVersion = "1.0.1"
   val catsCoreVersion = "2.10.0"
   val catsEffectVersion = "3.5.3"
