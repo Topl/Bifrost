91aa892fe85a26286f0065a3610795fdcbecbcd0
Scala Steward <scala_steward@virtuslab.com>
2024-07-30 04:54:30 +0000

    Update grpc-netty-shaded to 1.64.2 in dev

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val fs2Version = "3.10.2"
   val logback = "1.5.6"
   val orientDbVersion = "3.2.32"
-  val ioGrpcVersion = "1.64.0"
+  val ioGrpcVersion = "1.64.2"
   val http4sVersion = "0.23.26"
   val protobufSpecsVersion = "2.0.0-beta3+3-bd44cc82-SNAPSHOT"
   val bramblScVersion = "2.0.0-beta8"
